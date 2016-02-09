7fc4a63fe7a670b6464af2afbea264ec1a9ccc3e
kittiu <kittiu@kittiu>
2016-02-09 10:46:27 +0700

    [ENH] l10n_th_amount_text_ext, just add more comment.

--- order_invoice_line_percentage/models/sale.py
+++ order_invoice_line_percentage/models/sale.py
@@ -19,4 +19,23 @@ class sale_order(models.Model):
             self.order_line[0].tax_id and \
             self.order_line[0].tax_id[0].price_include or False
 
+    @api.model
+    def _append_invoice_line(self, invoice, lines):
+        if invoice.is_deposit:
+            lines = super(sale_order, self)._append_invoice_line(invoice,
+                                                                 lines)
+        return lines
+
+    @api.model
+    def _selected_invoice_lines(self, order, states):
+        """ Overwrite """
+        lines = []
+        for line in order.order_line and \
+                not self._context.get('invoice_plan_percent', False):  # Check
+            if line.invoiced:
+                continue
+            elif (line.state in states):
+                lines.append(line.id)
+        return lines
+
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
