e3d42d5ba674efede8572937bd41f7f5530c75f9
kittiu <kittiu@kittiu>
2016-02-14 12:16:37 +0700

    [ADD] res_currency_rate_ex

---
+++ res_currency_rate_ext/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Currency Rate Extension',
+    'version': '1.0',
+    'author': 'Ecosoft',
+    'summary': 'Allowing Smaller and Bigger Exchange Rate',
+    'description': """
+    """,
+    'category': 'Accounting',
+    'website': 'http://www.ecosoft.co.th',
+    'depends': ['account'],
+    'data': ['currency_rate_view.xml'],
+    'auto_install': False,
+    'application': True,
+    'installable': True,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
