1c01abcdc6e74eee67ae2fedbbe7fc0db18f9ed2
Kitti U. <kittiu@gmail.com>
2016-02-27 14:12:43 +0700

    [ADD] account_invoice_overwrite_duedate

---
+++ account_invoice_overwrite_duedate/models/account_invoice.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# © <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models
+
+
+class ModelName(models.Model):
+    _inherit = "model.name"
