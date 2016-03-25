7c4b883a25cb465b2faf2195b702066813f73bc3
siraparn <siraparn@trinityroots.co.th>
2016-03-25 12:10:41 +0700

    [FIX] fix flake8 cleaning code

--- pabi_procurement/models/purchase.py
+++ pabi_procurement/models/purchase.py
@@ -2,7 +2,7 @@
 # © 2015 TrinityRoots
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import fields, models, api, _
+from openerp import fields, models, api
 import time
 
 
@@ -112,4 +112,3 @@ class PurchaseOrderLine(models.Model):
         'purchase_requisition_line',
         'Purchase Requisition Line'
     )
-
