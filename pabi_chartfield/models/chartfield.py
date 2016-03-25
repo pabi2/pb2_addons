2ae481733be7f9b46c5421c0b2f1709424e07b63
Kitti U. <kittiu@gmail.com>
2016-03-25 18:02:23 +0700

    [ENH] pabi_chartfield

--- pabi_chartfield/models/chartfield.py
+++ pabi_chartfield/models/chartfield.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-from openerp import api, fields
+from openerp import api, fields, models
 
 
 CHART_VIEW = [
@@ -43,6 +43,42 @@ class ChartField(object):
     division_id = fields.Many2one('res.division', 'Division')
     department_id = fields.Many2one('res.department', 'Department')
     costcenter_id = fields.Many2one('res.costcenter', 'Costcenter')
+
+    @api.onchange('costcenter_id')
+    def _onchange_costcenter_id(self):
+
+        self.org_id = self.costcenter_id.org_id
+        self.sector_id = self.costcenter_id.sector_id
+        self.division_group_id = self.costcenter_id.division_group_id
+        self.division_id = self.costcenter_id.division_id
+        self.department_id = self.costcenter_id.department_id
+        self.costcenter_id = self.costcenter_id
+
+        self.spa_id = False
+        self.mission_id = False
+        self.program_scheme_id = False
+        self.program_group_id = False
+        self.program_id = False
+        self.project_group_id = False
+        self.project_id = False
+
+    @api.onchange('project_id')
+    def _onchange_project_id(self):
+
+        self.org_id = False
+        self.sector_id = False
+        self.division_group_id = False
+        self.division_id = False
+        self.department_id = False
+        self.costcenter_id = False
+
+        self.spa_id = self.project_id.program_id.current_spa_id
+        self.mission_id = self.project_id.mission_id
+        self.program_scheme_id = self.project_id.program_scheme_id
+        self.program_group_id = self.project_id.program_group_id
+        self.program_id = self.project_id.program_id
+        self.project_group_id = self.project_id.project_group_id
+        self.project_id = self.project_id
 
     @api.multi
     def validate_chartfields(self, chart_type):
--- pabi_chartfield/models/purchase.py
+++ pabi_chartfield/models/purchase.py
@@ -6,39 +6,3 @@ from .chartfield import \
 
 class PurchaseOrderLine(ChartField, models.Model):
     _inherit = 'purchase.order.line'
-
-    @api.onchange('costcenter_id')
-    def _onchange_costcenter_id(self):
-
-        self.org_id = self.costcenter_id.org_id
-        self.sector_id = self.costcenter_id.sector_id
-        self.division_group_id = self.costcenter_id.division_group_id
-        self.division_id = self.costcenter_id.division_id
-        self.department_id = self.costcenter_id.department_id
-        self.costcenter_id = self.costcenter_id
-
-        self.spa_id = False
-        self.mission_id = False
-        self.program_scheme_id = False
-        self.program_group_id = False
-        self.program_id = False
-        self.project_group_id = False
-        self.project_id = False
-
-    @api.onchange('project_id')
-    def _onchange_project_id(self):
-
-        self.org_id = False
-        self.sector_id = False
-        self.division_group_id = False
-        self.division_id = False
-        self.department_id = False
-        self.costcenter_id = False
-
-        self.spa_id = self.project_id.program_id.current_spa_id
-        self.mission_id = self.project_id.mission_id
-        self.program_scheme_id = self.project_id.program_scheme_id
-        self.program_group_id = self.project_id.program_group_id
-        self.program_id = self.project_id.program_id
-        self.project_group_id = self.project_id.project_group_id
-        self.project_id = self.project_id
