1dafcee85adba19b8b8be593182fc6e02f4cc058
Probuse <kaushik.probuse@gmail.com>
2016-04-15 09:09:52 +0530

    [ADD]account_move_line_doc_ref: added module for pass doc ref to account move lines from invoices

---
+++ account_move_line_doc_ref/__openerp__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Account Move Line - Doc Ref',
+    'version': '1.0',
+    'author': 'Ecosoft',
+    'summary': 'Account Move Line - Doc Ref',
+    'description': """
+Account Move Line - Doc Ref
+    """,
+    'category': 'Accounting',
+    'website': 'http://www.ecosoft.co.th',
+    'images': [],
+    'depends': [
+        'account',
+    ],
+    'demo': [],
+    'data': [
+        'views/account_move_line_view.xml',
+    ],
+    'test': [
+    ],
+    'auto_install': False,
+    'application': True,
+    'installable': True,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
