05068582915140504b255ff4e94b746bcd61ab66
Probuse <kaushik.probuse@gmail.com>
2016-04-22 16:02:47 +0530

    [REM]voucher_move_line_doc_ref: commented unsed line

--- voucher_move_line_doc_ref/models/account_voucher.py
+++ voucher_move_line_doc_ref/models/account_voucher.py
@@ -11,7 +11,7 @@ class AccountVoucher(models.Model):
             action_move_line_create(cr, uid, ids, context=context)
         for voucher in self.browse(cr, uid, ids, context):
             if voucher.move_id:
-                voucher.move_id.line_id.write({'doc_ref': voucher.number})
+                # voucher.move_id.line_id.write({'doc_ref': voucher.number})
                 self.pool.get('account.move.line').write(
                     cr, uid,
                     voucher.move_id.line_id.ids,
