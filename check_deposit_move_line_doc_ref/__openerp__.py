49e6c0806983f4bff2020454ca1907e7a6f2bfb3
Probuse <kaushik.probuse@gmail.com>
2016-04-22 16:06:10 +0530

    [ADD]check_deposit_move_line_doc_ref: Added module for pass doc ref for deposit

---
+++ check_deposit_move_line_doc_ref/__openerp__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Account Check Deposit Move Line - Doc Ref',
+    'version': '1.0',
+    'author': 'Ecosoft',
+    'summary': 'Account Check Deposit Move Line - Doc Ref',
+    'description': """
+Account Check Deposit Move Line - Doc Ref
+    """,
+    'category': 'Accounting',
+    'website': 'http://www.ecosoft.co.th',
+    'images': [],
+    'depends': [
+        'account_move_line_doc_ref',
+        'account_check_deposit',
+    ],
+    'demo': [],
+    'data': [
+    ],
+    'test': [
+    ],
+    'auto_install': False,
+    'application': True,
+    'installable': True,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ check_deposit_move_line_doc_ref/models/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+from . import check_deposit
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
