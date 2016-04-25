69e19395dcfb32478d5a4f4fe09792ef6725bc2e
Kitti U. <kittiu@gmail.com>
2016-04-25 18:53:20 +0700

    [ADD] language_switcher

---
+++ language_switcher/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "Language Switcher",
+    'version': '0.1',
+    'category': 'Uncategorized',
+    'description': """
+Language Switcher.
+""",
+    'author': "Kitti U.",
+    'website': 'http://ecosoft.co.th',
+    'license': 'AGPL-3',
+    "depends": ['base', 'web'],
+    "data": [
+        'views/template.xml',
+    ],
+    'test': [
+    ],
+    "demo": [],
+    "active": True,
+    "installable": True,
+}
