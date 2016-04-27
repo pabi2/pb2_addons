aa31e3a5d7ff02fe1fc30b7c21437669da13c738
Kitti U. <kittiu@gmail.com>
2016-04-27 23:58:17 +0700

    [ENH] pabi_chartfield, add nstda_course

--- pabi_chartfield/__openerp__.py
+++ pabi_chartfield/__openerp__.py
@@ -17,6 +17,7 @@
     'data': [
         'security/ir.model.access.csv',
         'data/cost_control_data.xml',
+        'data/cost.control.csv',
         'views/account_budget_view.xml',
         'views/account_invoice_view.xml',
         'views/analytic_view.xml',
