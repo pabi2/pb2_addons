e939e39ab6c5715b93c5f3856a4cce414fe6bc79
siraparn <siraparn@trinityroots.co.th>
2016-05-04 20:26:51 +0700

    [FIX] cleanse some code

--- pabi_procurement/models/__init__.py
+++ pabi_procurement/models/__init__.py
@@ -4,6 +4,5 @@ from . import purchase
 from . import purchase_request
 from . import purchase_requisition
 from . import stock
-from . import partner
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
