e546eec44d916ea23da6a231f1f3c69e05354854
siraparn <siraparn@trinityroots.co.th>
2016-05-06 09:16:59 +0700

    [ENH] Rework on WA

--- pabi_procurement/wizard/__init__.py
+++ pabi_procurement/wizard/__init__.py
@@ -2,6 +2,5 @@
 
 from . import purchase_request_line_make_purchase_requisition
 from . import reject_reason
-from . import print_purchase_work_acceptance
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
