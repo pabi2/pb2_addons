34c29241a8b6ba51e3f0e4a70a78c7a30ebf75f4
Kitti U. <kittiu@gmail.com>
2016-05-06 14:10:15 +0700

    [REM] pabi_ou_field_required

--- pabi_ou_field_required/model/hr_expense.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from openerp import fields, models
-
-
-class HrExpenseExpense(models.Model):
-    _inherit = 'hr.expense.expense'
-
-    operating_unit_id = fields.Many2one(required=True)
--- pabi_ou_field_required/model/purchase_request_line_make_purchase_requisition.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from openerp import fields, models
-
-
-class PurchaseRequestLineMakePurchaseRequisition(models.TransientModel):
-    _inherit = "purchase.request.line.make.purchase.requisition"
-
-    operating_unit_id = fields.Many2one(required=True)
