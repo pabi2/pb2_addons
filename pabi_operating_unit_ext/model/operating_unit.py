a90cd1e20a00e245a7e099a060140412ab0ff065
Kitti U. <kittiu@gmail.com>
2016-05-15 12:04:03 +0700

    Bug #563

--- pabi_operating_unit_ext/model/operating_unit.py
+++ pabi_operating_unit_ext/model/operating_unit.py
@@ -8,3 +8,10 @@ class OperatingUnit(models.Model):
     @api.model
     def _ou_domain(self):
         return [('id', 'in', [g.id for g in self.env.user.operating_unit_ids])]
+
+    @api.model
+    def name_search(self, name, args=None, operator='ilike', limit=100):
+        res = super(OperatingUnit, self).name_search(
+            name, args=args, operator=operator, limit=limit)
+        domain_ids = self._ou_domain()[0][2]  # Ensure domain
+        return filter(lambda x: x[0] in domain_ids, res)
