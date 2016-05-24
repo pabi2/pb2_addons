d1cf33702013beb79cff60017dd7825b0ae6f27a
Kitti U. <kittiu@gmail.com>
2016-05-24 21:18:09 +0700

    [ENH] l10n_th_fields, unique taxbranch

--- l10n_th_fields/partner.py
+++ l10n_th_fields/partner.py
@@ -7,8 +7,16 @@ from openerp.exceptions import ValidationError
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    vat = fields.Char(string='Tax ID', size=13)
-    taxbranch = fields.Char(string='Tax Branch ID', size=5)
+    vat = fields.Char(
+        string='Tax ID',
+        size=13,
+        copy=False,
+    )
+    taxbranch = fields.Char(
+        string='Tax Branch ID',
+        size=5,
+        copy=False,
+    )
 
     @api.one
     @api.constrains('vat')
@@ -23,5 +31,22 @@ class ResPartner(models.Model):
         if self.taxbranch and len(self.taxbranch) != 5:
             raise ValidationError(
                 _("Tax Branch must be 5 digits"))
+
+    @api.one
+    @api.constrains('name', 'supplier', 'customer')
+    def _check_partner_name(self):
+        count = self.search_count([('name', '=', self.name)])
+        if count > 1:
+            raise ValidationError("Partner Name must be unique!")
+
+    @api.one
+    @api.constrains('vat', 'taxbranch')
+    def _check_vat_taxbranch_unique(self):
+        if self.vat or self.taxbranch:
+            count = self.search_count([('vat', '=', self.vat),
+                                       ('taxbranch', '=', self.taxbranch)])
+            if count > 1:
+                raise ValidationError(
+                    _("Tax ID + Tax Branch ID must be unique!"))
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
