d94cf716ead1297f8aec45b90bd2a3ff3a2c54af
Probuse <kaushik.probuse@gmail.com>
2016-06-06 14:03:35 +0530

    [FIX]pabi_chartfield: changed version for travis error

--- pabi_chartfield/__openerp__.py
+++ pabi_chartfield/__openerp__.py
@@ -5,7 +5,7 @@
     'author': "Ecosoft",
     'website': "http://ecosoft.co.th",
     'category': 'Account',
-    'version': '0.1.0',
+    'version': '8.0.1.0.0',
     'depends': [
         'pabi_base',
         'account_budget_activity',
