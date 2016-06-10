e9b1141a8ab4bfb9cb574d2873e60123d07d0e49
Probuse <kaushik.probuse@gmail.com>
2016-06-10 11:44:46 +0530

    [ADD]hr_expense_cancel_reason: added module for add cancel reason on refuse expense

---
+++ hr_expense_cancel_reason/models/hr_expense.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields
+
+
+class HRExpenseExpense(models.Model):
+    _inherit = 'hr.expense.expense'
+
+    cancel_reason_txt = fields.Char(
+        string="Description",
+        readonly=True)
