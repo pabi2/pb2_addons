284bbbe5d28205f06e5a239b44424c427178ab23
Kitti U. <kittiu@gmail.com>
2016-06-13 23:15:00 +0700

    [FIX] hr_expense_advance_clearing

--- hr_expense_advance_clearing/models/account_invoice.py
+++ hr_expense_advance_clearing/models/account_invoice.py
@@ -8,13 +8,9 @@ from openerp import models, api, fields
 class AccountInvoice(models.Model):
     _inherit = "account.invoice"
 
-    advance_move_line_id = fields.Many2one(
-        'account.move.line',
-        'Expense Advance Journal Item',
-        readonly=True,
-        copy=False,
+    is_advance_clearing = fields.Boolean(
+        string='Advance Clearing?',
     )
-    is_advance_clearing = fields.Boolean('Advance Clearing?')
 
     @api.model
     def _get_invoice_total(self, invoice):
@@ -33,28 +29,15 @@ class AccountInvoice(models.Model):
 
     @api.multi
     def invoice_validate(self):
-        line_accounts = []
-        for invoice in self:
-            if invoice.is_advance_clearing:
-                for line in invoice.invoice_line:
-                    line_accounts.append(line.account_id.id)
         result = super(AccountInvoice, self).invoice_validate()
         for invoice in self:
-            if invoice.is_advance_clearing:
-                if invoice.amount_total > 0.0:
-                    if invoice.move_id:
-                        move_line =\
-                            invoice.move_id.line_id.filtered(
-                                lambda x: x.account_id.id in line_accounts)
-        #             if invoice.advance_move_line_id:
-        #                 advance_move_line = invoice.advance_move_line_id
-        #                 move_lines = invoice.move_id.line_id
-        #                 move_line_to_reconcile = advance_move_line
-        #                 move_line =\
-        #                     move_lines.filtered(
-        #                         lambda x: x.account_id.reconcile is True and
-        #                         x.account_id.type == 'other')
-        #                 move_line_to_reconcile += move_line
-                        if move_line:
-                            move_line.reconcile_partial(type='manual')
+            if invoice.is_advance_clearing and not invoice.amount_total:
+                move_lines = \
+                    self.env['account.move.line'].search(
+                        [('state', '=', 'valid'),
+                         ('account_id.type', '=', 'payable'),
+                         ('reconcile_id', '=', False),
+                         ('move_id', '=', invoice.move_id.id),
+                         ('debit', '=', 0.0), ('credit', '=', 0.0)])
+                move_lines.reconcile(type='manual')
         return result
