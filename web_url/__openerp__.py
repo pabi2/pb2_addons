726d0fec7c5e1f3b019a17670d37a18d6b4bc384
siraparn <siraparn@trinityroots.co.th>
2016-06-23 13:28:36 +0700

    [FIX] flake 8 test

--- web_url/__openerp__.py
+++ web_url/__openerp__.py
@@ -1,17 +1,15 @@
 {
     "name": "Web/URL widget",
-    "description":
-        """
-        After Installation of this module, user will be directly redirect to given URL, if field's widget set to widget="url" in tree view.
-        
+    "description": """
+        After Installation of this module, user will be directly redirect to
+        given URL, if field's widget set to widget="url" in tree view.
         You can send feedback to info@emiprotechnologies.com,
- 
         """,
     "version": "8.0",
-    "author" : "Emipro Technologies",
-    "website" : "http://www.emiprotechnologies.com",
-    "category" : "Tools",
-    "depends" : ["web"],
+    "author": "Emipro Technologies",
+    "website": "http://www.emiprotechnologies.com",
+    "category": "Tools",
+    "depends": ["web"],
     "data": [
         "views/web_url.xml"
     ],
