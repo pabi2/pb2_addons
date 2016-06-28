94057f239eaac405bc1e1870ba096dcd69c52816
siraparn <siraparn@trinityroots.co.th>
2016-06-28 17:14:09 +0700

    [FIX] PD send_prweb_requisition

--- pabi_procurement/models/purchase.py
+++ pabi_procurement/models/purchase.py
@@ -3,6 +3,7 @@
 from openerp import fields, models, api, _
 from openerp.exceptions import Warning as UserError
 from openerp.tools import float_compare
+from openerp.osv.orm import browse_record_list, browse_record, browse_null
 
 
 class PurchaseOrder(models.Model):
@@ -145,6 +146,110 @@ class PurchaseOrder(models.Model):
         picking = self.env['stock.picking'].search([('id', '=', res[0])])
         picking.verified = True
         return res
+
+    def do_merge(self, cr, uid, ids, context=None):
+        """
+        To merge similar type of purchase orders.
+        Orders will only be merged if:
+        * Purchase Orders are in draft
+        * Purchase Orders belong to the same partner
+        * Purchase Orders are have same stock location, same pricelist, same currency
+        Lines will only be merged if:
+        * Order lines are exactly the same except for the quantity and unit
+
+         @param self: The object pointer.
+         @param cr: A database cursor
+         @param uid: ID of the user currently logged in
+         @param ids: the ID or list of IDs
+         @param context: A standard dictionary
+
+         @return: new purchase order id
+
+        """
+        #TOFIX: merged order line should be unlink
+        def make_key(br, fields):
+            list_key = []
+            for field in fields:
+                field_val = getattr(br, field)
+                if field in ('product_id', 'account_analytic_id'):
+                    if not field_val:
+                        field_val = False
+                if isinstance(field_val, browse_record):
+                    field_val = field_val.id
+                elif isinstance(field_val, browse_null):
+                    field_val = False
+                elif isinstance(field_val, browse_record_list):
+                    field_val = ((6, 0, tuple([v.id for v in field_val])),)
+                list_key.append((field, field_val))
+            list_key.sort()
+            return tuple(list_key)
+
+        context = dict(context or {})
+
+        # Compute what the new orders should contain
+        new_orders = {}
+
+        order_lines_to_move = {}
+        for porder in [order for order in self.browse(cr, uid, ids, context=context) if order.state == 'draft']:
+            order_key = make_key(porder, ('partner_id', 'location_id', 'pricelist_id', 'currency_id'))
+            new_order = new_orders.setdefault(order_key, ({}, []))
+            new_order[1].append(porder.id)
+            order_infos = new_order[0]
+            order_lines_to_move.setdefault(order_key, [])
+
+            if not order_infos:
+                order_infos.update({
+                    'origin': porder.origin,
+                    'date_order': porder.date_order,
+                    'partner_id': porder.partner_id.id,
+                    'dest_address_id': porder.dest_address_id.id,
+                    'picking_type_id': porder.picking_type_id.id,
+                    'location_id': porder.location_id.id,
+                    'pricelist_id': porder.pricelist_id.id,
+                    'currency_id': porder.currency_id.id,
+                    'state': 'draft',
+                    'order_line': {},
+                    'notes': '%s' % (porder.notes or '',),
+                    'fiscal_position': porder.fiscal_position and porder.fiscal_position.id or False,
+                })
+            else:
+                if porder.date_order < order_infos['date_order']:
+                    order_infos['date_order'] = porder.date_order
+                if porder.notes:
+                    order_infos['notes'] = (order_infos['notes'] or '') + ('\n%s' % (porder.notes,))
+                if porder.origin:
+                    order_infos['origin'] = (order_infos['origin'] or '') + ' ' + porder.origin
+
+            order_lines_to_move[order_key] += [order_line.id for order_line in porder.order_line
+                                               if order_line.state != 'cancel']
+
+        allorders = []
+        orders_info = {}
+        for order_key, (order_data, old_ids) in new_orders.iteritems():
+            # skip merges with only one order
+            if len(old_ids) < 2:
+                allorders += (old_ids or [])
+                continue
+
+            # cleanup order line data
+            for key, value in order_data['order_line'].iteritems():
+                del value['uom_factor']
+                value.update(dict(key))
+            order_data['order_line'] = [(6, 0, order_lines_to_move[order_key])]
+
+            # create the new order
+            context.update({'mail_create_nolog': True})
+            neworder_id = self.create(cr, uid, order_data)
+            self.message_post(cr, uid, [neworder_id], body=_("RFQ created"), context=context)
+            orders_info.update({neworder_id: old_ids})
+            allorders.append(neworder_id)
+
+            # make triggers pointing to the old orders point to the new order
+            for old_id in old_ids:
+                self.redirect_workflow(cr, uid, [(old_id, neworder_id)])
+                self.signal_workflow(cr, uid, [old_id], 'purchase_cancel')
+
+        return orders_info
 
 
 class PRWebPurchaseMethod(models.Model):
