f2e5bedea1f432e689c25277e6938f836a5f8b5e
Probuse <kaushik.probuse@gmail.com>
2016-06-29 15:09:38 +0530

    [ADD]hr_expense_value_date: added new module for issues/717

---
+++ hr_expense_value_date/__openerp__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'HR Expense Value Date',
+    'version': '1.0',
+    'author': "Ecosoft",
+    'category': 'Human Resources',
+    'license': 'AGPL-3',
+    'images': [],
+    'website': "http://ecosoft.co.th",
+    'description': """
+    When a supplier payment is validated it records
+    the date history on related expense.
+    """,
+    'depends': [
+        'l10n_th_fields',
+        'hr_expense_auto_invoice',
+    ],
+    'demo': [],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/hr_expense_view.xml',
+        'wizard/change_value_date_wizard_view.xml',
+    ],
+    'auto_install': False,
+    'installable': True,
+}
---
+++ hr_expense_value_date/models/hr_expense.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api
+
+
+class DateValueHistory(models.Model):
+    _name = 'date.value.history'
+
+    name = fields.Char(
+        compute='_compute_name',
+        string='Name',
+        store=True,
+    )
+    voucher_id = fields.Many2one(
+        'account.voucher',
+        string='Supplier Payment',
+        required=True,
+    )
+    invoice_id = fields.Many2one(
+        'account.invoice',
+        string='Supplier Invoice',
+    )
+    expense_id = fields.Many2one(
+        'hr.expense.expense',
+        string='Expense',
+    )
+    date_value = fields.Date(
+        string='Value Date',
+    )
+    amount = fields.Float(
+        string='Amount',
+    )
+    user_id = fields.Many2one(
+        'res.users',
+        string='Created By',
+    )
+    date = fields.Date(
+        string='Created On',
+    )
+
+    @api.depends('voucher_id', 'voucher_id.number',
+                 'invoice_id', 'invoice_id.number',
+                 'expense_id', 'expense_id.number')
+    def _compute_name(self):
+        for record in self:
+            record.name = record.voucher_id.number + '-' + \
+                record.invoice_id.number + '-' + \
+                record.expense_id.number
+
+
+class HRExpenseExpense(models.Model):
+    _inherit = 'hr.expense.expense'
+
+    date_value_history_ids = fields.One2many(
+        'date.value.history',
+        'expense_id',
+        string='Value Date History',
+        readonly=True,
+    )
