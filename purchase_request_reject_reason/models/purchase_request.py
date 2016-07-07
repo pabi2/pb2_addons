b06775dff61069de69d7a8656f9623a216d306ad
siraparn <siraparn@trinityroots.co.th>
2016-07-07 12:21:30 +0700

    [ADD] Purchase Request Reject Reason Module

---
+++ purchase_request_reject_reason/models/purchase_request.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from openerp import fields, models
+
+
+class PurchaseRequest(models.Model):
+    _inherit = "purchase.request"
+
+    reject_reason_txt = fields.Char(
+        string="Rejected Reason",
+        readonly=True,
+    )
