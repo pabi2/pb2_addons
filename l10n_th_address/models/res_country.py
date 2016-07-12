f8446da43898b9582d80a1d81dbd9aca4aa8a633
Kitti U. <kittiu@gmail.com>
2016-07-12 13:14:27 +0700

    [ENH] remove EN field

--- l10n_th_address/models/res_country.py
+++ l10n_th_address/models/res_country.py
@@ -3,20 +3,12 @@
 from openerp import models, fields
 
 
-class ResCountry(models.Model):
-
-    _inherit = 'res.country'
-
-    name_en = fields.Char(string='Country (en)', required=False)
-
-
 class ResCountryProvince(models.Model):
 
     _name = 'res.country.province'
     _description = 'Provinces'
 
     name = fields.Char(string='Province', required=True)
-    name_en = fields.Char(string='Province (en)', required=False)
     country_id = fields.Many2one(
         'res.country',
         string='Country',
@@ -30,7 +22,6 @@ class ResCountryDistrict(models.Model):
     _description = 'Districts'
 
     name = fields.Char(string='District', required=True)
-    name_en = fields.Char(string='District (en)', required=False)
     province_id = fields.Many2one(
         'res.country.province',
         string='Province',
@@ -44,7 +35,6 @@ class ResCountryTownship(models.Model):
     _description = 'Township'
 
     name = fields.Char(string='Township', required=True)
-    name_en = fields.Char(string='Township (en)', required=False)
     district_id = fields.Many2one(
         'res.country.district',
         string='District',
