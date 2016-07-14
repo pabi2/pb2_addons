dc1dfeea01fcae08e64608d610168ce6db7ee346
siraparn <siraparn@trinityroots.co.th>
2016-07-14 17:36:17 +0700

    [ADD] purchase force done module

---
+++ purchase_force_done/__openerp__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# © 2015 TrinityRoots>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Purchase - Purchase Order Force Done",
+    "summary": "Module summary",
+    "version": "8.0.1.0.0",
+    "category": "Purchase",
+    "description": """
+
+Purchase - Purchase Order Force Done
+====================================
+
+  *When users need to assign done state, They have to specify the reason first.
+  *After the reason has been specified, Remaining shipments will be cancelled.
+
+    """,
+    "website": "http://www.trinityroots.co.th",
+    "author": "TrinityRoots",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "purchase",
+    ],
+    "data": [
+        "wizard/force_done_reason_view.xml",
+        "security/ir.model.access.csv",
+        "views/purchase_view.xml",
+    ],
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
