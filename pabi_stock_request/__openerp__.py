98d8382858c077b431254706eb809ee2b2578268
Kitti U. <kittiu@gmail.com>
2016-08-01 00:06:33 +0700

    [ADD] pabi_stock_request

---
+++ pabi_stock_request/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Stock Request",
+    "summary": "",
+    "version": "8.0.1.0.0",
+    "category": "Warehouse",
+    "description": """
+
+    """,
+    "website": "https://ecosoft.co.th/",
+    "author": "Kitti U.",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/stock_request_view.xml",
+    ],
+}
