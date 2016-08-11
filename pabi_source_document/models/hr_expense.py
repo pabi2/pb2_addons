ef3327c7756745cc769e314eecf3d40ae1693dfb
Probuse <kaushik.probuse@gmail.com>
2016-08-11 12:22:41 +0530

    [IMP]pabi_source_document: improved code working with multi supplier invoice from expense ref:issues/949

--- pabi_source_document/models/hr_expense.py
+++ pabi_source_document/models/hr_expense.py
@@ -5,12 +5,11 @@ from openerp import models, api
 class HRExpenseExpense(models.Model):
     _inherit = 'hr.expense.expense'
 
-    @api.multi
-    def _create_supplier_invoice_from_expense(self, merge_line=False):
-        invoice = super(HRExpenseExpense, self).\
-            _create_supplier_invoice_from_expense()
-        if invoice:
-            for expense in self:
-                invoice.write({'source_document_id': '%s,%s'
+
+    @api.model
+    def _prepare_inv_header(self, partner_id, expense):
+        invoice_vals = super(HRExpenseExpense, self).\
+            _prepare_inv_header(partner_id, expense)
+        invoice_vals.update({'source_document_id': '%s,%s'
                                % (self._model, expense.id)})
-        return invoice
+        return invoice_vals
