6a8d0cc6ef2d3938c17f750f77fb652790511ec9
siraparn <siraparn@trinityroots.co.th>
2016-08-16 13:30:49 +0700

    [ENH] update form

--- pabi_purchase_work_acceptance/wizard/po_line_invoice.py
+++ pabi_purchase_work_acceptance/wizard/po_line_invoice.py
@@ -58,4 +58,6 @@ class PurchaseLineInvoice(models.TransientModel):
             WAcceptance = self.env['purchase.work.acceptance']
             acceptance = WAcceptance.browse(active_id)
             acceptance.invoice_created = True
+            for wa_line in acceptance.acceptance_line_ids:
+                wa_line,line_id.invoice_id
         return res
