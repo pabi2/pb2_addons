2171245e842539715955ee3c8c0ec6f02252f05d
Probuse <kaushik.probuse@gmail.com>
2016-08-24 16:24:44 +0530

    [ADD]pabi_th_vat_report: Added new module to add taxbranch in vat report ref: issues/396

---
+++ pabi_th_vat_report/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "NSTDA :: PABI2 - Vat Report Extended",
+    "summary": "",
+    "version": "1.0",
+    "category": "",
+    "description": """
+Vat Report By TaxBranch
+    """,
+    "website": "https://ecosoft.co.th/",
+    "author": "Kitti U.",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        'l10n_th_vat_report',
+    ],
+    "data": [
+        "report/vat_report_view.xml",
+    ],
+}
