d47db311061b79725e60d8e9c128f1f62fc53b21
Probuse <kaushik.probuse@gmail.com>
2016-09-13 15:16:39 +0800

    [FIX]pabi_chartfield: fix issue with dynamic domain on project and section on stock request ref: issues/980

--- pabi_chartfield/models/stock_request.py
+++ pabi_chartfield/models/stock_request.py
@@ -27,7 +27,7 @@ class ResProject(models.Model):
     _inherit = 'res.project'
 
     @api.model
-    def search(self, args, offset=0, limit=None, order=None, count=False):
+    def name_search(self, name='', args=None, operator='ilike', limit=100):
         # Find matched project manager's projects
         project_manager_emp_id = self._context.get('project_manager_emp_id',
                                                    False)
@@ -38,24 +38,55 @@ class ResProject(models.Model):
                                   ])
             if projects:
                 args += [('id', 'in', projects._ids)]
-        return super(ResProject, self).search(args, offset=offset,
-                                              limit=limit, order=order,
-                                              count=count)
+        return super(ResProject, self).name_search(name=name,
+                                                   args=args,
+                                                   operator=operator,
+                                                   limit=limit)
+
+#     @api.model
+#     def search(self, args, offset=0, limit=None, order=None, count=False):
+#         # Find matched project manager's projects
+#         project_manager_emp_id = self._context.get('project_manager_emp_id',
+#                                                    False)
+#         if project_manager_emp_id:
+#             PM = self.env['res.project.member']
+#             projects = PM.search([('project_position', '=', 'manager'),
+#                                   ('employee_id', '=',project_manager_emp_id)
+#                                   ])
+#             if projects:
+#                 args += [('id', 'in', projects._ids)]
+#         return super(ResProject, self).search(args, offset=offset,
+#                                               limit=limit, order=order,
+#                                               count=count)
 
 
 class ResSection(models.Model):
     _inherit = 'res.section'
 
     @api.model
-    def search(self, args, offset=0, limit=None, order=None, count=False):
+    def name_search(self, name='', args=None, operator='ilike', limit=100):
         # Find all section under the division of this requester
-        employee_id = self._context.get('employee_id',
-                                        False)
+        employee_id = self._context.get('employee_id', False)
         if employee_id:
             employee = self.env['hr.employee'].browse(employee_id)
             division_id = employee.section_id.division_id.id
             if division_id:
                 args += [('division_id', '=', division_id)]
-        return super(ResSection, self).search(args, offset=offset,
-                                              limit=limit, order=order,
-                                              count=count)
+        return super(ResSection, self).name_search(name=name,
+                                                   args=args,
+                                                   operator=operator,
+                                                   limit=limit)
+
+#     @api.model
+#     def search(self, args, offset=0, limit=None, order=None, count=False):
+#         # Find all section under the division of this requester
+#         employee_id = self._context.get('employee_id',
+#                                         False)
+#         if employee_id:
+#             employee = self.env['hr.employee'].browse(employee_id)
+#             division_id = employee.section_id.division_id.id
+#             if division_id:
+#                 args += [('division_id', '=', division_id)]
+#         return super(ResSection, self).search(args, offset=offset,
+#                                               limit=limit, order=order,
+#                                               count=count)
