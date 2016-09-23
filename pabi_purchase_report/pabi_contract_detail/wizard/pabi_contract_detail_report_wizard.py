fd6eac8389b161c0e3434eac21d235a34adac130
siraparn <siraparn@trinityroots.co.th>
2016-09-23 17:35:59 +0700

    [ADD] Init PABI Purchase Report

---
+++ pabi_purchase_report/pabi_contract_detail/wizard/pabi_contract_detail_report_wizard.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from openerp import models, fields, api
+
+
+class PabiContractDetailReportWizard(models.TransientModel):
+
+    _name = 'pabi.contract.detail.report.wizard'
+
+    purchase_type_id = fields.Many2one(
+        'purchase.type',
+        string='Purchase Type',
+    )
+    purchase_method_id = fields.Many2one(
+        'purchase.method',
+        string='Purchase Type',
+    )
+    date_from = fields.Date(string='Contract Start Date')
+    date_to = fields.Date(string='Contract End Date')
+    format = fields.Selection(
+        [('pdf', 'PDF'),
+         ('xls', 'Excel')],
+        string='Format',
+        required=True,
+        default='pdf',
+    )
+
+    @api.multi
+    def run_report(self):
+        data = {'parameters': {}}
+        report_name = self.format == 'pdf' and 'pabi_contract_detail_report' or \
+            'pabi_contract_detail_report_xls'
+        # For SQL, we search simply pass params
+        data['parameters']['purchase_type'] = self.purchase_type_id.id
+        data['parameters']['purchase_method'] = self.purchase_method_id.id
+        data['parameters']['date_from'] = self.date_from
+        data['parameters']['date_to'] = self.date_to
+        res = {
+            'type': 'ir.actions.report.xml',
+            'report_name': report_name,
+            'datas': data,
+        }
+        return res
