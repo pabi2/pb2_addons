00820272bfee07cca5737c03206eb14deccf7a7b
Kitti U <kittiu@gmail.com>
2016-09-28 16:28:34 +0700

    [ENH] pabi_account, add option "Show Account" for invoice.

--- pabi_account/__openerp__.py
+++ pabi_account/__openerp__.py
@@ -28,5 +28,6 @@
         "views/account_config.xml",
         "views/account_voucher_view.xml",
         "views/voucher_payment_receipt_view.xml",
+        "views/account_invoice_view.xml",
     ],
 }
