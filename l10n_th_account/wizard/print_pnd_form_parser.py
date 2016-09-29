f36bfc718cf0194339f39c36ce885e13850cdd9e
Kitti U <kittiu@gmail.com>
2016-09-29 10:13:49 +0700

    [ADD] account_bank_receipt, clone from account_check_deposit

--- l10n_th_account/wizard/print_pnd_form_parser.py
+++ l10n_th_account/wizard/print_pnd_form_parser.py
@@ -15,7 +15,7 @@ def report_pnd53_form_parser(cr, uid, ids, data, context):
 
 jasper_reports.report_jasper(
     'report.report_pnd53_form',  # report_name in report_data.xml
-    'report.pnd53.form',  # Model View name
+    'account.voucher',  # Model View name
     parser=report_pnd53_form_parser,
 )
 
@@ -33,6 +33,6 @@ def report_pnd3_form_parser(cr, uid, ids, data, context):
 
 jasper_reports.report_jasper(
     'report.report_pnd3_form',  # report_name in report_data.xml
-    'report.pnd3.form',  # Model View name
+    'account.voucher',  # Model View name
     parser=report_pnd3_form_parser,
 )
--- l10n_th_account/wizard/print_pnd_form_wizard.py
+++ l10n_th_account/wizard/print_pnd_form_wizard.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from openerp import models, fields, api, _
-from openerp.exceptions import ValidationError
+from openerp.exceptions import ValidationError, Warning as UserError
 from openerp.addons.l10n_th_account.models.res_partner \
     import INCOME_TAX_FORM
 
@@ -30,14 +30,17 @@ class PrintPNDFormWizard(models.TransientModel):
             report_name = 'report_pnd3_form'
         if not report_name:
             raise ValidationError(_('Selected form not found!'))
-        # For ORM, we search for ids, and only pass ids to parser and jasper
+
         domain = [
             ('income_tax_form', '=', self.income_tax_form),
             ('wht_period_id', '=', self.calendar_period_id.period_id.id),
+            ('wht_sequence', '>', 0),
         ]
-        ids = self.env['report.pnd.form'].search(domain)._ids
+        ids = self.env['account.voucher'].search(domain)._ids
         data['parameters']['ids'] = ids
         company = self.env.user.company_id.partner_id
+        if not company.vat:
+            raise UserError(_('No Tax ID on Company'))
         company_taxid = len(company.vat) == 13 and company.vat or ''
         company_branch = \
             len(company.taxbranch) == 5 and company.taxbranch or ''
@@ -48,5 +51,4 @@ class PrintPNDFormWizard(models.TransientModel):
             'report_name': report_name,
             'datas': data,
         }
-        print res
         return res
