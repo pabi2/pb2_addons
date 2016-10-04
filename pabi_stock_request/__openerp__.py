2fbcf321736edc5b7bea6aa7f6c6ddd328df4dfc
siraparn <siraparn@trinityroots.co.th>
2016-10-04 20:21:49 +0700

    [ADD] pabi_stock_request

---
+++ pabi_stock_request/__openerp__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "NSTDA :: PABI2 - Stock Request",
+    "summary": "Pabi2 Procurement",
+    "version": "8.0.1.0.0",
+    "category": "Purchase",
+    "description": """
+
+PABI2 - Purchase Stock Request
+================================
+
+  * Change some field's action and input.
+    """,
+    "website": "https://nstda.or.th/",
+    "author": "TrinityRoots",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "stock",
+        "pabi_procurement",
+    ],
+    "data": [
+        "views/stock_request_view.xml",
+    ],
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
