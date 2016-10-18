7deef6eafc252a6d6941c9d34f25cf9a8ba96f52
mr.jakkrich <mr.jakkrich@gmail.com>
2016-10-18 14:26:31 +0700

    [ADD] nstdaauth_cas for sso

---
+++ nstdaauth_cas/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "NSTDA-BASE :: Authentication CAS",
+    'description': """NSTDA CAS Authentication""",
+    'author': 'Jakkrich.cha',
+    'category': 'NSTDA-BASE',
+    'depends': ['base', 'base_setup', 'web'],
+    'data': [
+            'static/src/views/nstdaauth_cas.xml',
+            'res_config_view.xml',
+            'ir_ui_menu_view.xml'
+    ],
+    'qweb': [
+        "static/src/xml/nstdaauth_cas.xml"
+    ],
+    'installable': True,
+    'external_dependencies': {
+        'python': ['ldap'],
+    }
+}
