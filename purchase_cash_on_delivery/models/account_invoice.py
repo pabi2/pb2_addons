aee0f76b6ddc20e311958547a727101165c12067
Kitti U <kittiu@gmail.com>
2016-10-25 18:05:42 +0700

    -

--- purchase_cash_on_delivery/models/account_invoice.py
+++ purchase_cash_on_delivery/models/account_invoice.py
@@ -8,6 +8,9 @@ class AccountInvoiceLine(models.Model):
     @api.model
     def move_line_get_item(self, line):
         res = super(AccountInvoiceLine, self).move_line_get_item(line)
-        prepaid_account_id = self.env.user.company_id.prepaid_account_id.id
-        res.update({'account_id': prepaid_account_id})
+        cod_pay_term = self.env.ref('purchase_cash_on_delivery.'
+                                    'cash_on_delivery_payment_term')
+        if line.invoice_id.payment_term == cod_pay_term:
+            prepaid_account_id = self.env.user.company_id.prepaid_account_id.id
+            res.update({'account_id': prepaid_account_id})
         return res
