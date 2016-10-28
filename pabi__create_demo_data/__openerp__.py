ac56b4d4250ec98de0e0b15f594cc58a2e705c74
Kitti U <kittiu@gmail.com>
2016-10-28 18:59:55 +0700

    -

---
+++ pabi__create_demo_data/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "NSTDA :: PABI2 - Demo Data",
+    "summary": "",
+    "version": "1.0",
+    "category": "Tools",
+    "description": """
+
+
+    """,
+    "website": "https://ecosoft.co.th/",
+    "author": "Kitti U.",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "purchase",
+    ],
+    "data": [
+    ],
+}
