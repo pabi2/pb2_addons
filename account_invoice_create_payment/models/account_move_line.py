8ccb9ce92e725160511edef58b477e66ec921e3d
Kitti U <kittiu@gmail.com>
2016-11-24 10:07:37 +0700

    [FIX] #1276

--- account_invoice_create_payment/models/account_move_line.py
+++ account_invoice_create_payment/models/account_move_line.py
@@ -9,7 +9,10 @@ class AccountMoveLine(models.Model):
     def search(self, args, offset=0, limit=None, order=None, count=False):
         if self._context.get('filter_by_invoice_ids'):
             _ids = self._context.get('filter_by_invoice_ids')
-            args += [('invoice', 'in', _ids)]
+            args += ['|', ('invoice', 'in', _ids),  # For selected invoices
+                     ('journal_id.type', 'not in',  # For non invoice moves
+                      ['sale', 'sale_refund', 'sale_debitnote',
+                       'purchase', 'purchase_refund', 'purchase_debitnote'])]
         return super(AccountMoveLine, self).search(args, offset=offset,
                                                    limit=limit, order=order,
                                                    count=count)
