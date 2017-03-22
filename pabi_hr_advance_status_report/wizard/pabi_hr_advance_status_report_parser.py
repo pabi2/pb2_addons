e25ed2ba4178f9cb6d697138f65a2be32e668f9b
Kitti U <kittiu@gmail.com>
2017-03-22 22:16:41 +0700

    [REF] clean up code.

--- pabi_hr_advance_status_report/wizard/pabi_hr_advance_status_report_parser.py
+++ pabi_hr_advance_status_report/wizard/pabi_hr_advance_status_report_parser.py
@@ -6,26 +6,23 @@ def pabi_hr_advance_status_report_parser(cr, uid, ids, data, context):
     # For ORM, just pass ids
     return {
         'ids': data['parameters']['ids'],
-        'parameters': {
-            'xxx': data['parameters']['ids'],
-        }
     }
 
 
 jasper_reports.report_jasper(
-    'report.pabi_hr_advance_status_report', # report_name in report_data.xml
+    'report.pabi_hr_advance_status_report',
     'hr.expense.expense',
     parser=pabi_hr_advance_status_report_parser,
 )
 
 jasper_reports.report_jasper(
-    'report.pabi_hr_advance_status_summary_report', # report_name in report_data.xml
+    'report.pabi_hr_advance_status_summary_report',
     'hr.expense.expense',
     parser=pabi_hr_advance_status_report_parser,
 )
 
 jasper_reports.report_jasper(
-    'report.pabi_hr_advance_status_detail_report', # report_name in report_data.xml
+    'report.pabi_hr_advance_status_detail_report',
     'hr.expense.expense',
     parser=pabi_hr_advance_status_report_parser,
 )
--- pabi_hr_advance_status_report/wizard/pabi_hr_advance_status_report_wizard.py
+++ pabi_hr_advance_status_report/wizard/pabi_hr_advance_status_report_wizard.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 from openerp import models, fields, api
-
 import time
 
-class PabiHrAdvanceStatusReportWizard(models.TransientModel):
 
+class PabiHrAdvanceStatusReportWizard(models.TransientModel):
     _name = 'pabi.hr.advance.status.report.wizard'
 
-    run_date = fields.Date(string="Run Date", default=time.strftime("%Y-%m-%d"))
-
+    run_date = fields.Date(
+        string='Run Date',
+        default=lambda self: fields.Date.context_today(self),
+    )
     report_type = fields.Selection(
         [('status', 'Status'),
          ('summary', 'Summary'),
@@ -22,16 +23,16 @@ class PabiHrAdvanceStatusReportWizard(models.TransientModel):
     def run_report(self):
         data = {'parameters': {}}
         Report = self.env['hr.expense.expense']
-
-        report_name = 'pabi_hr_advance_status_report'
-        ids = Report.search([('state', '=', 'paid'),('amount_to_clearing', '>', 0.0)],
-                            order='operating_unit_id, employee_id, invoice_id')._ids
-
-        if self.report_type == 'summary':
+        report_name = False
+        ids = Report.search(
+            [('state', '=', 'paid'), ('amount_to_clearing', '>', 0.0)],
+            order='operating_unit_id, employee_id, invoice_id')._ids
+        if self.report_type == 'status':
+            report_name = 'pabi_hr_advance_status_report'
+        elif self.report_type == 'summary':
             report_name = 'pabi_hr_advance_status_summary_report'
         elif self.report_type == 'detail':
             report_name = 'pabi_hr_advance_status_detail_report'
-
         data['parameters']['ids'] = ids
         res = {
             'type': 'ir.actions.report.xml',
