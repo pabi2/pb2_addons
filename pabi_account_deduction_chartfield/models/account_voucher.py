bea2dca96415e65291e713a331c8c0f0f7a33efc
Kitti U <kittiu@gmail.com>
2017-03-27 17:53:04 +0700

    [ENH] validation on Payment Difference, AG/A vs Account

--- pabi_account_deduction_chartfield/models/account_voucher.py
+++ pabi_account_deduction_chartfield/models/account_voucher.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
-from openerp import models, api
+from openerp import models, api, _
 from openerp.addons.account_budget_activity.models.account_activity \
     import ActivityCommon
 from openerp.addons.pabi_chartfield.models.chartfield \
     import ChartFieldAction
+from openerp.exceptions import ValidationError
 
 
 class AccuontVoucherMultipleReconcile(ActivityCommon,
@@ -16,6 +17,17 @@ class AccuontVoucherMultipleReconcile(ActivityCommon,
         res = super(AccuontVoucherMultipleReconcile, self).create(vals)
         res.update_related_dimension(vals)
         return res
+
+    @api.one
+    @api.constrains('activity_group_id', 'activity_id', 'account_id')
+    def _check_account_activity(self):
+        report_type = self.account_id.user_type.report_type
+        if report_type in ('asset', 'liability') and \
+                (self.activity_group_id or self.activity_id):
+            raise ValidationError(_('AG/A is not required for Balance Sheet'))
+        if report_type not in ('asset', 'liability') and \
+                not (self.activity_group_id and self.activity_id):
+            raise ValidationError(_('AG/A is required for Non-Balance Sheet'))
 
 
 class AccountVoucher(models.Model):
