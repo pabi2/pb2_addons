344305e76ad9c46ab611c9510ab5e1680d1510de
Probuse <kaushik.probuse@gmail.com>
2017-03-30 17:31:23 +0530

    [ADD]account_bank_receipt_deduction: added new module for bank receipt write off entry refissues/1433

---
+++ account_bank_receipt_deduction/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Account Bank Receipt Deduction',
+    'version': '8.0.0.1.0',
+    'category': 'Accounting & Finance',
+    'license': 'AGPL-3',
+    'summary': 'Manage trasfer from intransit account the bank deduction',
+    'author': "Ecosoft",
+    'website': 'http://www.ecosoft.co.th/',
+    'depends': [
+        'account_bank_receipt',
+    ],
+    'data': [
+        'views/account_bank_receipt_view.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
