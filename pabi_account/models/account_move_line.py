42fd1a0d47284a7984de6fc679bd801fd56943f1
Kitti U <kittiu@gmail.com>
2017-04-22 19:24:10 +0700

    [ENH] move sorting order of move line to view level

--- pabi_account/models/account_move_line.py
+++ pabi_account/models/account_move_line.py
@@ -4,8 +4,6 @@ from openerp import models, api
 
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
-    # Change order debit first then credit. Can't do much as it affect perf
-    _order = 'move_id, debit desc, credit desc , account_id'
 
     @api.model
     def _update_analytic_dimension(self, vals):
