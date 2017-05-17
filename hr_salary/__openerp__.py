87e7b9ff6f7ff3e28c4ed129f5ef1f4a857ddf02
Kitti U <kittiu@gmail.com>
2017-05-17 11:33:15 +0700

    Init

---
+++ hr_salary/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "HR Salary Expense",
+    "version": "8.0.0.1.0",
+    "license": 'AGPL-3',
+    "author": "Ecosoft",
+    "category": "Accounting & Finance",
+    "depends": [
+        "hr",
+        "account_accountant",
+    ],
+    "description": """
+
+    """,
+    "data": [
+        "views/hr_salary_view.xml",
+    ],
+    'installable': True,
+}
