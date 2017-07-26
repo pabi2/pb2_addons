82912ea3f668b0f2aeace17773ca0bd4328eb2bf
Kitti U <kittiu@gmail.com>
2017-07-26 10:00:15 +0700

    Small UI enh for budget plan and control

--- pabi_budget_plan/models/account_budget.py
+++ pabi_budget_plan/models/account_budget.py
@@ -7,20 +7,10 @@ class AccountBudget(models.Model):
     _inherit = 'account.budget'
     _order = 'create_date desc'
 
-    # prev_planned_amount = fields.Float(
-    #     string='Planned Amount',
-    #     readonly=True,
-    # )
     policy_amount = fields.Float(
         string='Policy Amount',
         readonly=True,
     )
-    # ref_breakdown_id = fields.Many2one(
-    #     'budget.fiscal.policy.breakdown',
-    #     string="Breakdown Reference",
-    #     copy=True,
-    #     readonly=True,
-    # )
 
     @api.multi
     def _get_doc_number(self):
@@ -92,25 +82,6 @@ class AccountBudget(models.Model):
                 raise ValidationError(
                     _('Duplicated budget on the same fiscal year!'))
 
-    # @api.multi
-    # def budget_confirm(self):
-    #     for rec in self:
-    #        name = self.env['ir.sequence'].next_by_code('budget.control.unit')
-    #         rec.write({'name': name})
-    #         # rec.ref_budget_id.budget_cancel()
-    #     return super(AccountBudget, self).budget_confirm()
-
-    # # New Revision
-    # @api.multi
-    # def new_minor_revision(self):
-    #     result = super(AccountBudget, self).new_minor_revision()
-    #     if result.get('domain', []):
-    #         new_budget_id = result['domain'][0][2]
-    #         new_budget = self.browse(new_budget_id)
-    #         new_budget.ref_budget_id = self.id
-    #         new_budget.name = '/'
-    #     return result
-
     @api.multi
     def unlink(self):
         for policy in self:
@@ -119,35 +90,3 @@ class AccountBudget(models.Model):
                     _('Cannot delete budget(s)\
                     which are not in draft or cancelled.'))
         return super(AccountBudget, self).unlink()
-
-    # @api.multi
-    # def get_all_version(self):
-    #     self.ensure_one()
-    #     budget_ids = []
-    #     if self.ref_budget_id:
-    #         budget = self.ref_budget_id
-    #         while budget:
-    #             budget_ids.append(budget.id)
-    #             budget = budget.ref_budget_id
-    #     budget = self
-    #     while budget:
-    #         ref_budget =\
-    #             self.search([('ref_budget_id', '=', budget.id)])
-    #         if ref_budget:
-    #             budget_ids.append(ref_budget.id)
-    #         budget = ref_budget
-    #     act = 'account_budget_activity.act_account_budget_view'
-    #     action = self.env.ref(act)
-    #     result = action.read()[0]
-    #     dom = [('id', 'in', budget_ids)]
-    #     result.update({'domain': dom})
-    #     return result
-
-
-class AccountBudgetLine(models.Model):
-    _inherit = 'account.budget.line'
-
-    planned_amount = fields.Float(
-        string='Current Amount',  # Existing field, change label only
-        help="Current Planned Amount"
-    )
