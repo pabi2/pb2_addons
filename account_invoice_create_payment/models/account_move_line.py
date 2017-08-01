c8f2b0474a7dd8d54c188a2957cf45205f799429
Kitti U <kittiu@gmail.com>
2017-08-01 17:21:24 +0700

    Fix account_invoice_create_payment, ensure that JE of voucher can display

--- account_invoice_create_payment/models/account_move_line.py
+++ account_invoice_create_payment/models/account_move_line.py
@@ -9,12 +9,11 @@ class AccountMoveLine(models.Model):
     def search(self, args, offset=0, limit=None, order=None, count=False):
         if self._context.get('filter_by_invoice_ids'):
             _ids = self._context.get('filter_by_invoice_ids')
-            # kittiu: Not sure why I did, just keep it for reference
-            # args += ['|', ('invoice', 'in', _ids),  # For selected invoices
-            #          ('journal_id.type', 'not in',  # For non invoice moves
-            #           ['sale', 'sale_refund', 'sale_debitnote',
-            #            'purchase', 'purchase_refund', 'purchase_debitnote'])]
-            args += [('invoice', 'in', _ids)]
+            # Require this args, to ensure voucher's JE can be shown
+            args += ['|', ('invoice', 'in', _ids),  # For selected invoices
+                     ('journal_id.type', 'not in',  # For non invoice moves
+                      ['sale', 'sale_refund', 'sale_debitnote',
+                       'purchase', 'purchase_refund', 'purchase_debitnote'])]
         else:
             args += [(1, '=', 1)]  # don't remove, it require for net pay
         return super(AccountMoveLine, self).search(args, offset=offset,
