f5d8083cfa8495372179621e0febe8fd1d3cdb59
Kitti U <kittiu@gmail.com>
2017-08-15 00:57:27 +0700

    Remove checking that bank account to journal is 1-1
    We no longer use this, as we can hard code bank account in tempalte

--- payment_export/models/account_bank.py
+++ payment_export/models/account_bank.py
@@ -6,10 +6,10 @@ from openerp.exceptions import ValidationError
 class ResPartnerBank(models.Model):
     _inherit = 'res.partner.bank'
 
-    @api.one
-    @api.constrains('journal_id')
-    def _check_journal_id(self):
-        if self.journal_id and len(self.search([('journal_id', '=',
-                                               self.journal_id.id)])._ids) > 1:
-            raise ValidationError(_("An Account Journal can't "
-                                    "be used for more than 1 bank account"))
+    # @api.one
+    # @api.constrains('journal_id')
+    # def _check_journal_id(self):
+    #     if self.journal_id and len(self.search([('journal_id', '=',
+    #                                         self.journal_id.id)])._ids) > 1:
+    #         raise ValidationError(_("An Account Journal can't "
+    #                                 "be used for more than 1 bank account"))
