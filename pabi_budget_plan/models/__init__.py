9882489a626080314b9dbc4ca13e0521e020a26a
newtratip <tharathip.chaweewongphan@gmail.com>
2017-08-29 01:38:30 +0700

    [IMP] add import file

--- pabi_budget_plan/models/__init__.py
+++ pabi_budget_plan/models/__init__.py
@@ -5,5 +5,6 @@ from . import budget_policy
 from . import budget_breakdown
 from . import account_budget
 from . import account
+from . import pabi_xls
 # NOT DONE YET
 # from . import budget_policy_report
