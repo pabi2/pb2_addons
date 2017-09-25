5fa03b8af63b0459b3ba7e983bdad32bd99a1c99
Kitti U <kittiu@gmail.com>
2017-09-25 15:20:11 +0700

    -

--- pabi_account_move_adjustment/wizard/account_use_model.py
+++ pabi_account_move_adjustment/wizard/account_use_model.py
@@ -7,7 +7,6 @@ class AccountUseModel(models.TransientModel):
 
     @api.multi
     def create_entries(self):
-        end_period_date = self.env['account.period'].find().date_stop
-        self = self.with_context(end_period_date=end_period_date)
         res = super(AccountUseModel, self).create_entries()
+        res.pop('views')
         return res
