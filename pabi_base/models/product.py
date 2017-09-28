d4a3d905485f402fe9b00dbe73a116285c96f9cf
Kitti U <kittiu@gmail.com>
2017-09-28 14:18:30 +0700

    Add SAP code into product

--- pabi_base/models/product.py
+++ pabi_base/models/product.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
-from openerp import models
+from openerp import models, fields
 
 
 class ProductProduct(models.Model):
     _inherit = 'product.product'
     _order = 'default_code'  # Remove name sorting for performance improve
+
+    sap_code = fields.Char(
+        string='SAP Code',
+        size=10,
+    )
 
 
 class ProductCategory(models.Model):
