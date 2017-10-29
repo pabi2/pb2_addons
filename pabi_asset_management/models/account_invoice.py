6828807a59de386b702acc7ad56bba5b1c533a39
Kitti U <kittiu@gmail.com>
2017-10-29 17:50:56 +0700

    [ENH] add smart button in invoice for asset adjustment

--- pabi_asset_management/models/account_invoice.py
+++ pabi_asset_management/models/account_invoice.py
@@ -1,6 +1,28 @@
 # -*- coding: utf-8 -*-
-from openerp import models, api, _
+from openerp import models, api, fields, _
 from openerp.exceptions import ValidationError
+
+
+class AccountInvoice(models.Model):
+    _inherit = 'account.invoice'
+
+    asset_adjust_id = fields.Many2one(
+        'account.asset.adjust',
+        string='Asset Adjustment',
+        readonly=True,
+        copy=False,
+    )
+
+    @api.multi
+    def action_open_asset_adjust(self):
+        self.ensure_one()
+        action = self.env.ref('pabi_asset_management.'
+                              'action_account_asset_adjust')
+        if not action:
+            raise ValidationError(_('No Action'))
+        res = action.read([])[0]
+        res['domain'] = [('id', '=', self.asset_adjust_id.id)]
+        return res
 
 
 class AccountInvoiceLine(models.Model):
