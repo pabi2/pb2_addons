9d6a9cf0f4c3fbb8652c656c551d78b24825f4e4
newtratip <tharathip.chaweewongphan@gmail.com>
2017-10-31 17:29:23 +0700

    [IMP] Add Config Field in HR Config

---
+++ pabi_advance_dunning_letter/models/res_config.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from openerp import models, fields
+
+
+class HRConfigSettings(models.TransientModel):
+    _inherit = 'hr.config.settings'
+
+    group_email = fields.Char(
+        string='Group Email',
+        related='company_id.group_email',
+    )
+    head_account_employee_id = fields.Many2one(
+        'hr.employee',
+        string='Head Accounting',
+        related='company_id.head_account_employee_id',
+    )
