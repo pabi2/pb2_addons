7926529c66072f1dc989b457e2e2bfb8110655e9
Kitti U <kittiu@gmail.com>
2017-11-03 17:00:29 +0700

    [ENH] long term invest reort, Add function field ref_payments

--- pabi_long_term_investment_report/models/account_move_line.py
+++ pabi_long_term_investment_report/models/account_move_line.py
@@ -12,3 +12,13 @@ class AccountMoveLine(models.Model):
         ondelete='restrict',
         readonly=True,
     )
+    ref_payments = fields.Char(
+        string='Ref Payments',
+        compute='_compute_ref_payments',
+    )
+
+    @api.multi
+    def _compute_ref_payments(self):
+        for rec in self:
+            ref_payments = rec.invoice.payment_ids.mapped('ref')
+            rec.ref_payments = ', '.join(ref_payments)
