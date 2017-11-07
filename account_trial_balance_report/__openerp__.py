124e1c33112e8414bb70f983ae7f7807baae38e0
Kitti U <kittiu@gmail.com>
2017-11-07 18:54:01 +0700

    Trial Balance Report

---
+++ account_trial_balance_report/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Trial Balance (web)',
+    'summary': '',
+    'version': '8.0.1.0.0',
+    'category': 'Accounting & Finance',
+    'description': """
+
+    """,
+    'website': 'https://ecosoft.co.th/',
+    'author': 'Kitti U.',
+    'license': 'AGPL-3',
+    'application': False,
+    'installable': True,
+    'depends': [
+        'account',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'wizard/trial_balance_wizard.xml',
+        'views/trial_balance_report.xml',
+    ],
+}
---
+++ account_trial_balance_report/wizard/trial_balance_wizard.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api, _
+from openerp.exceptions import ValidationError
+
+
+class AccountTrialBalanceWizard(models.TransientModel):
+    _name = 'account.trial.balance.wizard'
+
+    fiscalyear_id = fields.Many2one(
+        'account.fiscalyear',
+        string='Fiscal Year',
+        default=lambda self: self.env['account.fiscalyear'].find(),
+        required=True,
+    )
+    filter = fields.Selection(
+        [('filter_no', 'No Filters'),
+         ('filter_date', 'Date'),
+         ('filter_period', 'Periods')],
+        string='Filter by',
+        default='filter_no',
+        required=True
+    )
+    period_start = fields.Many2one(
+        'account.period',
+        string='Start Period',
+        domain="[('fiscalyear_id', '=', fiscalyear_id)]",
+    )
+    period_stop = fields.Many2one(
+        'account.period',
+        string='End Period',
+        domain="[('fiscalyear_id', '=', fiscalyear_id)]",
+    )
+    date_start = fields.Date(
+        string='Start Date',
+    )
+    date_stop = fields.Date(
+        string='End Date',
+    )
+    target_move = fields.Selection(
+        [('posted', 'All Posted Entries'),
+         ('all', 'All Entries')],
+        string='Target Moves',
+        required=True,
+        default='posted',
+    )
+    with_movement = fields.Boolean(
+        string='With Movement',
+        default=True,
+    )
+
+    @api.onchange('fiscalyear_id', 'filter')
+    def _onchange_fiscalyear_id(self):
+        self.date_start = self.fiscalyear_id.date_start
+        self.date_stop = self.fiscalyear_id.date_stop
+        self.period_start = self.env['account.period'].find(dt=self.date_start)
+        self.period_stop = self.env['account.period'].find(dt=self.date_stop)
+
+    @api.onchange('period_start')
+    def _onchange_period_start(self):
+        self.date_start = self.period_start.date_start
+
+    @api.onchange('period_stop')
+    def _onchange_period_stop(self):
+        self.date_stop = self.period_stop.date_stop
+
+    @api.multi
+    def run_report(self):
+        self.ensure_one()
+        TB = self.env['account.trial.balance.report']
+        report_id = TB.generate_report(self.fiscalyear_id.id,
+                                       self.date_start,
+                                       self.date_stop,
+                                       self.target_move,
+                                       self.with_movement)
+        action = self.env.ref('account_trial_balance_report.'
+                              'action_account_trial_balance_report')
+        result = action.read()[0]
+        result.update({'res_id': report_id,
+                       'domain': [('id', '=', report_id)]})
+        return result
