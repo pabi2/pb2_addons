944f42bf0face9457c043dbe8c1210e422848994
Kitti U <kittiu@gmail.com>
2017-11-13 21:20:53 +0700

    Fix the way stock_account works for chartfield

--- account_budget_activity/models/stock_account.py
+++ account_budget_activity/models/stock_account.py
@@ -12,13 +12,17 @@ class StockQuant(models.Model):
             _prepare_account_move_line(move, qty, cost,
                                        credit_account_id, debit_account_id)
         Analytic = self.env['account.analytic.account']
-        # dimensions = Analytic._analytic_dimensions()
-        # for d in dimensions:
-        #     for r in res:
-        #         r[2].update({d: move[d].id})
-        # Find analytic id (so that, it can consume budget)
+        # Profit & Loss account of this product
+        bs_accounts = [
+            move.product_id.property_stock_account_input.id,
+            move.product_id.property_stock_account_output.id,
+            move.product_id.categ_id.property_stock_account_input_categ.id,
+            move.product_id.categ_id.property_stock_account_output_categ.id,
+        ]
         for r in res:
-            analytic_account = Analytic.create_matched_analytic(move)
-            if analytic_account:
-                r[2]['analytic_account_id'] = analytic_account.id
+            # For non B&L, we assign analytic
+            if r[2]['account_id'] not in bs_accounts:
+                analytic_account = Analytic.create_matched_analytic(move)
+                if analytic_account:
+                    r[2]['analytic_account_id'] = analytic_account.id
         return res
--- pabi_asset_management/models/stock_account.py
+++ pabi_asset_management/models/stock_account.py
@@ -14,12 +14,6 @@ class StockQuant(models.Model):
         res = super(StockQuant, self).\
             _prepare_account_move_line(move, qty, cost,
                                        credit_account_id, debit_account_id)
-        # For PABI2 case, we use different approach in determining asset_categ
-        # As such, we need to clear it first and reassign again.
-        for r in res:
-            r[2].update({'asset_profile_id': False,
-                         'analytic_account_id': False, })
-        # If stock valuation account used, it means this is an asset
         asset_profile_id = move.product_id.asset_profile_id.id
         stock_account_id = move.product_id.stock_valuation_account_id.id
         for r in res:
@@ -28,11 +22,4 @@ class StockQuant(models.Model):
                 r[2]['stock_move_id'] = move.id
                 r[2]['parent_asset_id'] = \
                     move.purchase_line_id.parent_asset_id.id
-        # Recalculate analytic
-        Analytic = self.env['account.analytic.account']
-        for r in res:
-            if r[2]['asset_profile_id']:
-                analytic_account = Analytic.create_matched_analytic(move)
-                if analytic_account:
-                    r[2]['analytic_account_id'] = analytic_account.id
         return res
