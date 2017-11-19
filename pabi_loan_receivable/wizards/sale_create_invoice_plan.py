31ca17cf9e8ff480a7e0921278cb96d8ebf42766
Kitti U <kittiu@gmail.com>
2017-11-19 11:52:06 +0700

    Fix pabi_loan_receivable, OU mismatch error

--- pabi_loan_receivable/wizards/sale_create_invoice_plan.py
+++ pabi_loan_receivable/wizards/sale_create_invoice_plan.py
@@ -13,23 +13,36 @@ class sale_create_invoice_plan(models.TransientModel):
         installments = super(sale_create_invoice_plan, self)._onchange_plan()
         # Case Loan Agreement
         loan_agreement_id = self._context.get('loan_agreement_id', False)
+        install_amount = self._context.get('loan_install_amount', False)
         if not loan_agreement_id:
             return installments
+
         # Auto fill installment, based on
         # 1) first date 2) amount 3) num_installment
         order_id = self._context['active_id']
         order = self.env['sale.order'].browse(order_id)
         first_date = datetime.strptime(order.date_order,
                                        '%Y-%m-%d %H:%M:%S').date()
-        avg_amount = self.num_installment and \
-            (self.order_amount / self.num_installment) or 0.0
-        avg_percent = self.num_installment and \
-            (100.0 / self.num_installment) or 0.0
-        i = 0
+        avg_amount = install_amount or \
+            (self.num_installment and
+             (self.order_amount / self.num_installment) or 0.0)
+
+        i = 1
+        accum_amount = 0.0
+        accum_percent = 0.0
+        num_install = len(installments)
         for install in installments:
             next_date = first_date + relativedelta(months=i)
             install.date_invoice = next_date.strftime('%Y-%m-%d')
-            install.amount = avg_amount
-            install.percent = avg_percent
+            remain_amount = self.order_amount - accum_amount
+            if i == num_install or remain_amount < avg_amount:  # Final round
+                install.amount = remain_amount
+                install.percent = 100.0 - accum_percent
+            else:
+                # Decimal will interact with each other.
+                install.amount = avg_amount
+                install.percent = (install.amount / self.order_amount) * 100.0
+            accum_amount += install.amount
+            accum_percent += install.percent
             i += 1
         return installments
