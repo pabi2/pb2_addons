62d4e97cce005aece2b3c2601589e006bac62e22
Kitti U <kittiu@gmail.com>
2017-12-21 16:52:30 +0700

    Update budget_cost_centre_report_wizard.py

--- pabi_budget_report/wizards/budget_cost_centre_report_wizard.py
+++ pabi_budget_report/wizards/budget_cost_centre_report_wizard.py
@@ -3,13 +3,6 @@ from openerp import models, fields, api, _
 from openerp.exceptions import except_orm
 
 
-class BudgetCostCentreReportWizardX2(models.Model):
-    _name = 'wizard.budget.report.by.cost.centre'
-    
-class BudgetCostCentreReportWizardX(models.Model):
-    _name = 'budget.report.by.cost.centre.wizard'
-
-
 class BudgetCostCentreReportWizard(models.Model):
     _name = 'budget.cost.centre.report.wizard'
 
