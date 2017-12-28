580d1c8abebe33ae8a79b11225b21f03b4207c29
Kitti U <kittiu@gmail.com>
2017-12-28 16:20:27 +0700

    Refactor code in account_budget_activity

--- account_budget_activity/models/purchase_request.py
+++ account_budget_activity/models/purchase_request.py
@@ -3,45 +3,16 @@ from openerp import api, fields, models, _
 from openerp import tools
 from openerp.exceptions import ValidationError
 from .account_activity import ActivityCommon
+from .budget_commit import CommitCommon
+from .budget_commit import CommitLineCommon
 
 
-class PurchaseRequest(models.Model):
+class PurchaseRequest(CommitCommon, models.Model):
     _inherit = 'purchase.request'
 
-    budget_commit_ids = fields.One2many(
-        'account.analytic.line',
-        'purchase_request_id',
-        string='Budget Commitment',
-        readonly=True,
-    )
-    budget_transition_ids = fields.One2many(
-        'budget.transition',
-        'purchase_request_id',
-        string='Budget Transition',
-        domain=[('source_model', '=', 'purchase.request.line'),
-                '|', ('active', '=', True), ('active', '=', False)],
-        readonly=True,
-    )
-
-    @api.multi
-    def release_all_committed_budget(self):
-        for rec in self:
-            rec.line_ids.release_committed_budget()
-            rec.budget_transition_ids.filtered('active').\
-                write({'active': False})
-
-    @api.multi
-    def recreate_all_budget_commitment(self):
-        """ This method is used for development only """
-        for rec in self:
-            rec.budget_commit_ids.unlink()
-            rec.line_ids._create_analytic_line(reverse=True)
-            rec.budget_transition_ids.\
-                filtered('active').filtered('forward').\
-                return_budget_commitment(['purchase_request_line_id'])
-            rec.budget_transition_ids.\
-                filtered('active').filtered('backward').\
-                regain_budget_commitment(['purchase_request_line_id'])
+    # Extension to budget_transition.py
+    budget_commit_ids = fields.One2many(inverse_name='purchase_request_id')
+    budget_transition_ids = fields.One2many(inverse_name='purchase_request_id')
 
     @api.multi
     def button_to_approve(self):
@@ -74,8 +45,13 @@ class PurchaseRequest(models.Model):
         return super(PurchaseRequest, self).write(vals)
 
 
-class PurchaseRequestLine(ActivityCommon, models.Model):
+class PurchaseRequestLine(CommitLineCommon, ActivityCommon, models.Model):
     _inherit = 'purchase.request.line'
+
+    budget_commit_ids = fields.One2many(
+        inverse_name='purchase_request_line_id')
+    budget_transition_ids = fields.One2many(
+        inverse_name='purchase_request_line_id')
 
     purchased_qty = fields.Float(
         string='Purchased Quantity',
@@ -100,24 +76,26 @@ class PurchaseRequestLine(ActivityCommon, models.Model):
         store=True,
         help="purchase_request_purchase_order_line_rel is a View."
     )
-    budget_commit_ids = fields.One2many(
-        'account.analytic.line',
-        'purchase_request_line_id',
-        string='Budget Commitment',
-        readonly=True,
-    )
-    budget_commit_bal = fields.Float(
-        string='Budget Balance',
-        compute='_compute_budget_commit_bal',
-    )
-    budget_transition_ids = fields.One2many(
-        'budget.transition',
-        'purchase_request_line_id',
-        string='Budget Transition',
-        domain=[('source_model', '=', 'purchase.request.line'),
-                '|', ('active', '=', True), ('active', '=', False)],
-        readonly=True,
-    )
+    # budget_commit_ids = fields.One2many(
+    #     'account.analytic.line',
+    #     'purchase_request_line_id',
+    #     string='Budget Commitment',
+    #     readonly=True,
+    # )
+    # budget_commit_bal = fields.Float(
+    #     string='Budget Balance',
+    #     compute='_compute_budget_commit_bal',
+    # )
+    # budget_transition_ids = fields.One2many(
+    #     'budget.transition',
+    #     'purchase_request_line_id',
+    #     string='Budget Transition',
+    #     domain=[('source_model', '=', 'purchase.request.line'),
+    #             '|', ('active', '=', True), ('active', '=', False)],
+    #     readonly=True,
+    # )
+
+
 
     @api.multi
     def name_get(self):
@@ -129,14 +107,14 @@ class PurchaseRequestLine(ActivityCommon, models.Model):
                               line.name or '-')))
         return result
 
-    @api.model
-    def _get_purchase_lines_view_sql(self):
-        return """
-        select purchase_request_line_id, pol.id as purchase_order_line_id
-        from purchase_request_purchase_requisition_line_rel prq
-        join purchase_order_line pol
-        on pol.requisition_line_id = prq.purchase_requisition_line_id
-        """
+    # @api.model   # kittiu: TOBE DELETED
+    # def _get_purchase_lines_view_sql(self):
+    #     return """
+    #     select purchase_request_line_id, pol.id as purchase_order_line_id
+    #     from purchase_request_purchase_requisition_line_rel prq
+    #     join purchase_order_line pol
+    #     on pol.requisition_line_id = prq.purchase_requisition_line_id
+    #     """
 
     @api.multi
     @api.depends('requisition_lines.purchase_line_ids.pur_line_id')
@@ -146,20 +124,20 @@ class PurchaseRequestLine(ActivityCommon, models.Model):
                 rec.requisition_lines.mapped('purchase_line_ids').\
                 mapped('pur_line_id')
 
-    @api.multi
-    def _compute_budget_commit_bal(self):
-        for rec in self:
-            rec.budget_commit_bal = sum(rec.budget_commit_ids.mapped('amount'))
-
-    @api.multi
-    def release_committed_budget(self):
-        _field = 'purchase_request_line_id'
-        Analytic = self.env['account.analytic.line']
-        for rec in self:
-            aline = Analytic.search([(_field, '=', rec.id)],
-                                    order='create_date desc', limit=1)
-            if aline and rec.budget_commit_bal:
-                aline.copy({'amount': -rec.budget_commit_bal})
+    # @api.multi
+    # def _compute_budget_commit_bal(self):
+    #     for rec in self:
+    #       rec.budget_commit_bal = sum(rec.budget_commit_ids.mapped('amount'))
+    #
+    # @api.multi
+    # def release_committed_budget(self):
+    #     _field = 'purchase_request_line_id'
+    #     Analytic = self.env['account.analytic.line']
+    #     for rec in self:
+    #         aline = Analytic.search([(_field, '=', rec.id)],
+    #                                 order='create_date desc', limit=1)
+    #         if aline and rec.budget_commit_bal:
+    #             aline.copy({'amount': -rec.budget_commit_bal})
 
     @api.multi
     @api.depends('requisition_lines.purchase_line_ids.order_id.state')
@@ -194,69 +172,69 @@ class PurchaseRequestLine(ActivityCommon, models.Model):
     def _price_subtotal(self, line_qty):
         return self.price_unit * line_qty
 
-    @api.multi
-    def _prepare_analytic_line(self, reverse=False, currency=False):
-        self.ensure_one()
-        # general_account_id = self._get_account_id_from_pr_line()
-        general_journal = self.env['account.journal'].search(
-            [('type', '=', 'purchase'),
-             ('company_id', '=', self.company_id.id)], limit=1)
-        if not general_journal:
-            raise Warning(_('Define an accounting journal for purchase'))
-        if not general_journal.is_budget_commit:
-            return False
-        if not general_journal.pr_commitment_analytic_journal_id or \
-                not general_journal.pr_commitment_account_id:
-            raise ValidationError(
-                _("No analytic journal for PR commitment defined on the "
-                  "accounting journal '%s'") % general_journal.name)
-        analytic_journal = general_journal.pr_commitment_analytic_journal_id
-
-        # Pre check, is eligible line
-        Budget = self.env['account.budget']
-        if not Budget.budget_eligible_line(analytic_journal, self):
-            return False
-
-        # Use PR Commitment Account
-        general_account_id = general_journal.pr_commitment_account_id.id
-
-        line_qty = False
-        line_amount = False
-        if 'diff_qty' in self._context:
-            line_qty = self._context.get('diff_qty')
-        elif 'diff_amount' in self._context:
-            line_amount = self._context.get('diff_amount')
-        else:
-            line_qty = self.product_qty
-        if not line_qty and not line_amount:
-            return False
-        price_subtotal = line_amount or self._price_subtotal(line_qty)
-
-        sign = reverse and -1 or 1
-        company_currency = self.env.user.company_id.currency_id
-        currency = currency or company_currency
-        return {
-            'name': self.name,
-            'product_id': False,
-            'account_id': self.analytic_account_id.id,
-            'unit_amount': line_qty,
-            'product_uom_id': False,
-            'amount': currency.compute(sign * price_subtotal,
-                                       company_currency),
-            'general_account_id': general_account_id,
-            'journal_id': analytic_journal.id,
-            'ref': self.request_id.name,
-            'user_id': self._uid,
-            # PR
-            'purchase_request_line_id': self.id,
-            # Fiscal
-            'fiscalyear_id': self.fiscalyear_id.id,
-        }
-
-    @api.multi
-    def _create_analytic_line(self, reverse=False):
-        for rec in self:
-            vals = rec._prepare_analytic_line(
-                reverse=reverse, currency=rec.request_id.currency_id)
-            if vals:
-                self.env['account.analytic.line'].sudo().create(vals)
+    # @api.multi
+    # def _prepare_analytic_line(self, reverse=False, currency=False):
+    #     self.ensure_one()
+    #     # general_account_id = self._get_account_id_from_pr_line()
+    #     general_journal = self.env['account.journal'].search(
+    #         [('type', '=', 'purchase'),
+    #          ('company_id', '=', self.company_id.id)], limit=1)
+    #     if not general_journal:
+    #         raise Warning(_('Define an accounting journal for purchase'))
+    #     if not general_journal.is_budget_commit:
+    #         return False
+    #     if not general_journal.pr_commitment_analytic_journal_id or \
+    #             not general_journal.pr_commitment_account_id:
+    #         raise ValidationError(
+    #             _("No analytic journal for PR commitment defined on the "
+    #               "accounting journal '%s'") % general_journal.name)
+    #     analytic_journal = general_journal.pr_commitment_analytic_journal_id
+    #
+    #     # Pre check, is eligible line
+    #     Budget = self.env['account.budget']
+    #     if not Budget.budget_eligible_line(analytic_journal, self):
+    #         return False
+    #
+    #     # Use PR Commitment Account
+    #     general_account_id = general_journal.pr_commitment_account_id.id
+    #
+    #     line_qty = False
+    #     line_amount = False
+    #     if 'diff_qty' in self._context:
+    #         line_qty = self._context.get('diff_qty')
+    #     elif 'diff_amount' in self._context:
+    #         line_amount = self._context.get('diff_amount')
+    #     else:
+    #         line_qty = self.product_qty
+    #     if not line_qty and not line_amount:
+    #         return False
+    #     price_subtotal = line_amount or self._price_subtotal(line_qty)
+    #
+    #     sign = reverse and -1 or 1
+    #     company_currency = self.env.user.company_id.currency_id
+    #     currency = currency or company_currency
+    #     return {
+    #         'name': self.name,
+    #         'product_id': False,
+    #         'account_id': self.analytic_account_id.id,
+    #         'unit_amount': line_qty,
+    #         'product_uom_id': False,
+    #         'amount': currency.compute(sign * price_subtotal,
+    #                                    company_currency),
+    #         'general_account_id': general_account_id,
+    #         'journal_id': analytic_journal.id,
+    #         'ref': self.request_id.name,
+    #         'user_id': self._uid,
+    #         # PR
+    #         'purchase_request_line_id': self.id,
+    #         # Fiscal
+    #         'fiscalyear_id': self.fiscalyear_id.id,
+    #     }
+    #
+    # @api.multi
+    # def _create_analytic_line(self, reverse=False):
+    #     for rec in self:
+    #         vals = rec._prepare_analytic_line(
+    #             reverse=reverse, currency=rec.request_id.currency_id)
+    #         if vals:
+    #             self.env['account.analytic.line'].sudo().create(vals)
