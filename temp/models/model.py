5225b8ca42f5e272896448a77bf80c0ceb2bb021
Kitti U <kittiu@gmail.com>
2018-01-10 09:10:34 +0700

    Temp module for fixing things

---
+++ temp/models/model.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from openerp import models, fields, api, _
+from openerp.exceptions import ValidationError
+
+
+class XXX(models.TransientModel):
+    _name = "invest.asset.selection.wiz"
+
+
+class YYY(models.TransientModel):
+    _name = "invest.asset.selection.wiz.line"
