0a6d7cd4102cff4075636564bfadc66ba21febe9
Kitti U <kittiu@gmail.com>
2018-03-02 15:56:18 +0700

    [ENH] pabi_chartfield_merged, ensure chartfield_id can't change

--- pabi_chartfield_merged/models/hr_expense.py
+++ pabi_chartfield_merged/models/hr_expense.py
@@ -1,7 +1,16 @@
 # -*- coding: utf-8 -*-
-from openerp import models
+from openerp import models, api, _
+from openerp.exceptions import ValidationError
 from .chartfield import MergedChartField
 
 
 class HRExpenseLine(MergedChartField, models.Model):
     _inherit = 'hr.expense.line'
+
+    @api.multi
+    def write(self, vals):
+        if vals.get('chartfield_id', False):
+            raise ValidationError(
+                _('Changing budget field is not allowed,\n'
+                  'budget was commit during document creation'))
+        return super(HRExpenseLine, self).write(vals)
