9d154618ce46aac281b64360efc4bf0c695afe27
Kitti U <kittiu@gmail.com>
2018-03-29 13:42:37 +0700

    -

--- pabi_budget_plan/models/res_invest_asset.py
+++ pabi_budget_plan/models/res_invest_asset.py
@@ -12,11 +12,11 @@ class ResInvestAsset(models.Model):
         Fiscal = self.env['account.fiscalyear']
         Seq = self.env['ir.sequence']
         fiscalyear_id = self._context.get('fiscalyear_id', False)
-        if not fiscalyear_id:
-            fiscalyear_id = Fiscal.find()
         for rec in self:
             if rec.code:
                 continue
+            if not fiscalyear_id:
+                fiscalyear_id = rec.fiscalyear_id.id or Fiscal.find()
             ctx = {'fiscalyear_id': fiscalyear_id}
             rec.write({
                 'fiscalyear_id': fiscalyear_id,
