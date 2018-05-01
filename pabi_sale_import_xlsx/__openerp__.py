a92e58fb5c4d5fd5928fe039b3ee0844c1eef28e
Kitti U <kittiu@gmail.com>
2018-05-01 23:52:09 +0700

    [ENH] xlsx import to add import by wizard

---
+++ pabi_sale_import_xlsx/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "NSTDA :: PABI2 - Sales Import XLSX",
+    "version": "1.0",
+    "description": """
+    """,
+    "website": "https://ecosoft.co.th/",
+    "author": "Ecosoft",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        'sale',
+    ],
+    "data": [
+        'xlsx_template/templates.xml',
+        'xlsx_template/xlsx_import_wizard.xml',
+        'xlsx_template/load_template.xml',
+    ],
+}
