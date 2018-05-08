bccf1de30d0a86a0a35577f39da463bd222c892b
newtratip <tharathip.chaweewongphan@gmail.com>
2018-05-08 17:59:11 +0700

    [Add] Add purchase contract report

---
+++ pabi_purchase_contract_report/reports/xlsx_report_purchase_contract.py
@@ -0,0 +1,25 @@
+from openerp import models, fields, api
+
+
+class XLSXReporPurchaseContract(models.TransientModel):
+    _name = 'xlsx.report.purchase.contract'
+    _inherit = 'xlsx.report'
+
+    # Search Criteria
+    date_report = fields.Date(
+        string='Report Date',
+        required=True,
+    )
+    # Report Result
+    results = fields.Many2many(
+        'purchase.contract',
+        string='Results',
+        compute='_compute_results',
+        help='Use compute fields, so there is nothing store in database',
+    )
+
+    @api.multi
+    def _compute_results(self):
+        self.ensure_one()
+        self.results = self.env['purchase.contract'].search(
+            [('collateral_remand_date', '>=', self.date_report)])
