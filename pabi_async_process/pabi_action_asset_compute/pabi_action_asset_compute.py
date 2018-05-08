e40892061477015eccd8f562b0d066a824ee10a9
Kitti U <kittiu@gmail.com>
2018-05-08 15:42:17 +0700

    -

--- pabi_async_process/pabi_action_asset_compute/pabi_action_asset_compute.py
+++ pabi_async_process/pabi_action_asset_compute/pabi_action_asset_compute.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from openerp import models, fields, api, _
+from openerp.exceptions import Warning as UserError
 
 
 class PabiActionAssetCompute(models.TransientModel):
@@ -24,6 +25,24 @@ class PabiActionAssetCompute(models.TransientModel):
         string='Profile',
     )
 
+    @api.multi
+    def check_affected_asset(self):
+        self.ensure_one()
+        AssetLine = self.env['account.asset.line']
+        assets = self._search_asset(self.calendar_period_id.id,
+                                    self.categ_ids.ids,
+                                    self.profile_ids.ids)
+        # Assume 1 depre line for 1 asset always
+        num_asset = AssetLine.search_count(
+            [('asset_id', 'in', assets.ids),
+             ('type', '=', 'depreciate'),
+             ('init_entry', '=', False),
+             ('line_date', '<=', self.calendar_period_id.date_stop),
+             ('line_date', '>=', self.calendar_period_id.date_start),
+             ('move_check', '=', False)])
+        raise UserError(
+            _('%s assets will be computed in this operation') % num_asset)
+
     @api.model
     def _get_categ_domain(self):
         asset_journal = self.env.ref('pabi_asset_management.journal_asset')
@@ -34,18 +53,23 @@ class PabiActionAssetCompute(models.TransientModel):
     def _onchange_categ_ids(self):
         self.profile_ids = []
 
-    @api.multi
-    def asset_compute(self, period_id, categ_ids, profile_ids):
+    @api.model
+    def _search_asset(self, period_id, categ_ids, profile_ids):
         # Search for
         domain = []
         if categ_ids:
             domain += [('profile_id.product_categ_id', 'in', categ_ids)]
         if profile_ids:
             domain += [('profile_id', 'in', profile_ids)]
-        period = self.env['account.period'].browse(period_id)
         # Start computing
         assets = self.env['account.asset'].search(
             [('state', '=', 'open'), ('type', '=', 'normal')] + domain)
+        return assets
+
+    @api.multi
+    def asset_compute(self, period_id, categ_ids, profile_ids):
+        period = self.env['account.period'].browse(period_id)
+        assets = self._search_asset(period_id, categ_ids, profile_ids)
         created_move_ids, error_log = \
             assets._compute_entries(period, check_triggers=True)
         # Return
