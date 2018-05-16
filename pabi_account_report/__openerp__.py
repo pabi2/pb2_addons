3b3c810ced85665957630eaf8ebdd17cc00ccc6d
newtratip <tharathip.chaweewongphan@gmail.com>
2018-05-16 18:14:24 +0700

    [IMP] Add SLA Purchase Report

--- pabi_account_report/__openerp__.py
+++ pabi_account_report/__openerp__.py
@@ -27,6 +27,7 @@
         'reports/xlsx_report_payable_detail.xml',
         'reports/xlsx_report_payable_balance.xml',
         'reports/xlsx_report_purchase_billing.xml',
+        'reports/xlsx_report_sla_purchase.xml',
     ],
     'demo': [
     ],
