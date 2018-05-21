25b92f888236816aeb527376693f16241095ede7
newtratip <tharathip.chaweewongphan@gmail.com>
2018-05-21 17:05:33 +0700

    [IMP] Adjust payable detail

--- pabi_account_report/reports/payable_detail_view.py
+++ pabi_account_report/reports/payable_detail_view.py
@@ -11,14 +11,9 @@ class PayableDetailView(models.Model):
         string='ID',
         readonly=True,
     )
-    invoice_id = fields.Many2one(
-        'account.invoice',
-        string='Invoice',
-        readonly=True,
-    )
-    purchase_id = fields.Many2one(
-        'purchase.order',
-        string='Purchase',
+    move_line_id = fields.Many2one(
+        'account.move.line',
+        string='Move Line',
         readonly=True,
     )
     voucher_id = fields.Many2one(
@@ -26,69 +21,45 @@ class PayableDetailView(models.Model):
         string='Voucher',
         readonly=True,
     )
-    tax_id = fields.Many2one(
-        'account.voucher.tax',
-        string='Tax',
-        readonly=True,
-    )
     export_id = fields.Many2one(
         'payment.export',
         string='Export',
         readonly=True,
     )
-    account_invoice_move_id = fields.Many2one(
-        'account.move',
-        string='Account Invoice Move',
+    purchase_id = fields.Many2one(
+        'purchase.order',
+        string='Purchase Order',
         readonly=True,
     )
-    account_invoice_move_line_id = fields.Many2one(
-        'account.move.line',
-        string='Account Invoice Move Line',
-        readonly=True,
-    )
-    account_voucher_move_id = fields.Many2one(
-        'account.move',
-        string='Account Voucher Move',
-        readonly=True,
-    )
-    account_voucher_move_line_id = fields.Many2one(
-        'account.move.line',
-        string='Account Voucher Move Line',
+    tax_id = fields.Many2one(
+        'account.voucher.tax',
+        string='Tax',
         readonly=True,
     )
 
     def _get_sql_view(self):
         sql_view = """
-            SELECT ROW_NUMBER() OVER(ORDER BY avml.id) AS id,
-                   inv.id AS invoice_id,
-                   po.id AS purchase_id,
-                   av.id AS voucher_id,
-                   tax.id AS tax_id,
-                   pel.export_id,
-                   invml.move_id AS account_invoice_move_id,
-                   invml.id AS account_invoice_move_line_id,
-                   avml.move_id AS account_voucher_move_id,
-                   avml.id AS account_voucher_move_line_id
-            FROM (SELECT *
-                  FROM account_move_line
-                  WHERE doctype = 'payment') avml
-            LEFT JOIN (SELECT l.*, m.date AS date_move, m.name AS move_name
-                       FROM account_move_line l
-                       LEFT JOIN account_move m ON l.move_id = m.id
-                       WHERE l.doctype IN ('in_invoice', 'in_refund',
-                                           'in_invoice_debitnote')) invml
-                ON avml.reconcile_partial_id = invml.reconcile_partial_id
-                OR avml.reconcile_id = invml.reconcile_id
-            LEFT JOIN account_invoice inv ON invml.move_id = inv.move_id
+            SELECT ROW_NUMBER() OVER(ORDER BY av.id) AS id,
+                l.id AS move_line_id, av.id AS voucher_id, exp_line.export_id,
+                po.id AS purchase_id, tax.id AS tax_id
+            FROM account_move_line l
+            LEFT JOIN account_invoice inv ON l.move_id = inv.move_id
             LEFT JOIN purchase_invoice_rel rel ON inv.id = rel.invoice_id
             LEFT JOIN purchase_order po ON rel.purchase_id = po.id
-            LEFT JOIN account_voucher av ON avml.move_id = av.move_id
-            LEFT JOIN payment_export_line pel ON av.id = pel.voucher_id
+            LEFT JOIN account_move_reconcile r ON l.reconcile_id = r.id
+                OR l.reconcile_partial_id = r.id
+            LEFT JOIN (SELECT * FROM account_move_line
+                       WHERE doctype = 'payment') l2 ON
+                r.id = l2.reconcile_id OR r.id = l2.reconcile_partial_id
+            LEFT JOIN account_voucher av ON l2.move_id = av.move_id
+            LEFT JOIN (SELECT * FROM payment_export_line l3
+                       LEFT JOIN payment_export exp ON l3.export_id = exp.id
+                       WHERE exp.state = 'done') exp_line ON
+                av.id = exp_line.voucher_id
             LEFT JOIN account_voucher_tax tax ON inv.id = tax.invoice_id
                 AND tax.voucher_id = av.id AND tax.tax_code_type = 'wht'
-            WHERE avml.reconcile_partial_id IS NOT NULL OR
-                  avml.reconcile_id IS NOT NULL
-            ORDER BY invml.partner_id, invml.date_move, invml.move_name
+            WHERE l.doctype IN ('in_invoice', 'in_refund') AND
+                l.account_id = inv.account_id
         """
         return sql_view
 
--- pabi_account_report/reports/xlsx_report_payable_detail.py
+++ pabi_account_report/reports/xlsx_report_payable_detail.py
@@ -37,7 +37,7 @@ class XLSXReportPayableDetail(models.TransientModel):
     end_period_id = fields.Many2one(
         'account.period',
         string='Period To',
-        domain=[('special', '=', False)],
+        domain=[('special', '=', False), ('state', '=', 'draft')],
     )
     start_doc_posting_date = fields.Date(
         string='Document Posting Date From',
@@ -57,8 +57,7 @@ class XLSXReportPayableDetail(models.TransientModel):
         'report', 'move_id',
         string='Document Number(s)',
         domain=[('state', '=', 'posted'),
-                ('doctype', 'in', ['in_invoice', 'in_refund',
-                                   'in_invoice_debitnote'])]
+                ('doctype', 'in', ['in_invoice', 'in_refund'])],
     )
     # Report Result
     results = fields.Many2many(
@@ -72,39 +71,33 @@ class XLSXReportPayableDetail(models.TransientModel):
     def _compute_results(self):
         self.ensure_one()
         Result = self.env['payable.detail.view']
-        dom = [('account_invoice_move_id.state', '=', 'posted'),
-               ('account_voucher_move_id.state', '=', 'posted'),
-               ('voucher_id.state', '=', 'posted'),
-               ('export_id.state', '=', 'done'),
-               ('account_invoice_move_line_id.account_id', 'in',
-                self.account_ids.ids)]
+        dom = [('move_line_id.account_id', 'in', self.account_ids.ids)]
         if self.partner_ids:
-            dom += [('account_invoice_move_line_id.partner_id', 'in',
-                     self.partner_ids.ids)]
+            dom += [('move_line_id.partner_id', 'in', self.partner_ids.ids)]
         if self.start_fiscalyear_id:
-            dom += [('account_invoice_move_line_id.period_id.fiscalyear_id'
-                     '.date_start', '>=', self.start_fiscalyear_id.date_start)]
+            dom += [('move_line_id.period_id.fiscalyear_id.date_start', '>=',
+                     self.start_fiscalyear_id.date_start)]
         if self.end_fiscalyear_id:
-            dom += [('account_invoice_move_line_id.period_id.fiscalyear_id'
-                     '.date_start', '<=', self.end_fiscalyear_id.date_start)]
+            dom += [('move_line_id.period_id.fiscalyear_id.date_start', '<=',
+                     self.end_fiscalyear_id.date_start)]
         if self.start_period_id:
-            dom += [('account_invoice_move_line_id.period_id.date_start', '>=',
+            dom += [('move_line_id.period_id.date_start', '>=',
                      self.start_period_id.date_start)]
         if self.end_period_id:
-            dom += [('account_invoice_move_line_id.period_id.date_start', '<=',
+            dom += [('move_line_id.period_id.date_start', '<=',
                      self.end_period_id.date_start)]
         if self.start_doc_posting_date:
-            dom += [('account_invoice_move_id.date', '>=',
+            dom += [('move_line_id.move_id.date', '>=',
                      self.start_doc_posting_date)]
         if self.end_doc_posting_date:
-            dom += [('account_invoice_move_id.date', '<=',
+            dom += [('move_line_id.move_id.date', '<=',
                      self.end_doc_posting_date)]
         if self.start_doc_date:
-            dom += [('account_invoice_move_id.date_document', '>=',
+            dom += [('move_line_id.move_id.date_document', '>=',
                      self.start_doc_date)]
         if self.end_doc_date:
-            dom += [('account_invoice_move_id.date_document', '<=',
+            dom += [('move_line_id.move_id.date_document', '<=',
                      self.end_doc_date)]
         if self.move_ids:
-            dom += [('account_invoice_move_id', 'in', self.move_ids.ids)]
+            dom += [('move_line_id.move_id', 'in', self.move_ids.ids)]
         self.results = Result.search(dom)
