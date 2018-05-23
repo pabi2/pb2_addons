e320859e703cc24a850f3c9311e6fb98d3d0ff7c
Kitti U <kittiu@gmail.com>
2018-05-23 10:45:46 +0700

    [ENH] pabi_th_doctype, add new doctype POS Order

--- l10n_th_doctype_order/models/sale.py
+++ l10n_th_doctype_order/models/sale.py
@@ -10,14 +10,15 @@ class SaleOrder(models.Model):
 
     @api.model
     def create(self, vals):
-        # Find doctype_id
-        if vals.get('order_type', False):
-            self = self.with_context(order_type=vals.get('order_type'))
-        refer_type = _DOCTYPE.get(self._context.get('order_type'))
-        doctype = self.env['res.doctype'].get_doctype(refer_type)
-        fiscalyear_id = self.env['account.fiscalyear'].find()
-        # --
-        self = self.with_context(doctype_id=doctype.id,
-                                 fiscalyear_id=fiscalyear_id)
-        vals['name'] = self.env['ir.sequence'].next_by_doctype()
+        if vals.get('name', '/') == '/':
+            # Find doctype_id
+            if vals.get('order_type', False):
+                self = self.with_context(order_type=vals.get('order_type'))
+            refer_type = _DOCTYPE.get(self._context.get('order_type'))
+            doctype = self.env['res.doctype'].get_doctype(refer_type)
+            fiscalyear_id = self.env['account.fiscalyear'].find()
+            # --
+            self = self.with_context(doctype_id=doctype.id,
+                                     fiscalyear_id=fiscalyear_id)
+            vals['name'] = self.env['ir.sequence'].next_by_doctype()
         return super(SaleOrder, self).create(vals)
---
+++ pabi_th_doctype/models/sale.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from openerp import models, api
+
+_DOCTYPE = {'quotation': 'sale_quotation',
+            'sale_order': 'sale_order'}
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.model
+    def create(self, vals):
+        if vals.get('name', '/') == '/':
+            # Automatic Workflow = POS Order
+            if vals.get('workflow_process_id', False):
+                doctype = self.env['res.doctype'].get_doctype('pos_order')
+                fiscalyear_id = self.env['account.fiscalyear'].find()
+                # --
+                self = self.with_context(doctype_id=doctype.id,
+                                         fiscalyear_id=fiscalyear_id)
+                vals['name'] = self.env['ir.sequence'].next_by_doctype()
+        return super(SaleOrder, self).create(vals)
