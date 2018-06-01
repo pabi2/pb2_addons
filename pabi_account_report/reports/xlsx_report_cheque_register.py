bf8c8e65af557e581ab1cebb8a4379cf66615580
newtratip <tharathip.chaweewongphan@gmail.com>
2018-06-01 15:45:53 +0700

    [IMP] Adjust cheque register report

--- pabi_account_report/reports/xlsx_report_cheque_register.py
+++ pabi_account_report/reports/xlsx_report_cheque_register.py
@@ -3,16 +3,35 @@ from openerp import models, fields, api
 
 class XLSXReportChequeRegisterReport(models.TransientModel):
     _name = 'xlsx.report.cheque.register'
-    _inherit = 'xlsx.report'
+    _inherit = 'report.account.common'
 
-    # Search Criteria
+    fiscalyear_ids = fields.Many2many(
+        default=False,
+    )
+    date_from = fields.Date(
+        default=False,
+    )
+    date_to = fields.Date(
+        default=False,
+    )
+    filter = fields.Selection(
+        [('filter_date', 'Date')],
+        string='Filter by',
+        required=True,
+        default='filter_date',
+    )
     date_cheque_received = fields.Date(
         string='Cheque Received Date',
+        default=lambda self: fields.Date.context_today(self),
+        required=True,
+    )
+    journal_ids = fields.Many2many(
+        'account.journal',
+        string='Payment Method(s)',
+        domain=[('type', '=', 'bank'), ('intransit', '=', False)],
     )
     cheque_lot_ids = fields.Many2many(
         'cheque.lot',
-        'xlsx_report_cheque_register_cheque_lot_rel',
-        'report_id', 'lot_id',
         string='Lot Number(s)',
     )
     number_cheque_from = fields.Char(
@@ -20,12 +39,6 @@ class XLSXReportChequeRegisterReport(models.TransientModel):
     )
     number_cheque_to = fields.Char(
         string='Cheque Number To',
-    )
-    account_ids = fields.Many2many(
-        'account.account',
-        'xlsx_report_cheque_register_account_rel',
-        'report_id', 'account_id',
-        string='Account(s)',
     )
     # Report Result
     results = fields.Many2many(
@@ -43,13 +56,12 @@ class XLSXReportChequeRegisterReport(models.TransientModel):
         if self.date_cheque_received:
             dom += [('voucher_id.date_cheque_received', '=',
                      self.date_cheque_received)]
+        if self.journal_ids:
+            dom += [('journal_id', 'in', self.journal_ids.ids)]
         if self.cheque_lot_ids:
             dom += [('cheque_lot_id', 'in', self.cheque_lot_ids.ids)]
         if self.number_cheque_from:
             dom += [('number', '>=', self.number_cheque_from)]
         if self.number_cheque_to:
             dom += [('number', '<=', self.number_cheque_to)]
-        if self.account_ids:
-            dom += [('cheque_lot_id.journal_id.default_credit_account_id',
-                     'in', self.account_ids.ids)]
         self.results = Result.search(dom, order="journal_id,number")
