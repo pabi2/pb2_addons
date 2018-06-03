a24f5c38e40e7d0a23a4fedfa96fc061d3968eb9
newtratip <tharathip.chaweewongphan@gmail.com>
2018-06-03 23:03:25 +0700

    [IMP] Adjust partner detail report

--- pabi_account_report/reports/report_account_common.py
+++ pabi_account_report/reports/report_account_common.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*
-import time
 from openerp import models, fields, api, _
 from openerp.exceptions import ValidationError
+import time
 
 
 class ReportAccountCommon(models.AbstractModel):
@@ -10,49 +10,62 @@ class ReportAccountCommon(models.AbstractModel):
 
     chart_account_id = fields.Many2one(
         'account.account',
-        string='Chart of Account',
-        help='Select Charts of Accounts',
-        required=True,
+        string='Chart of Accounts',
         domain=[('parent_id', '=', False)],
+        required=True,
         default=lambda self: self._get_account(),
+        help='Select Chart of Accounts',
     )
     company_id = fields.Many2one(
         'res.company',
         string='Company',
         default=lambda self: self.env.user.company_id,
     )
-    fiscalyear_ids = fields.Many2many(
+    fiscalyear_start_id = fields.Many2one(
         'account.fiscalyear',
-        string='Fiscal Year(s)',
+        string='Start Fiscal Year',
         default=lambda self: self._get_fiscalyear(),
+    )
+    fiscalyear_end_id = fields.Many2one(
+        'account.fiscalyear',
+        string='End Fiscal Year',
+        default=lambda self: self._get_fiscalyear(),
+    )
+    fiscalyear_date_start = fields.Date(
+        string='Start Fiscal Year Date',
+        related='fiscalyear_start_id.date_start',
+    )
+    fiscalyear_date_end = fields.Date(
+        string='End Fiscal Year Date',
+        related='fiscalyear_end_id.date_stop',
     )
     filter = fields.Selection(
         [('filter_no', 'No Filters'),
-         ('filter_date', 'Date'),
+         ('filter_date', 'Dates'),
          ('filter_period', 'Periods')],
         string='Filter by',
         required=True,
         default='filter_no',
     )
-    date_from = fields.Date(
+    date_start = fields.Date(
         string='Start Date',
     )
-    date_to = fields.Date(
+    date_end = fields.Date(
         string='End Date',
     )
-    period_from = fields.Many2one(
+    period_start_id = fields.Many2one(
         'account.period',
         string='Start Period',
     )
-    period_to = fields.Many2one(
+    period_end_id = fields.Many2one(
         'account.period',
         string='End Period',
     )
 
     @api.model
     def _get_account(self):
-        domain = [('parent_id', '=', False),
-                  ('company_id', '=', self.env.user.company_id.id)]
+        company_id = self.env.user.company_id.id
+        domain = [('company_id', '=', company_id), ('parent_id', '=', False)]
         account = self.env['account.account'].search(domain, limit=1)
         return account
 
@@ -76,67 +89,8 @@ class ReportAccountCommon(models.AbstractModel):
                       ('date_stop', '>=', now)]
             fiscalyear = self.env['account.fiscalyear'].search(domain, limit=1)
             self.company_id = company
-            self.fiscalyear_ids = fiscalyear
-
-    @api.onchange('filter')
-    def _onchange_filter(self):
-        context = self._context.copy()
-        filter = self.filter
-        fiscalyear_ids = False
-        if context.get('fiscalyear_ids', False):
-            fiscalyear_ids = context['fiscalyear_ids'][0][2]
-        if filter == 'filter_no':
-            self.period_from = False
-            self.period_to = False
-            self.date_from = False
-            self.date_to = False
-        if filter == 'filter_date':
-            if fiscalyear_ids:
-                fyear = self.env['account.fiscalyear'].browse(fiscalyear_ids)
-                date_from = min(fyear.mapped('date_start'))
-                date_to = max(fyear.mapped('date_stop')) > time.strftime(
-                    '%Y-%m-%d') and time.strftime('%Y-%m-%d') or \
-                    max(fyear.mapped('date_stop'))
-                if date_to < date_from:
-                    date_to = date_from
-            else:
-                date_from, date_to = time.strftime(
-                    '%Y-01-01'), time.strftime('%Y-%m-%d')
-            self.period_from = False
-            self.period_to = False
-            self.date_from = date_from
-            self.date_to = date_to
-        if filter == 'filter_period' and fiscalyear_ids:
-            start_period = end_period = False
-            self._cr.execute('''
-                SELECT * FROM (SELECT p.id
-                               FROM account_period p
-                               LEFT JOIN account_fiscalyear f
-                                ON p.fiscalyear_id = f.id
-                               WHERE f.id IN %s
-                               AND COALESCE(p.special, FALSE) = FALSE
-                               ORDER BY p.date_start ASC
-                               LIMIT 1) AS period_start
-                UNION ALL
-                SELECT * FROM (SELECT p.id
-                               FROM account_period p
-                               LEFT JOIN account_fiscalyear f
-                                ON p.fiscalyear_id = f.id
-                               WHERE f.id IN %s
-                               AND p.date_start < NOW()
-                               AND COALESCE(p.special, FALSE) = FALSE
-                               ORDER BY p.date_stop DESC
-                               LIMIT 1) AS period_stop''',
-                             (tuple(fiscalyear_ids), tuple(fiscalyear_ids)))
-            periods = [i[0] for i in self._cr.fetchall()]
-            if periods:
-                start_period = end_period = periods[0]
-                if len(periods) > 1:
-                    end_period = periods[1]
-            self.period_from = start_period
-            self.period_to = end_period
-            self.date_from = False
-            self.date_to = False
+            self.fiscalyear_start_id = fiscalyear
+            self.fiscalyear_end_id = fiscalyear
 
     @api.multi
     def run_report(self):
