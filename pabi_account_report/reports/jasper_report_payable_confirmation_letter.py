3a239b012abc99236890bbb6964c6d82acf943e0
newtratip <tharathip.chaweewongphan@gmail.com>
2018-06-03 23:14:36 +0700

    [IMP] Adjust payable confirmation letter

--- pabi_account_report/reports/jasper_report_payable_confirmation_letter.py
+++ pabi_account_report/reports/jasper_report_payable_confirmation_letter.py
@@ -1,46 +1,43 @@
 # -*- coding: utf-8 -*
-import time
-from datetime import datetime
 from openerp import models, fields, api, _
 from openerp.exceptions import ValidationError
+from datetime import datetime
+import time
 
 
 class JasperReportPayableConfirmationLetter(models.TransientModel):
     _name = 'payable.confirmation.letter'
     _inherit = 'report.account.common'
 
-    fiscalyear_ids = fields.Many2many(
+    fiscalyear_start_id = fields.Many2one(
         default=False,
     )
-    date_from = fields.Date(
+    fiscalyear_end_id = fields.Many2one(
         default=False,
     )
-    date_to = fields.Date(
+    date_start = fields.Date(
+        default=False,
+    )
+    date_end = fields.Date(
         default=False,
     )
     filter = fields.Selection(
-        [('filter_date', 'Date')],
-        string='Filter by',
-        required=True,
+        readonly=True,
         default='filter_date',
     )
     date_report = fields.Date(
         string='Report Date',
-        default=lambda self: fields.Date.context_today(self),
         required=True,
+        default=lambda self: fields.Date.context_today(self),
     )
     account_ids = fields.Many2many(
         'account.account',
-        'jasper_report_payable_confirmation_letter_account_rel',
-        'report_id', 'account_id',
         string='Filter on accounts',
         help='''Only selected accounts will be printed.
                 Leave empty to print all accounts.''',
     )
     partner_ids = fields.Many2many(
         'res.partner',
-        'jasper_report_payable_confirmation_letter_partner_rel',
-        'report_id', 'partner_id',
         string='Filter on partners',
         help='''Only selected partners will be printed.
                 Leave empty to print all partners.''',
