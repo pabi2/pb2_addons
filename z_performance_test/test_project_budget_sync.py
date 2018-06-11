0b63117982d1b7b6ee2f2d99cceaf0524a35fe1f
Kitti U <kittiu@gmail.com>
2018-06-11 17:46:45 +0700

    -

---
+++ z_performance_test/test_project_budget_sync.py
@@ -0,0 +1,50 @@
+import openerplib
+connection = openerplib.get_connection(
+    hostname="pabi2o-test.intra.nstda.or.th",
+    port=80,
+    database="PABI2",
+    login="admin",
+    password="admin",
+    protocol="jsonrpc",
+    user_id=1)
+# connection = openerplib.get_connection(
+#     hostname="localhost",
+#     port=8069,
+#     database="PABI2_integration",
+#     login="admin",
+#     password="admin",
+#     protocol="jsonrpc",
+#     user_id=1)
+
+connection.check_login()
+
+# Copy, budget = CTRL/UNIT/18/200060 to all other sections
+
+Project = connection.get_model('res.project')
+Budget = connection.get_model('account.budget')
+BudgetPlan = connection.get_model('res.project.budget.plan')
+project_ids = Project.search([('budget_plan_ids', '=', False)])
+print project_ids
+# Create budget plan line
+# for project in project_ids:
+#     data = {'project_id': project,
+#             'budget_method': 'expense',
+#             'fiscalyear_id': 3,
+#             'charge_type': 'external',
+#             'activity_group_id': 162,
+#             'm1': 10000000, }
+#     new_budget = BudgetPlan.create(data)
+#     print new_budget
+
+# budget_ids = Budget.search([('chart_view', '=', 'project_base')])
+# # Synce
+# for budget in budget_ids:
+#     print budget
+#     res = Budget.sync_budget_my_project([budget])
+
+
+plan_ids = BudgetPlan.search([('synced', '=', False)])
+print 'BudgetPlan: %s' % plan_ids
+for plan in plan_ids:
+    print plan
+    BudgetPlan.write([plan], {'released_amount': 10000000})
