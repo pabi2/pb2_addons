e7f91db9ec9ffc5b26e61b75e2b2fd55d7d2472b
Kitti U <kittiu@gmail.com>
2018-06-13 11:19:37 +0700

    -

--- pabi_procurement/__openerp__.py
+++ pabi_procurement/__openerp__.py
@@ -46,6 +46,7 @@ PABI2 - Procurement Module
         "purchase_operating_unit",
         "account_budget_activity",
         "pabi_attachment_helper",
+        "stock_request",
     ],
     "data": [
         "security/purchase_requisition.xml",
