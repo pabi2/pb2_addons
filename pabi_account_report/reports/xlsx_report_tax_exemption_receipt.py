0d5684dd7d136abe706763d55572cc9c343f7c4e
Saran L <saranp502@hotmail.com>
2018-06-28 15:45:41 +0700

    update Tax exemption receipt Report

--- pabi_account_report/reports/xlsx_report_tax_exemption_receipt.py
+++ pabi_account_report/reports/xlsx_report_tax_exemption_receipt.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*
-from openerp import models, fields, api
+from openerp import models, fields, api, tools
 
 
 class XLSXReportOutputTax(models.TransientModel):
@@ -27,7 +27,7 @@ class XLSXReportOutputTax(models.TransientModel):
         required=True,
     )
     results = fields.Many2many(
-        'account.invoice',
+        'tax.exemption.view',
         string='Results',
         compute='_compute_results',
         help='Use compute fields, so there is nothing store in database',
@@ -36,10 +36,77 @@ class XLSXReportOutputTax(models.TransientModel):
     @api.multi
     def _compute_results(self):
         self.ensure_one()
-        Result = self.env['account.invoice']
-        dom = [('type','in',['out_invoice', 'out_refund']), ('tax_line', '=', False), ('state','not in',['draff','cancel'])]
+        Result = self.env['tax.exemption.view']
+        dom = []
         if self.calendar_period_id:
-            dom += [('period_id', '=', self.calendar_period_id.id)]
+            dom += [('invoice_id.period_id', '=', self.calendar_period_id.id)]
         if self.taxbranch_id:
-            dom += [('taxbranch_id', '=', self.taxbranch_id.id)]
-        self.results = Result.search(dom, order='number_preprint')
+            dom += [('invoice_id.taxbranch_id', '=', self.taxbranch_id.id)]
+        self.results = Result.search(dom)
+
+class TaxExemptionlView(models.Model):
+    _name = 'tax.exemption.view'
+    _auto = False
+
+    date_invoice = fields.Date(
+        string='Posting Date',
+        readonly=True,
+    )
+    number_preprint = fields.Char(
+        string='Number',
+        readonly=True
+    )
+    invoice_id = fields.Many2one(
+        'account.invoice',
+        string='Invoice',
+        readonly=True,
+    )
+    partner_id = fields.Many2one(
+        'res.partner',
+        string='Partner ID',
+        readonly=True,
+    )
+    amount_untaxed = fields.Float(
+        string='Subtotal',
+        readonly=True,
+    )
+    number = fields.Char(
+        string='Document Number',
+        readonly=True
+    )
+    document_origin = fields.Char(
+        string='Document Origin',
+        readonly=True
+    )
+    validate_user_id = fields.Many2one(
+        'res.users',
+        string='User ID',
+        readonly=True,
+    )
+
+    def _get_sql_view(self):
+        sql_view = """
+            SELECT id, id AS invoice_id,
+                NULL AS interface_id, date_invoice, number_preprint, partner_id, amount_untaxed, source_document_id,
+                number, NULL AS document_origin, validate_user_id
+            FROM account_invoice
+            WHERE type IN ('out_invoice', 'out_refund')
+                AND state NOT IN ('draft','cancel')
+                AND id NOT IN (SELECT DISTINCT invoice_id FROM account_invoice_tax)
+            UNION
+            SELECT invf.id, NULL AS invoice_id, invf.id AS interface_id,
+                invf_line.date AS date_invoice, invf.preprint_number AS number_perprint,
+                invf_line.partner_id, sum(debit) AS amount_untaxed, NULL AS source_document_id, invf.number,
+                invf.name AS document_origin, invf.validate_user_id
+            FROM interface_account_entry invf
+            INNER JOIN interface_account_entry_line invf_line ON invf.id = invf_line.interface_id
+            WHERE invf.type = 'invoice' AND invf.state = 'done'
+                AND invf.id NOT IN (SELECT DISTINCT invoice_id FROM account_invoice_tax)
+            GROUP BY invf.id, invf_line.date, invf.preprint_number, invf_line.partner_id
+        """
+        return sql_view
+
+    def init(self, cr):
+        tools.drop_view_if_exists(cr, self._table)
+        cr.execute("""CREATE OR REPLACE VIEW %s AS (%s)"""
+                   % (self._table, self._get_sql_view()))
