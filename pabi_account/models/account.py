a2a8feb57fc78c7702a95a3e3eea06b818a62e93
Kitti U <kittiu@gmail.com>
2018-07-06 14:47:46 +0700

    [ENH] pabi_account, move's date_document to follow origin doc

--- pabi_account/models/account.py
+++ pabi_account/models/account.py
@@ -19,9 +19,22 @@ class AccountMove(models.Model):
         string='Document Date',
         readonly=True,
         states={'draft': [('readonly', False)]},
-        copy=False,
-        default=lambda self: fields.Date.context_today(self),
+        # copy=False,
+        # default=lambda self: fields.Date.context_today(self),
+        compute='_compute_date_document',
+        store=True,
+        help="Document date follow original document's document date, "
+        "otherwise, use current date",
     )
+
+    @api.multi
+    @api.depends('document_id')
+    def _compute_date_document(self):
+        for rec in self:
+            if rec.document_id and 'date_document' in rec.document_id:
+                rec.date_document = rec.document_id.date_document
+            else:
+                rec.date_document = fields.Date.context_today(self)
 
     @api.multi
     def _write(self, vals):
