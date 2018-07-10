a097c662a455d73247feca630f822314033ca02e
Kitti U <kittiu@gmail.com>
2018-07-10 17:24:19 +0700

    -

--- pabi_budget_plan/models/budget_plan/budget_plan_personnel.py
+++ pabi_budget_plan/models/budget_plan/budget_plan_personnel.py
@@ -100,7 +100,7 @@ class BudgetPlanPersonnelLine(BPLMonthCommon, ActivityCommon, models.Model):
     )
     personnel_costcenter_id = fields.Many2one(
         'res.personnel.costcenter',
-        required=True,
+        required=False,  # If not false, can't import
     )
     # Default fund to NSTDA, so it will be sent to Budget Control too.
     fund_id = fields.Many2one(
