25cb8e80e7b42b944e2d86a56112994e9a939079
Kitti U <kittiu@gmail.com>
2018-07-11 00:48:36 +0700

    -

--- z_performance_test/test_project_copy_budget_plan.py
+++ z_performance_test/test_project_copy_budget_plan.py
@@ -21,7 +21,11 @@ connection.check_login()
 BudgetPlan = connection.get_model('res.project.budget.plan')
 Project = connection.get_model('res.project')
 
-plan_ids = BudgetPlan.search([('fiscalyear_id.name', '=', '2018')])
+plan_ids_2019 = BudgetPlan.search([('fiscalyear_id.name', '=', '2019')])
+res = BudgetPlan.read(plan_ids_2019, ['project_id'])
+project_ids = [x['project_id'][0] for x in res]
+plan_ids = BudgetPlan.search([('fiscalyear_id.name', '=', '2018'),
+                              ('project_id', 'not in', project_ids)])
 project_ids = []
 for plan in plan_ids:
     # Only copy if no fiscaly year 2019 for this project yet
@@ -32,7 +36,7 @@ for plan in plan_ids:
                                   ('project_id', '=', project_id)])
     if not plan_ids:
         new_plan = BudgetPlan.copy(plan, {'fiscalyear_id': fy_2019})
-        res = Project.sync_budget_control([project_id])
+        # res = Project.sync_budget_control([project_id])
         print 'New budget plan id: %s' % new_plan
     else:
         print 'Budget plan already exist for %s' % plan_ids[0]
