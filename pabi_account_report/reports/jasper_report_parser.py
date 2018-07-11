ea46f8ab32d0d64fe896dd49daea27b6e87555f2
newtratip <tharathip.chaweewongphan@gmail.com>
2018-07-11 18:00:54 +0700

    [IMP] Adjust payable confirmation letter

--- pabi_account_report/reports/jasper_report_parser.py
+++ pabi_account_report/reports/jasper_report_parser.py
@@ -7,12 +7,6 @@ def jasper_report_parser(cr, uid, ids, data, context):
 
 
 jasper_reports.report_jasper(
-    'report.payable_confirmation_letter',
-    'account.move.line',
-    parser=jasper_report_parser,
-)
-
-jasper_reports.report_jasper(
     'report.cd_receivable_payment_history_group_by_customer',
     'cd.receivable.payment.history.view',
     parser=jasper_report_parser,
