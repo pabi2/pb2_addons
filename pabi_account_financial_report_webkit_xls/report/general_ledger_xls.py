ac1fd28caf822b8be375c7c547bfb81dce064726
Saran L <saranp502@hotmail.com>
2018-07-18 11:10:17 +0700

    edit summary to correct

--- pabi_account_financial_report_webkit_xls/report/general_ledger_xls.py
+++ pabi_account_financial_report_webkit_xls/report/general_ledger_xls.py
@@ -451,17 +451,17 @@ class general_ledger_xls(report_xls):
                     row_pos = self.xls_write_row(
                         ws, row_pos, row_data, ll_cell_style)
 
-                debit_start = rowcol_to_cell(row_start, 16)
-                debit_end = rowcol_to_cell(row_pos - 1, 16)
+                debit_start = rowcol_to_cell(row_start, 20)
+                debit_end = rowcol_to_cell(row_pos - 1, 20)
                 debit_formula = 'SUM(' + debit_start + ':' + debit_end + ')'
-                credit_start = rowcol_to_cell(row_start, 17)
-                credit_end = rowcol_to_cell(row_pos - 1, 17)
+                credit_start = rowcol_to_cell(row_start, 21)
+                credit_end = rowcol_to_cell(row_pos - 1, 21)
                 credit_formula = 'SUM(' + credit_start + ':' + credit_end + ')'
-                balance_debit = rowcol_to_cell(row_pos, 16)
-                balance_credit = rowcol_to_cell(row_pos, 17)
+                balance_debit = rowcol_to_cell(row_pos, 20)
+                balance_credit = rowcol_to_cell(row_pos, 21)
                 balance_formula = balance_debit + '-' + balance_credit
                 c_specs = [
-                    ('acc_title', 15, 0, 'text',
+                    ('acc_title', 19, 0, 'text',
                      ' - '.join([account.code, account.name])),
                     ('cum_bal', 1, 0, 'text',
                      _('Cumulated Balance on Account'),
