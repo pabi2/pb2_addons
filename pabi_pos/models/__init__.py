ce26fecd51516801e9e66df5776f5a170fe7a198
Kitti U <kittiu@gmail.com>
2018-07-21 23:49:58 +0700

    [ENH] pabi_pos, add method create_inventory_adjustment()

--- pabi_pos/models/__init__.py
+++ pabi_pos/models/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from . import sale_workflow_process
 from . import sale
-from . import pos_interface
+from . import pos_web_interface
 from . import product
-from . import product_interface
 from . import stock
