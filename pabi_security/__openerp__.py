59283e889fcdfe51b8a2259fdc5e46162224ca8d
som <jutamatk@ecosoft.co.th>
2018-07-25 14:19:23 +0700

    add file

--- pabi_security/__openerp__.py
+++ pabi_security/__openerp__.py
@@ -19,5 +19,8 @@ This module allow easy management of security for each user.
     'data': [
         'security/ir.model.access.csv',
         'views/pabi_security_view.xml',
+        'xlsx_template/templates.xml',
+        'xlsx_template/xlsx_template_wizard.xml',
+        'xlsx_template/load_template.xml',
     ],
 }
