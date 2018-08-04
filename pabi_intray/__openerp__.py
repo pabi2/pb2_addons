3bf65c21f2ff0ef54d7ee9ffffd863e36ba47b65
Kitti U <kittiu@gmail.com>
2018-08-04 19:35:08 +0700

    [ADD] pabi_intray

---
+++ pabi_intray/__openerp__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "NSTDA :: PABI2 - Intray",
+    "summary": "",
+    "version": "1.0",
+    "category": "Hidden",
+    "description": """
+This module contain helper function to post message as subtype intray.
+So, intray system can watch and/or post message to portal.
+
+It also contain defined message for following modules,
+- Stock Request
+
+    """,
+    "website": "https://nstda.or.th/",
+    "author": "Kitti U.",
+    "license": "AGPL-3",
+    "depends": [
+        'mail',
+        'stock_request',
+    ],
+    "data": [
+        'security/ir.model.access.csv',
+        'data/message_subtype.xml',
+        'data/stock_request_message.xml',
+    ],
+    "application": False,
+    "installable": True,
+}
---
+++ pabi_intray/models/pabi_intray.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+from openerp import models, fields
+from openerp import tools
+
+
+class PABIIntray(models.Model):
+    """ Extension Data for Intray Messsage """
+    _name = 'pabi.intray'
+    _descripion = 'PABI Intray Extension'
+
+    message_id = fields.Many2one(
+        'mail.message',
+        string='Message ID',
+        readonly=True,
+    )
+    owner = fields.Char(
+        string='Owner Emp ID',
+        readonly=True,
+    )
+    boss = fields.Char(
+        string='Boss Emp ID',
+        readonly=True,
+    )
+    action = fields.Selection(
+        [('A', 'Approve'),
+         ('R', 'Reject')],
+        string='Action Type',
+        readonly=True,
+    )
+    is_complete = fields.Boolean(
+        string='Complete Flow?',
+        readonly=True,
+        default=False,
+    )
+    url = fields.Char(
+        string='URL',
+        readonly=True,
+        help="URL without <server>, will be joining with web.base.url"
+    )
+
+    def init(self, cr):
+        """ View pabi_intray_view, to be used from external system, no ORM """
+        tools.drop_view_if_exists(cr, 'pabi_intray_view')
+        cr.execute("""CREATE or REPLACE VIEW pabi_intray_view as (
+            select pi.id, pi.owner, pi.boss, pi.action, pi.is_complete,
+            (select value from ir_config_parameter
+             where key = 'web.base.url') || '?' || url as url,
+            mm.subject, mm.body
+            from pabi_intray pi
+            join mail_message mm on mm.id = pi.message_id
+        )""")
