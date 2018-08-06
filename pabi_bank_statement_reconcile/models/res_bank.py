0e0835df6ebf2bc220d77d2c1148a1f15f8c116a
Kitti U <kittiu@gmail.com>
2018-08-06 20:08:09 +0700

    [ENH] pabi_bank_statement_reconcile

---
+++ pabi_bank_statement_reconcile/models/res_bank.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from openerp import models, api, _
+import logging
+_logger = logging.getLogger(__name__)
+
+
+class ResPartnerBank(models.Model):
+    _inherit = 'res.partner.bank'
+
+    @api.model
+    def convert_acc_number_to_journal(self, acc_number=False):
+        """ Given acc_number must be 10 digits, we need to ensure that """
+        _logger.info('Input acc_number = %s' % acc_number)
+        acc_number = acc_number or ''
+        acc_number = acc_number.strip()
+        acc_number = acc_number.replace('-', '')
+        acc_number = acc_number.zfill(10)
+        acct = self.search([('acc_number', '=', acc_number)])
+        journal = acct.journal_id.name or ''
+        _logger.info('Output journal = %s' % journal)
+        return journal
