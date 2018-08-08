8493e406cf355f82355684849c283b56c8421125
Kitti U <kittiu@gmail.com>
2018-08-08 23:04:29 +0700

    -

--- pabi_apps_config/models/account.py
+++ pabi_apps_config/models/account.py
@@ -8,6 +8,7 @@ class AccountAccount(models.Model):
     @api.model
     def name_search(self, name='', args=None, operator='ilike', limit=100):
         # Do not show view account
+        args = args or []
         if not self._context.get('show_account_view', False):
             args += [('type', '!=', 'view')]
         return super(AccountAccount, self).name_search(name=name, args=args,
