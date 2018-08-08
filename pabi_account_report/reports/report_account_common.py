0206bbefed966c60497c3aaa9926787bcee95aeb
newtratip <tharathip.chaweewongphan@gmail.com>
2018-08-08 19:01:27 +0700

    [IMP] Adjust SLA Employee Report

--- pabi_account_report/reports/report_account_common.py
+++ pabi_account_report/reports/report_account_common.py
@@ -847,3 +847,68 @@ class PabiCommonLoanAgreementReportView(models.Model):
         tools.drop_view_if_exists(cr, self._table)
         cr.execute("""CREATE OR REPLACE VIEW %s AS (%s)"""
                    % (self._table, self._get_sql_view()))
+
+
+class PabiCommonAccountVoucherReportView(models.Model):
+    _name = 'pabi.common.account.voucher.report.view'
+    _auto = False
+
+    id = fields.Integer(
+        string='ID',
+        readonly=True,
+    )
+    voucher_id = fields.Many2one(
+        'account.voucher',
+        string='Voucher',
+        readonly=True,
+    )
+    invoice_id = fields.Many2one(
+        'account.invoice',
+        string='Invoice',
+        readonly=True,
+    )
+    voucher_line_id = fields.Many2one(
+        'account.voucher.line',
+        string='Voucher Line',
+        readonly=True,
+    )
+    fiscalyear = fields.Char(
+        string='Fiscal Year',
+        readonly=True,
+    )
+    voucher_number = fields.Char(
+        string='Voucher Number',
+        readonly=True,
+    )
+    invoice_number = fields.Char(
+        string='Invoice Number',
+        readonly=True,
+    )
+
+    def _get_sql_select(self):
+        sql_select = """
+            ROW_NUMBER() OVER(ORDER BY av.id, ai.id) AS id,
+            av.id AS voucher_id, ai.id AS invoice_id,
+            av_line.id AS voucher_line_id, af.name AS fiscalyear,
+            av.number AS voucher_number, ai.number AS invoice_number
+        """
+        return sql_select
+
+    def _get_sql_view(self):
+        sql_select = """
+            SELECT %s
+            FROM account_voucher av
+            LEFT JOIN account_voucher_line av_line
+                ON av.id = av_line.voucher_id
+            LEFT JOIN account_move_line am_line
+                ON av_line.move_line_id = am_line.id
+            LEFT JOIN account_invoice ai ON am_line.move_id = ai.move_id
+            LEFT JOIN account_period ap ON av.period_id = ap.id
+            LEFT JOIN account_fiscalyear af ON ap.fiscalyear_id = af.id
+        """ % (self._get_sql_select())
+        return sql_select
+
+    def init(self, cr):
+        tools.drop_view_if_exists(cr, self._table)
+        cr.execute("""CREATE OR REPLACE VIEW %s AS (%s)"""
+                   % (self._table, self._get_sql_view()))
