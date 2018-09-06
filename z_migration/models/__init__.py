acec54a9172fbdeba8ab715b224ac98efb5eb70c
newtratip <tharathip.chaweewongphan@gmail.com>
2018-09-06 11:02:06 +0700

    [IMP] Script

--- z_migration/models/__init__.py
+++ z_migration/models/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from . import purchase
-from . import create_purchase_work_acceptance
-from . import purchase_work_acceptance
 from . import purchase_request
 from . import account_asset
 from . import account_budget
+from . import purchase_billing
