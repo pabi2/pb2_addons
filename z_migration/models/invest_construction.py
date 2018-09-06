0caf84fadbb93b15ae1fa11265b313ba772d0501
newtratip <tharathip.chaweewongphan@gmail.com>
2018-09-06 16:27:51 +0700

    [IMP] Script

--- z_migration/models/invest_construction.py
+++ z_migration/models/invest_construction.py
@@ -14,3 +14,17 @@ class ResInvestConstruction(models.Model):
     def mork_action_approve(self):
         self.action_approve()
         return True
+
+
+class RestInvestConstructionPhase(models.Model):
+    _inherit = 'res.invest.construction.phase'
+
+    @api.multi
+    def mork_action_submit(self):
+        self.action_submit()
+        return True
+
+    @api.multi
+    def mork_action_approve(self):
+        self.action_approve()
+        return True
