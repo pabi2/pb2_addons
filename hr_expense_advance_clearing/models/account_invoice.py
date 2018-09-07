3a10e8f4b628deb4871cf087e4aa28e90171dfdd
Kitti U <kittiu@gmail.com>
2018-09-07 14:28:02 +0700

    -

--- hr_expense_advance_clearing/models/account_invoice.py
+++ hr_expense_advance_clearing/models/account_invoice.py
@@ -1,15 +1,10 @@
 # -*- coding: utf-8 -*-
-from openerp import models, api, fields, _
-from openerp.exceptions import ValidationError
+from openerp import models, api, fields
 
 
 class AccountInvoice(models.Model):
     _inherit = "account.invoice"
 
-    # is_advance_clearing = fields.Boolean(
-    #     string='Advance Clearing?',
-    #     copy=False,
-    # )
     supplier_invoice_type = fields.Selection(
         [('normal', 'Normal Invoice'),
          # Created from Normal Expense
@@ -98,37 +93,6 @@ class AccountInvoice(models.Model):
                              (advance.number,))
                         self.invoice_line += \
                             self.env['account.invoice.line'].new(line)
-
-    # Not use ???
-    # @api.model
-    # def _get_invoice_total(self, invoice):
-    #  amount_total = super(AccountInvoice, self)._get_invoice_total(invoice)
-    #     return amount_total - self._prev_advance_amount(invoice)
-
-    # Not use ???
-    # @api.model
-    # def _prev_advance_amount(self, invoice):
-    #  advance_account = self.env.user.company_id.employee_advance_account_id
-    #     if not advance_account:
-    #         raise ValidationError(_('No Employee Advance Account has been '
-    #                                 'set in Account Settings!'))
-    #     lines = invoice.invoice_line
-    #     # Advance with Negative Amount
-    #     # kittiu: Performance Tuning
-    #     # advance_lines = lines.filtered(lambda x: x.price_subtotal < 0 and
-    #     #                                x.account_id == advance_account)
-    #     # return sum([l.price_subtotal for l in advance_lines])
-    #     amount = 0.0
-    #     if lines:
-    #         self._cr.execute("""
-    #             select coalesce(sum(price_subtotal), 0.0) price_subtotal
-    #             from account_invoice_line
-    #             where id in %s
-    #             and account_id = %s
-    #             and price_subtotal < 0.0
-    #         """, (tuple(lines.ids), advance_account.id))
-    #         amount = self._cr.fetchone()[0]
-    #     return amount
 
     @api.multi
     def invoice_validate(self):
