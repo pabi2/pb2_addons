b049a93402a669415c0e2d4b99b883a968353dd7
Kitti U <kittiu@gmail.com>
2018-09-12 22:16:12 +0700

    -

--- l10n_th_account/models/account_invoice.py
+++ l10n_th_account/models/account_invoice.py
@@ -71,6 +71,24 @@ class AccountInvoice(models.Model):
             res['context']['default_amount'] = 0.0
         return res
 
+    @api.multi
+    def action_move_create(self):
+        try:
+            with self._cr.savepoint():
+                return super(AccountInvoice, self).action_move_create()
+        except psycopg2.OperationalError:
+            # Let's retry 3 times, each to wait 1 seconds
+            retry = self._context.get('retry', 1)
+            if retry <= 1:
+                self._cr.rollback()
+                # time.sleep(0.5)
+                retry += 1
+                return self.with_context(retry=retry).action_move_create()
+            raise ValidationError(
+                _('Waiting for next number, please try again!'))
+        except Exception:
+            raise
+
 
 class AccountInvoiceLine(models.Model):
 
