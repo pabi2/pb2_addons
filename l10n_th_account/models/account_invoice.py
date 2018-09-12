6d58da6dde9b4207b7af3a4401827b617b5543e7
Kitti U <kittiu@gmail.com>
2018-09-12 22:26:34 +0700

    -

--- l10n_th_account/models/account_invoice.py
+++ l10n_th_account/models/account_invoice.py
@@ -79,9 +79,9 @@ class AccountInvoice(models.Model):
         except psycopg2.OperationalError:
             # Let's retry 3 times, each to wait 1 seconds
             retry = self._context.get('retry', 1)
-            if retry <= 1:
+            if retry <= 5:
                 self._cr.rollback()
-                # time.sleep(0.5)
+                time.sleep(0.2)
                 retry += 1
                 return self.with_context(retry=retry).action_move_create()
             raise ValidationError(
