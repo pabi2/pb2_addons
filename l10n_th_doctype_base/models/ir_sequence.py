1e62724df3777128431ca46b45fcd11f06fcc493
Kitti U <kittiu@gmail.com>
2018-09-12 23:48:52 +0700

    -

--- l10n_th_doctype_base/models/ir_sequence.py
+++ l10n_th_doctype_base/models/ir_sequence.py
@@ -41,12 +41,13 @@ class IrSequence(models.Model):
         try:
             return super(IrSequence, self)._next()
         except psycopg2.OperationalError:
-            # Let's retry 3 times, each to wait 1 seconds
+            # Let's retry 3 times, each to wait 0.5 seconds
             retry = self._context.get('retry', 1)
             if retry <= 5:
                 time.sleep(0.5)
                 retry += 1
-                return super(IrSequence, self)._next()
+                self._cr.commit()
+                return self.with_context(retry=retry)._next()
             raise ValidationError(
                 _('Waiting for next number, please try again!'))
         except Exception:
