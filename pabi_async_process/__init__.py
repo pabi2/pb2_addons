4dc140ea4690ab1d473c42570d8efeba19c67f4c
Kitti U <kittiu@gmail.com>
2018-09-18 14:05:56 +0700

    -

--- pabi_async_process/__init__.py
+++ pabi_async_process/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from . import models
-from . import action_purchase_create_inovice
+from . import action_purchase_create_invoice
 from . import action_run_tax_report
 from . import action_generate_entries
 from . import action_open_hr_salary
