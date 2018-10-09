621b3e6792d195683428b1c9533d7402b2a5ee76
Kitti U <kittiu@gmail.com>
2018-10-09 12:18:45 +0700

    -

--- currency_rate_update_th/services/currency_getter.py
+++ currency_rate_update_th/services/currency_getter.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from openerp.addons.currency_rate_update.\
-    services.currency_getter import Currency_getter_factory, UnknowClassError
+    services.currency_getter import Currency_getter_factory
 
 
 class CurrencyGetterFactoryTHB(Currency_getter_factory):
@@ -11,12 +11,10 @@ class CurrencyGetterFactoryTHB(Currency_getter_factory):
 
     def register(self, class_name):
         """ for class_name = 'THB_getter' """
-        print '------------------th_register -- %s' % class_name
         if class_name != 'THB_getter':
             return Currency_getter_factory.register(self, class_name)
         else:
             exec "from .update_service_%s import %s" % \
                  (class_name.replace('_getter', ''), class_name)
             class_def = eval(class_name)
-            print class_def
             return class_def()
