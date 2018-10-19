4b34039ec80b4fba778b2be87cbc3b4b07475363
newtratip <tharathip.chaweewongphan@gmail.com>
2018-10-19 15:56:56 +0700

    [IMP] Add script set to draft purchase

---
+++ z_migration/scripts/purchase_orders/purchase_orders_act_set_to_draft.py
@@ -0,0 +1,46 @@
+"""
+This method will click button set to draft in purchase order.
+"""
+import sys
+import os
+try:
+    purchase_path = os.path.dirname(os.path.realpath(__file__))
+    script_path = os.path.dirname(purchase_path)
+    migration_path = os.path.dirname(script_path)
+    controller_path = '%s/controller' % migration_path
+    sys.path.insert(0, controller_path)
+    from connection import connection
+    import log
+except Exception:
+    pass
+
+# Model
+PurchaseOrder = connection.get_model('purchase.order')
+
+# Domain
+# po_names = ['PO18001191', 'PO18001192', 'PO18001193']
+# dom = [('name', 'in', po_names)]
+dom = [('order_type', '=', 'purchase_order'), ('state', 'in', ['cancel'])]
+
+# Search PO
+pos = PurchaseOrder.search_read(dom)
+
+log_po_names = [[], []]
+logger = log.setup_custom_logger('purchase_orders_act_set_to_draft')
+logger.info('Start process')
+logger.info('Total purchase order: %s' % len(pos))
+for po in pos:
+    try:
+        PurchaseOrder.action_cancel_draft([po['id']])
+        log_po_names[0].append(po['name'].encode('utf-8'))
+        logger.info('Pass: %s' % po['name'])
+    except Exception as ex:
+        log_po_names[1].append(po['name'].encode('utf-8'))
+        logger.error('Fail: %s (reason: %s)' % (po['name'], ex))
+summary = 'Summary: pass %s%s and fail %s%s' \
+          % (len(log_po_names[0]),
+             log_po_names[0] and ' %s' % str(tuple(log_po_names[0])) or '',
+             len(log_po_names[1]),
+             log_po_names[1] and ' %s' % str(tuple(log_po_names[1])) or '')
+logger.info(summary)
+logger.info('End process')
