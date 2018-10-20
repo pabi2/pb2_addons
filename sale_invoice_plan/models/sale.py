85dc7d39b8f2e60295cb191fad7830d2cda4540f
newtratip <tharathip.chaweewongphan@gmail.com>
2018-10-20 08:12:04 +0700

    [IMP] Change precision to 1 (I want to check for round(3.5, 1) = round(3.49, 1))

--- sale_invoice_plan/models/sale.py
+++ sale_invoice_plan/models/sale.py
@@ -132,7 +132,7 @@ class sale_order(models.Model):
             # kittiu: problem with decimal, so we dicide to test with 0
             # obj_precision = self.env['decimal.precision']
             # prec = obj_precision.precision_get('Account')
-            prec = 0
+            prec = 1
             # --
             for order_line in self.order_line:
                 subtotal = self.price_include and \
