17db3d7d59d270ce7ff85b44b70572849115fc5b
Siraparn <siraparn@trinityroots.co.th>
2018-10-21 21:36:39 +0700

    [ADD] pabi_stock_receipt : prohibit the multi line partial recepit

---
+++ pabi_stock_receipt/__openerp__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "NSTDA :: PABI2 - Stock Receipt",
+    "summary": "Pabi2 Stock",
+    "version": "8.0.1.0.0",
+    "category": "Stock",
+    "description": """
+
+PABI2 - Stock Receipt
+=====================
+
+  * Prohibit user for receiving the same product with multi lines.
+
+    """,
+    "website": "https://nstda.or.th/",
+    "author": "TrinityRoots",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "pabi_purchase_work_acceptance",
+    ],
+    "data": [
+    ],
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
