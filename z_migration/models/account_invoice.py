d1423ed9b9767edfb645c897432c096ba296e582
Kitti U <kittiu@gmail.com>
2018-10-23 23:17:05 +0700

    -

--- z_migration/models/account_invoice.py
+++ z_migration/models/account_invoice.py
@@ -7,6 +7,7 @@ class AccountInvoice(models.Model):
 
     type = fields.Selection(
         readonly=False,
+        default='in_invoice',
     )
     source_document = fields.Char(
         readonly=False,
