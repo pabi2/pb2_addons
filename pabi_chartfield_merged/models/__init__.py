375a48afa07952f6b36736a686d28c4ebc58e67b
Kitti U <kittiu@gmail.com>
2018-10-24 12:32:42 +0700

    [ENH] hr_salary

--- pabi_chartfield_merged/models/__init__.py
+++ pabi_chartfield_merged/models/__init__.py
@@ -5,7 +5,7 @@ from . import sale
 from . import account
 from . import account_invoice
 from . import hr_expense
-# from . import hr_salary
+from . import hr_salary
 from . import purchase_request
 from . import purchase_requisition
 from . import purchase_request_line_make_purchase_requisition
--- pabi_chartfield_merged/models/hr_salary.py
+++ pabi_chartfield_merged/models/hr_salary.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-# from openerp import models, fields
-# from .chartfield import MergedChartField
-#
-#
-# class HRSalaryLine(MergedChartField, models.Model):
-#     _inherit = 'hr.salary.line'
-#
-#     chartfield_id = fields.Many2one(
-#         domain=[('model', '=', 'res.personnel.costcenter')],
-#     )
+from openerp import models, fields
+from .chartfield import MergedChartField
+
+
+class HRSalaryLine(MergedChartField, models.Model):
+    _inherit = 'hr.salary.line'
+
+    chartfield_id = fields.Many2one(
+        domain=[],
+    )
