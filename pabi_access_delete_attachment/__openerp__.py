90b6f6ca2c680bb78ffd38275e2e78929324be8f
newtratip <tharathip.chaweewongphan@gmail.com>
2018-10-28 14:43:38 +0700

    [ADD] Add pabi_access_delete_attachment

---
+++ pabi_access_delete_attachment/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "NSTDA :: PABI2 - Access Delete Attachment",
+    "summary": "",
+    "version": "8.0.1.0.0",
+    "category": "Hidden",
+    "description": """
+Access right - delete attachment (Only state draft) \n
+1. Employee can delete own documents. \n
+2. Administration can delete all documents.
+    """,
+    "website": "https://ecosoft.co.th/",
+    "author": "Tharathip C.",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["base"],
+    "data": [],
+}
