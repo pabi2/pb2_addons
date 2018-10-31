fd476d77f575595d4e28a20a49d9dec4372811c8
Kitti U <kittiu@gmail.com>
2018-10-31 14:28:05 +0700

    [ENH] PND1 report

--- pabi_account_report/__openerp__.py
+++ pabi_account_report/__openerp__.py
@@ -72,6 +72,8 @@
         # Asset Reports
         'reports/asset_repair_report.xml',
         'reports/asset_register_report.xml',
+        # PND Reports
+        'reports/xlsx_report_pnd1.xml',
     ],
     'demo': [
     ],
