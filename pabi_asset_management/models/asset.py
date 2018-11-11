d4def6075319b105811125f9e002a3b385b5cd87
Kitti U <kittiu@gmail.com>
2018-11-11 13:34:03 +0700

    -

--- pabi_asset_management/models/asset.py
+++ pabi_asset_management/models/asset.py
@@ -532,9 +532,9 @@ class AccountAsset(ChartFieldAction, models.Model):
         DepreLine = self.env['account.asset.line']
         for asset in self:
             depre_lines = DepreLine.search([('type', '=', 'depreciate'),
-                                           ('init_entry', '=', True),
-                                           ('move_check', '=', False),
-                                           ('asset_id', '=', asset.id)])
+                                            ('init_entry', '=', True),
+                                            ('move_check', '=', False),
+                                            ('asset_id', '=', asset.id)])
             for depre in depre_lines:
                 if depre.asset_id.profile_type == 'normal':
                     depre.create_move()
