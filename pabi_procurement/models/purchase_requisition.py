a62f1d11d5c225d0ed1ac4db213e3a3d8e39b3b9
Siraparn <siraparn@trinityroots.co.th>
2018-11-21 01:13:04 +0700

    Revert "[IMP] pabi_procurement : stamp verify uid before sending to start workflow on AF"
    This reverts commit 65080eb173e8bdcebaabf5b9e22d6d8737e7a8f9.

--- pabi_procurement/models/purchase_requisition.py
+++ pabi_procurement/models/purchase_requisition.py
@@ -521,6 +521,9 @@ class PurchaseRequisition(models.Model):
     def set_verification_info(self):
         assert len(self) == 1, \
             'This option should only be used for a single id at a time.'
+        pabiweb_active = self.env.user.company_id.pabiweb_active
+        if pabiweb_active:  # If no connection to PRWeb, no need to send doc
+            self.print_call_for_bid_form()
         self.write({
             'verify_uid': self._uid,
             'date_verify': fields.Date.context_today(self),
@@ -531,9 +534,6 @@ class PurchaseRequisition(models.Model):
                     'verify_uid': self._uid,
                     'date_verify': fields.Date.context_today(self),
                 })
-        pabiweb_active = self.env.user.company_id.pabiweb_active
-        if pabiweb_active:  # If no connection to PRWeb, no need to send doc
-            self.print_call_for_bid_form()
         return True
 
     @api.multi
