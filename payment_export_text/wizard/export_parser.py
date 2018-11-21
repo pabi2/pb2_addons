dc36ae2a8d8aa387e182f2a86fcd610751d06aaa
Saran L <saranp502@hotmail.com>
2018-11-21 14:25:26 +0700

    [FIX] invoice_detail

--- payment_export_text/wizard/export_parser.py
+++ payment_export_text/wizard/export_parser.py
@@ -104,42 +104,38 @@ class DocumentExportParser(models.TransientModel):
                 data_list.append(line_detail_config_lines)
 
         # If not use footer
-        if config_id.invoice_detail_disabled:
-            return data_list
-
-        if export_lines:
-            for export_line in export_lines:
-                voucher_lines = export_line.voucher_id.line_ids
-                if voucher_lines:
-                    for voucher_line in voucher_lines:
-                        invoice_lines = voucher_line.invoice_id.invoice_line
-                        if invoice_lines:
-                            for invoice_line in invoice_lines:
-                                line_invoice_detail_config_lines = \
-                                    config_id.invoice_detail_config_line_ids.\
-                                    read(config_fields_to_read)
-                                for line in line_invoice_detail_config_lines:
-                                    model_id = active_model.id
-                                    if line.get('model_id', []):
-                                        model_id = line['model_id'][0]
-                                    if model_id == active_model.id:
-                                        eval_context = self._get_eval_context(
-                                            active_model.id, active_id)
-                                    else:
-                                        eval_context = self._get_eval_context(
-                                            model_id, voucher_line.id)
-                                    if line['field_code']:
-                                        eval(line['field_code'], eval_context,
-                                             mode="exec", nocopy=True)
-                                        value = \
-                                            eval_context.get('value', False)
-                                        line.update({'value': value})
-                                    else:
-                                        value = line['default_value'] and\
-                                            line['default_value'] or ''
-                                        line.update({'value': value})
-                                data_list.append(
-                                    line_invoice_detail_config_lines)
+        if not config_id.invoice_detail_disabled:
+            voucher_lines = [x.voucher_id.line_ids for x in export_lines]
+            if export_lines and voucher_lines:
+                for voucher_line in voucher_lines:
+                    invoice_lines = voucher_line.invoice_id.invoice_line
+                    if invoice_lines:
+                        for invoice_line in invoice_lines:
+                            line_invoice_detail_config_lines = \
+                                config_id.invoice_detail_config_line_ids.\
+                                read(config_fields_to_read)
+                            for line in line_invoice_detail_config_lines:
+                                model_id = active_model.id
+                                if line.get('model_id', []):
+                                    model_id = line['model_id'][0]
+                                if model_id == active_model.id:
+                                    eval_context = self._get_eval_context(
+                                        active_model.id, active_id)
+                                else:
+                                    eval_context = self._get_eval_context(
+                                        model_id, voucher_line.id)
+                                if line['field_code']:
+                                    eval(line['field_code'], eval_context,
+                                         mode="exec", nocopy=True)
+                                    value = \
+                                        eval_context.get('value', False)
+                                    line.update({'value': value})
+                                else:
+                                    value = line['default_value'] and\
+                                        line['default_value'] or ''
+                                    line.update({'value': value})
+                            data_list.append(
+                                line_invoice_detail_config_lines)
 
         # If not use footer
         if config_id.footer_disabled:
