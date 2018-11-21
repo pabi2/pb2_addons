9be739e0e4bc09059f37625bf3a5c3111a2eaad5
Kitti U <kittiu@gmail.com>
2018-11-21 21:13:35 +0700

    [ADD] stock_picking_cancel

---
+++ stock_picking_cancel/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Picking Cancel",
+    "summary": "",
+    "version": "1.0",
+    "category": "Hidden",
+    "description": """
+    """,
+    "website": "http://ecosoft.co.th/",
+    "author": "Kitti U.",
+    "license": "AGPL-3",
+    "depends": [
+        'stock',
+    ],
+    "data": [
+        'views/stock_view.xml',
+    ],
+    "application": False,
+    "installable": True,
+}
