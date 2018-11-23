cc7932d4b79670644231c3a5d72538f19c0395bd
Kitti U <kittiu@gmail.com>
2018-11-23 14:58:50 +0700

    [ENH] take out pabi_intray_view

--- pabi_intray/models/pabi_intray.py
+++ pabi_intray/models/pabi_intray.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from openerp import models, fields
-from openerp import tools
 
 
 class PABIIntray(models.Model):
@@ -40,19 +39,3 @@ class PABIIntray(models.Model):
         size=500,
         help="URL without <server>, will be joining with web.base.url",
     )
-
-    def init(self, cr):
-        """ View pabi_intray_view, to be used from external system, no ORM """
-        tools.drop_view_if_exists(cr, 'pabi_intray_view')
-        cr.execute("""CREATE or REPLACE VIEW pabi_intray_view as (
-            select pi.id, pi.owner, pi.boss, pi.action, pi.is_complete,
-            (select value from ir_config_parameter
-             where key = 'web.base.url') || '?' || url as url,
-            mm.subject, mm.body, mm.write_date
-            from pabi_intray pi
-            join mail_message mm on mm.id = pi.message_id
-        )""")
-        try:
-            cr.execute("GRANT SELECT ON pabi_intray_view TO intraymo")
-        except Exception:
-            pass
