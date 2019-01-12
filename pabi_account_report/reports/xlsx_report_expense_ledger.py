c3ba343dce58ac89502b38392c793b99b75f63fa
newtratip <tharathip.chaweewongphan@gmail.com>
2019-01-12 18:17:49 +0700

    [IMP] Adjust expense and revenue ledger report

--- pabi_account_report/reports/xlsx_report_expense_ledger.py
+++ pabi_account_report/reports/xlsx_report_expense_ledger.py
@@ -32,9 +32,13 @@ class XLSXReportExpenseLedger(models.TransientModel):
          ('external', 'External')],
         string='Charge Type',
     )
+    user_type = fields.Many2one(
+        'account.account.type',
+        string='Account Type',
+        default=lambda self: self.env.ref('account.data_account_type_expense'),
+    )
     results = fields.Many2many(
-        # 'pabi.common.account.report.view',  # Old
-        'account.move.line',  # New
+        'account.move.line',
         string='Results',
         compute='_compute_results',
         help='Use compute fields, so there is nothing store in database',
@@ -47,79 +51,42 @@ class XLSXReportExpenseLedger(models.TransientModel):
             rec.count_chartfield = len(rec.chartfield_ids)
 
     @api.multi
-    def _get_budget_ids(self, model):
-        return self.chartfield_ids.filtered(lambda l: l.model == model) \
-            .mapped('res_id')
-
-    @api.multi
     def _compute_results(self):
+        """
+        Solution
+        1. Get from account.move.line
+        2. Check account type is expense
+        """
         self.ensure_one()
-        # ================ Old =======================
-        # Result = self.env['pabi.common.account.report.view']
-        # dom = [('account_id.user_type.code', '=', 'Expense')]
-        # if self.account_ids:
-        #     dom += [('account_id', 'in', self.account_ids.ids)]
-        # if self.chartfield_ids:
-        #     ChartfieldView = self.env['chartfield.view']
-        #     chartfields = ChartfieldView.browse(self.chartfield_ids.ids)
-        #     budgets = ['%s,%s' % (x.model, x.res_id) for x in chartfields]
-        #     dom += [('budget', 'in', budgets)]
-        # if self.partner_ids:
-        #     dom += [('partner_id', 'in', self.partner_ids.ids)]
-        # if self.fiscalyear_start_id:
-        #     dom += [('invoice_posting_date', '>=',
-        #              self.fiscalyear_start_id.date_start)]
-        # if self.fiscalyear_end_id:
-        #     dom += [('invoice_posting_date', '<=',
-        #              self.fiscalyear_end_id.date_stop)]
-        # if self.period_start_id:
-        #     dom += [('invoice_posting_date', '>=',
-        #              self.period_start_id.date_start)]
-        # if self.period_end_id:
-        #     dom += [('invoice_posting_date', '<=',
-        #              self.period_end_id.date_stop)]
-        # if self.date_start:
-        #     dom += [('invoice_posting_date', '>=', self.date_start)]
-        # if self.date_end:
-        #     dom += [('invoice_posting_date', '<=', self.date_end)]
-        # self.results = Result.search(dom).sorted(
-        #                key=lambda l: (l.charge_type,
-        #                               l.account_id.code,
-        #                               l.activity_group_id.code,
-        #                               l.activity_id.code,
-        #                               l.period_id.fiscalyear_id.name,
-        #                               l.period_id.name))
-
-        # ================ New =======================
         Result = self.env['account.move.line']
-        dom = [('account_id.user_type.code', '=', 'Expense')]
+        dom = [('account_id.user_type', '=', self.user_type.id)]
         if self.account_ids:
             dom += [('account_id', 'in', self.account_ids.ids)]
-        if self.charge_type:
-            dom += [('charge_type', '=', self.charge_type)]
-        # Budget
-        section_ids = self._get_budget_ids('res.section')
-        project_ids = self._get_budget_ids('res.project')
-        invest_asset_ids = self._get_budget_ids('res.invest.asset')
-        invest_construction_phase_ids = \
-            self._get_budget_ids('res.invest.construction.phase')
-        personnel_costcenter_ids = \
-            self._get_budget_ids('res.personnel.costcenter')
-        self._cr.execute("""
-            select id
-            from account_move_line
-            where section_id in %s or project_id in %s or invest_asset_id in %s
-                or invest_construction_phase_id in %s or
-                personnel_costcenter_id in %s""" %
-                         (str(tuple(section_ids + [0, 0])),
-                          str(tuple(project_ids + [0, 0])),
-                          str(tuple(invest_asset_ids + [0, 0])),
-                          str(tuple(invest_construction_phase_ids + [0, 0])),
-                          str(tuple(personnel_costcenter_ids + [0, 0]))))
         if self.chartfield_ids:
-            dom += [('id', 'in', map(lambda l: l[0], self._cr.fetchall()))]
+            # map beetween chartfield_id with chartfield type
+            chartfields = [('section_id', 'sc:'),
+                           ('project_id', 'pj:'),
+                           ('invest_construction_phase_id', 'cp:'),
+                           ('invest_asset_id', 'ia:'),
+                           ('personnel_costcenter_id', 'pc:')]
+            where_str = ''
+            res_ids = []
+            for chartfield_id, chartfield_type in chartfields:
+                chartfield_ids = self.chartfield_ids.filtered(
+                    lambda l: l.type == chartfield_type).mapped('res_id')
+                if chartfield_ids:
+                    if where_str:
+                        where_str += ' or '
+                    where_str += chartfield_id + ' in %s'
+                    res_ids.append(tuple(chartfield_ids))
+            if res_ids:
+                sql = "select id from account_move_line where " + where_str
+                self._cr.execute(sql, res_ids)
+                dom += [('id', 'in', map(lambda l: l[0], self._cr.fetchall()))]
         if self.partner_ids:
             dom += [('partner_id', 'in', self.partner_ids.ids)]
+        if self.charge_type:
+            dom += [('charge_type', '=', self.charge_type)]
         if self.fiscalyear_start_id:
             dom += [('date', '>=', self.fiscalyear_start_id.date_start)]
         if self.fiscalyear_end_id:
