6dd5595b0629b1e17279b91536234606f281bfde
Sureerat Meepoo <sureerat.mee@biotec.or.th>
2019-03-06 16:47:07 +0700

    #3238-Budget_Method https://mobileapp.nstda.or.th/redmine/issues/3238 module: pabi_budget_drilldown_report ***เพิ่ม Budget Method ในรายงาน My Budget Report

--- pabi_account_report/reports/xlsx_report_advance_status.py
+++ pabi_account_report/reports/xlsx_report_advance_status.py
@@ -36,7 +36,7 @@ class XLSXReportAdvanceStatus(models.TransientModel):
             WHERE expense.is_employee_advance = TRUE AND
                 expense.amount_advanced IS NOT NULL
             GROUP BY expense.id
-            HAVING expense.amount_advanced >=
+            HAVING expense.amount_advanced >
                 SUM(CASE WHEN clearing.id IS NULL THEN 0
                          WHEN clearing.date <= '%s' THEN (CASE WHEN clearing.clearing_amount IS NULL THEN 0 ELSE clearing.clearing_amount END)
                          ELSE 0 END)
