c819e9ac9e675ff99d9ee57fc3ea4525fce41ac6
developer <developer@10.0.2.15>
2019-03-11 16:11:41 +0700

    #3281 - เพิ่ม domain ไม่เอา state = cancelled ใน Advance Status report

--- pabi_account_report/reports/xlsx_report_advance_status.py
+++ pabi_account_report/reports/xlsx_report_advance_status.py
@@ -58,6 +58,7 @@ class XLSXReportAdvanceStatus(models.TransientModel):
         date_report = self.date_report
         Result = self.env['hr.expense.expense']
         dom = [('is_employee_advance', '=', True),
+               ('state', '!=', 'cancelled'),
                #('invoice_id.date_paid', '!=', False),
                #('invoice_id.date_paid', '<=', date_report),
                ('id', 'in', self._get_expense_id(date_report))]
