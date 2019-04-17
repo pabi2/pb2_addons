d6cf447ddbc475ae4417e7ceb92e015115691c0b
developer <developer@10.0.2.15>
2019-04-17 15:59:15 +0700

    update summary view

--- pabi_budget_report/reports/rpt_budget_future_commit_summary.py
+++ pabi_budget_report/reports/rpt_budget_future_commit_summary.py
@@ -57,7 +57,7 @@ class RPTBudgetFutureCommitSummary(models.TransientModel):
         
         Result = self.env['rpt.budget.future.commit.summary.line']
         
-        """if self.chartfield_ids:
+        if self.chartfield_ids:
             chartfield = self.chartfield_ids
             
             section_ids = self._get_chartfield_ids(chartfield, 'sc:')
@@ -83,10 +83,6 @@ class RPTBudgetFutureCommitSummary(models.TransientModel):
             dom += [('section_program_id', '=', self.section_program_id.id)]
         if self.invest_asset_id:
             invest_asset_ids.append(self.invest_asset_id.id)
-        if self.activity_group_id:
-            dom += [('activity_group_id', '=', self.activity_group_id.id)]
-        if self.activity_id:
-            dom += [('activity_rpt_id', '=', self.activity_id.id)]
         if self.functional_area_id:
             dom += [('functional_area_id', '=', self.functional_area_id.id)]
         if self.program_group_id:
@@ -109,7 +105,7 @@ class RPTBudgetFutureCommitSummary(models.TransientModel):
         if len(invest_construction_phase_ids) > 0:
             dom += [('invest_construction_phase_id', 'in', invest_construction_phase_ids)]
         if len(invest_asset_ids) > 0:
-            dom += [('invest_asset_id', 'in', invest_asset_ids)]"""
+            dom += [('invest_asset_id', 'in', invest_asset_ids)]
 
         self.results = Result.search(dom, limit=3)
     
@@ -146,20 +142,21 @@ class RPTBudgetFutureCommitSummaryLine(models.Model):
     subtotal = fields.Float('subtotal', digits=(32, 2))
     po_commit = fields.Float('po_commit', digits=(32, 2))
     po_actual = fields.Float('po_actual', digits=(32, 2))
-    """remaining = fields.Float('remaining', digits=(32, 2))
+    remaining = fields.Float('remaining', digits=(32, 2))
     budget_view = fields.Char('budget_view')
     budget_code = fields.Char('budget_code')
     budget_name = fields.Char('budget_name')
+    sector_code = fields.Char('sector_code')
+    sector_name = fields.Char('sector_name')
+    subsector_code = fields.Char('subsector_code')
+    subsector_name = fields.Char('subsector_name')
+    division_code = fields.Char('division_code')
+    division_name = fields.Char('division_name')
     
-    activity_group_id = fields.Many2one('account.activity.group')
-    activity_rpt_id = fields.Many2one('account.activity')
     contract_id = fields.Many2one('purchase.contract')
     fiscalyear_id = fields.Many2one('account.fiscalyear')
     invest_construction_phase_id = fields.Many2one('res.invest.construction.phase')
     operating_unit_id = fields.Many2one('operating.unit')
-    sector_id = fields.Many2one('res.sector')
-    subsector_id = fields.Many2one('res.subsector')
-    division_id = fields.Many2one('res.division')
     section_id = fields.Many2one('res.section')
     section_program_id = fields.Many2one('res.section.program')
     invest_asset_id = fields.Many2one('res.invest.asset')
@@ -169,26 +166,61 @@ class RPTBudgetFutureCommitSummaryLine(models.Model):
     project_group_id = fields.Many2one('res.project.group')
     project_id = fields.Many2one('res.project')
     invest_construction_id = fields.Many2one('res.invest.construction')
-    purchase_id = fields.Many2one('purchase.order')"""
+    purchase_id = fields.Many2one('purchase.order')
     
     
     def _get_sql_view(self):
         sql_view = """
-        SELECT ROW_NUMBER() over (order by po.id) AS id,
-            fc.subtotal_th as subtotal,
-            SUM(sum_com.amount) as po_commit,
-            0 as po_actual
-        FROM rpt_budget_future_commit_line fc
-            LEFT JOIN purchase_order po ON po.id = fc.purchase_id
-            LEFT JOIN purchase_contract pur_con ON pur_con.id = po.contract_id
-            LEFT JOIN purchase_order_line pol ON pol.order_id = po.id
-            LEFT JOIN account_fiscalyear fis ON fis.id = pol.fiscalyear_id
-            LEFT JOIN res_currency cur ON cur.id = po.currency_id
-            LEFT JOIN res_currency_rate cur_r ON cur_r.currency_id = cur.id
-            INNER JOIN issi_budget_summary_commit_view sum_com ON sum_com.document = po.name
-        WHERE po.state in ('approved','confirmed','done') and pol.active = True
-            and po.order_type = 'purchase_order'
-        Group by po.id, fc.subtotal_th
+         SELECT 
+        ROW_NUMBER() over (order by pol.order_id) AS id,
+        SUM(ROUND((pol.product_qty*pol.price_unit)*
+        (CASE
+            WHEN cur.name = 'THB' THEN 1
+             WHEN (SELECT cur_r.rate_input FROM res_currency_rate cur_r 
+                 WHERE cur_r.currency_id = cur.id and CAST(po.date_order AS DATE) = CAST(cur_r.name AS DATE) limit 1) IS NULL 
+             THEN (SELECT cur_r.rate_input FROM res_currency_rate cur_r 
+                 WHERE cur_r.currency_id = cur.id and CAST(po.date_order AS DATE) > CAST(cur_r.name AS DATE) order by cur_r.name DESC limit 1)
+            ELSE (SELECT cur_r.rate_input FROM res_currency_rate cur_r 
+             WHERE cur_r.currency_id = cur.id and CAST(po.date_order AS DATE) = CAST(cur_r.name AS DATE) limit 1)
+        END), 2)) as subtotal,
+        sum(sum_com.amount) as po_commit, sum(sum_act.amount) as po_actual, sum_act.budget_view,
+        CASE 
+            WHEN pol.project_id IS NOT NULL THEN mpj.project_code
+            WHEN pol.invest_construction_phase_id IS NOT NULL THEN ricp.code
+            WHEN pol.section_id IS NOT NULL THEN mst.section_code
+            WHEN pol.invest_asset_id IS NOT NULL THEN inv_mst.section_code
+            ELSE null
+        END as budget_code,
+        CASE 
+            WHEN pol.project_id IS NOT NULL THEN mpj.project_name
+            WHEN pol.invest_construction_phase_id IS NOT NULL THEN ricp.name
+            WHEN pol.section_id IS NOT NULL THEN mst.section_name
+            WHEN pol.invest_asset_id IS NOT NULL THEN inv_mst.section_name
+            ELSE null
+        END as budget_name,
+        po.contract_id, pol.fiscalyear_id, pol.invest_construction_phase_id,
+        po.operating_unit_id, pol.sector_id, pol.subsector_id, pol.division_id, pol.section_id, pol.section_program_id, 
+        pol.invest_asset_id, pol.functional_area_id, pol.program_group_id, pol.program_id, pol.project_group_id, pol.project_id, 
+        pol.invest_construction_id, pol.order_id as purchase_id
+    FROM purchase_order po
+        LEFT JOIN purchase_order_line pol ON pol.order_id = po.id
+        LEFT JOIN res_currency cur ON cur.id = po.currency_id
+        LEFT JOIN etl_issi_m_project mpj ON mpj.pb2_project_id = pol.project_id
+        LEFT JOIN etl_issi_m_section mst ON mst.section_id = pol.section_id
+        LEFT JOIN res_invest_construction_phase ricp ON ricp.id = pol.invest_construction_phase_id
+        LEFT JOIN res_invest_asset res_inv ON res_inv.id = pol.invest_asset_id
+        LEFT JOIN etl_issi_m_section inv_mst ON inv_mst.section_id = res_inv.owner_section_id
+        INNER JOIN issi_budget_summary_commit_view sum_com ON sum_com.document = po.name
+        INNER JOIN issi_budget_summary_actual_view sum_act ON sum_act.ref_document = po.name
+    WHERE sum_com.budget_commit_type in ('po_commit') AND sum_com.budget_method in ('expense')
+        AND sum_act.charge_type in ('external') 
+        AND sum_act.budget_method in ('expense') AND sum_act.ref_document not like 'EX%'
+    GROUP BY sum_com.fisyear, sum_com.document, sum_act.budget_view,
+        po.contract_id, pol.fiscalyear_id, pol.invest_construction_phase_id,
+        po.operating_unit_id, pol.sector_id, pol.subsector_id, pol.division_id, pol.section_id, pol.section_program_id, 
+        pol.invest_asset_id, pol.functional_area_id, pol.program_group_id, pol.program_id, pol.project_group_id, pol.project_id, 
+        pol.invest_construction_id, pol.order_id, mpj.project_code, ricp.code, mst.section_code, inv_mst.section_code,
+        mpj.project_name, ricp.name, mst.section_name, inv_mst.section_name
         """
         
         return sql_view
