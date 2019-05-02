4e9a3f78fa3e022cd2e38bf6e9aa2957dbbb1143
developer <developer@10.0.2.15>
2019-05-02 14:43:49 +0700

    assign invoice's AG&A to move_line's credit line

--- pabi_asset_management/models/asset_adjust.py
+++ pabi_asset_management/models/asset_adjust.py
@@ -658,13 +658,9 @@ class AccountAssetAdjust(models.Model):
                     # assign invoice's AG&A to move_line's credit line
                     if movl.credit and (movl.account_id == invl.account_id):
                         _logger.info("account_id: %s", str(movl.account_id))
-                        activity_dict = {
-                            "activity_group_id": invl.activity_group_id.id,
-                            "activity_id": invl.activity_id.id,
-                            "activity_rpt_id": invl.activity_rpt_id.id,
-                        }
-                        update_dict = [(0, 0, activity_dict)]
-                        movl.write(update_dict)
+                        movl.write({'activity_group_id': invl.activity_group_id.id})
+                        movl.write({'activity_id': invl.activity_id.id})
+                        movl.write({'activity_rpt_id': invl.activity_rpt_id.id})
 
     @api.model
     def _setup_move_data(self, journal, adjust_date,
