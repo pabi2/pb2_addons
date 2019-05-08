da8e967a4d9b7a357283bd9646f3ec07ab63d02e
developer <developer@10.0.2.15>
2019-05-08 16:28:14 +0700

    create analytic line for expense

--- pabi_asset_management/models/asset_adjust.py
+++ pabi_asset_management/models/asset_adjust.py
@@ -1257,7 +1257,7 @@ class AccountAssetAdjustExpenseToAsset(MergedChartField, ActivityCommon,
         _logger.info("invl_analytic_line: %s", str(invl_analytic_line))
         
         domain = []
-        domain.append(("account_id", "=", self.account_analytic_id))
+        domain.append(("account_id", "=", self.account_analytic_id.id))
         domain.append(("amount", "=", (self.amount * -1)))
         _logger.info("domain: %s", str(domain))
         line_analytic_line = self.account_analytic_id.line_ids
