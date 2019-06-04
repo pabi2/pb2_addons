408508ef4d8c27624485b8ab88f61eab8314b26e
developer <developer@10.0.2.15>
2019-06-04 13:49:33 +0700

    #3438 - edit advance payment

--- pabi_account_report/reports/xlsx_report_advance_payment.py
+++ pabi_account_report/reports/xlsx_report_advance_payment.py
@@ -3,18 +3,6 @@ from openerp import models, fields, api
 
 
 
-class XLSXReportAdvancePaymentView(models.AbstractModel):
-    _name = 'xlsx.report.advance.payment.view'
-    _inherit = 'account.move.line'
-
-    move_line_id = fields.Many2one(
-        'account.move.line',
-        string='Move Line',
-    )
-    operating_unit = fields.Many2one(
-        'operating.unit',
-        string='Org',
-    )
 
 
 class XLSXReportAdvancePayment(models.TransientModel):
@@ -36,7 +24,7 @@ class XLSXReportAdvancePayment(models.TransientModel):
         domain=lambda self: self._get_domain_account_ids(),
     )
     results = fields.Many2many(
-        'xlsx.report.advance.payment.view',
+        'account.move.line',
         string='Results',
         compute='_compute_results',
         help='Use compute fields, so there is nothing store in database',
@@ -71,42 +59,10 @@ class XLSXReportAdvancePayment(models.TransientModel):
             dom += [('account_id', 'in', self.account_ids.ids)]
         elif not self.account_ids:
             dom += [('account_id', 'in', self._get_account_ids())]
-            
-        #self.results = Result.search(dom).sorted(key=lambda l: (l.invoice.operating_unit_id.name, l.partner_id.search_key,))
-        res_ids = Result.search(dom)
-        res_ids = str(tuple(res_ids.ids))
-        self._cr.execute("""
-            select mol.id as move_line_id, mol.name as name,
-            mol.date as date, mol.date_maturity as date_maturity,
-            mol.move_id as move_id, mol.partner_id as partner_id,
-            mol.reconcile_id as reconcile_id, mol.currency_id as currency_id,
-            mol.period_id as period_id, mol.account_id as account_id,
-            mol.document as document, mol.document_id as document_id
-            from account_move_line mol
-            left join account_move mov on mov.id = mol.move_id
-            left join res_partner part on part.id = mol.partner_id
-            left join account_move_reconcile mov_rec on mov_rec.id = mol.reconcile_id
-            left join account_account acc on acc.id = mol.account_id
-            left join res_currency cur on cur.id = mol.currency_id
-            where mol.id in """+(res_ids)+"""
-            order by acc.code, part.search_key
-        """)
+        
+        self.results = Result.search(dom).sorted(key=lambda l: (l.account_id.code, l.invoice.operating_unit_id.name, l.partner_id.search_key,))
+        
 
-        results = self._cr.dictfetchall()
-        ReportLine = self.env['xlsx.report.advance.payment.view']
-        res = []
-        for line in results:
-            mol = self.env["account.move.line"].browse(line['move_line_id'])
-            mov = self.env["account.move"].browse(line['move_id'])
-            
-            line['operating_unit'] = mol.invoice.operating_unit_id.id or mol.org_id.operating_unit_id.id or False
-            res += [line]   
-        res = sorted(res, key=lambda k: (k['account_id'], k['operating_unit']))
-        
-        for line in res:
-            self.results += ReportLine.new(line)
-        
-        return True
 
 
   
