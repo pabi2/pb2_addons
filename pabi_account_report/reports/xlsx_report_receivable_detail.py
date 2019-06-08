3389dfd61663ad40df821e3acb0ad4c347814072
szkai <42172571+szkai@users.noreply.github.com>
2019-06-08 17:57:41 +0700

    Update xlsx_report_receivable_detail.py

--- pabi_account_report/reports/xlsx_report_receivable_detail.py
+++ pabi_account_report/reports/xlsx_report_receivable_detail.py
@@ -6,30 +6,6 @@ import pandas as pd
 import re
 
 
-class XLSXReportReceivableDetailView(models.AbstractModel):
-    _name = 'xlsx.report.receivable.detail.view'
-    _inherit = 'account.move.line'
-
-    move_line_id = fields.Many2one(
-        'account.move.line',
-        string='Move Line',
-    )
-    move_name = fields.Text(
-        string='Move Name',
-    )
-    move_date = fields.Date(
-        string='Move Date',
-    )
-    number_preprint = fields.Text(
-        string='Preprint Number',
-    )
-    amount_preprint = fields.Float(
-        string='Preprint Amount',
-    )
-
-    
-    
-
 class XLSXReportReceivableDetail(models.TransientModel):
     _name = 'xlsx.report.receivable.detail'
     _inherit = 'report.account.common'
@@ -167,68 +143,6 @@ class XLSXReportReceivableDetail(models.TransientModel):
         for x in Moveline.search(dom):
             res_ids += [x.id if x.move_id.doctype == 'adjustment' or x.date_maturity else False]
         res_ids = list(filter(lambda l: l != False, res_ids))
-        #self.results = Result.search(res).sorted(
-        #    key=lambda l: (l.partner_id.search_key, l.date, l.move_id.name))
-        res_ids = str(tuple(res_ids + [0]))
-        self._cr.execute("""
-            select mol.id as move_line_id, mol.date as date, mol.date_maturity as date_maturity,
-            mol.move_id as move_id, mol.partner_id as partner_id,
-            mol.reconcile_id as reconcile_id, mol.currency_id as currency_id,
-            mol.document as document, mol.document_id as document_id
-            from account_move_line mol
-            left join account_move mov on mov.id = mol.move_id
-            left join res_partner part on part.id = mol.partner_id
-            left join account_move_reconcile mov_rec on mov_rec.id = mol.reconcile_id
-            left join res_currency cur on cur.id = mol.currency_id
-            where mol.id in """+(res_ids)+"""
-            order by part.search_key, mol.date, mov.name
-        """)
+        self.results = Result.search(res_ids).sorted(
+            key=lambda l: (l.partner_id.search_key, l.date, l.move_id.name))
 
-        results = self._cr.dictfetchall()
-        ReportLine = self.env['xlsx.report.receivable.detail.view']
-        for line in results:
-            mol = self.env["account.move.line"].browse(line['move_line_id'])
-            mov = self.env["account.move"].browse(line['move_id'])
-            if mov.document_id and mov.document_id._name == "account.invoice" and mov.id != mov.document_id.cancel_move_id.id:
-                move_name = []
-                move_date = []
-                number_preprint = []
-                amount_preprint = []
-                payment_ids = mov.document_id.payment_ids       
-                for payment in payment_ids:
-                    move_name += [payment.move_id.doctype == "receipt" and payment.move_id.name or False]
-                    move_date += [payment.move_id.doctype == "receipt" and payment.move_id.date or False]
-                    number_preprint += [payment.move_id.doctype == "receipt" and payment.move_id.document_id._name == "account.voucher" and payment.move_id.document_id.number_preprint or False]
-                    amount_preprint += [payment.move_id.doctype == "receipt" and payment.move_id.document_id._name == "account.voucher" and payment.document_id.amount or 0]
-                    
-                move_name = ", ".join(list(filter(lambda l: l != False, move_name)))
-                move_date = ", ".join(list(filter(lambda l: l != False, move_date)))
-                number_preprint = ", ".join(list(filter(lambda l: l != False, number_preprint)))
-                amount_preprint = sum(list(filter(lambda l: l != False, amount_preprint)))
-            
-            elif mov.document_id and mov.document_id._name == "interface.account.entry":
-                search_line = self.env["interface.account.entry"].search([("number", "=", mov.document_id.to_payment)]).line_ids.filtered(lambda l: l.account_id.code == mol.account_id.code and l.name == mol.name and l.id != mol.id)
-                move_name = mov.document_id.to_payment
-                move_date = self.env["interface.account.entry"].search([("number", "=", mov.document_id.to_payment)]).move_id.date
-                number_preprint = self.env["interface.account.entry"].search([("number", "=", mov.document_id.to_payment)]).preprint_number
-                amount_preprint = sum(search_line.mapped("credit")) - sum(search_line.mapped("debit"))
-            else:
-                move_name = None
-                move_date = None
-                number_preprint = None
-                amount_preprint = None
-            
-            line['move_name'] = move_name
-            line['move_date'] = move_date
-            line['number_preprint'] = number_preprint
-            line['amount_preprint'] = amount_preprint
-            
-            self.results += ReportLine.new(line)
-        return True
-
-
-        
-        
-        
-
-        
