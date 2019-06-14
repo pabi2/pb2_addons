d5383e2b7e3a73d5774b04f29d6543002b396e3d
developer <developer@10.0.2.15>
2019-06-14 16:42:11 +0700

    #3492 - update

--- pabi_utils/wizard/export_xlsx_template.py
+++ pabi_utils/wizard/export_xlsx_template.py
@@ -26,13 +26,42 @@ from openerp.addons.connector.queue.job import job, related_action
 from openerp.addons.connector.session import ConnectorSession
 from openerp.addons.connector.exception import RetryableJobError
 
+
 @job
-def action_done_async_process(session, model_name, res_id):
+def get_report_job(session, model_name, res_id, lang=False):
     try:
-        res = session.pool[model_name].action_export(session.cr, session.uid, [res_id], session.context)
-        return {'result': res}
+        # Update context
+        ctx = session.context.copy()
+        if lang:
+            ctx.update({'lang': lang})
+        out_file, out_name = session.pool[model_name].get_report(
+            session.cr, session.uid, [res_id], ctx)
+        # Make attachment and link ot job queue
+        job_uuid = session.context.get('job_uuid')
+        job = session.env['queue.job'].search([('uuid', '=', job_uuid)],
+                                              limit=1)
+        # Get init time
+        date_created = fields.Datetime.from_string(job.date_created)
+        ts = fields.Datetime.context_timestamp(job, date_created)
+        init_time = ts.strftime('%d/%m/%Y %H:%M:%S')
+        # Create output report place holder
+        desc = 'INIT: %s\n> UUID: %s' % (init_time, job_uuid)
+        session.env['ir.attachment'].create({
+            'name': out_name,
+            'datas': out_file,
+            'datas_fname': out_name,
+            'res_model': 'queue.job',
+            'res_id': job.id,
+            'type': 'binary',
+            'parent_id': session.env.ref('pabi_utils.dir_spool_report').id,
+            'description': desc,
+            'user_id': job.user_id.id,
+        })
+        # Result Description
+        result = _('Successfully created excel report : %s') % out_name
+        return result
     except Exception, e:
-        raise RetryableJobError(e)
+        raise FailedJobError(e)
 
 
 def adjust_cell_formula(value, k):
@@ -809,7 +838,7 @@ class ExportXlsxTemplate(models.TransientModel):
         self.ensure_one()
         if self.async_process == True:
             if self._context.get('job_uuid', False):  # Called from @job
-                return self.act_getfile()
+                return self.act_getfile_job()
             session = ConnectorSession(self._cr, self._uid, self._context)
             description = 'Export Xlsx - %s' % (self.res_model or self.name)
             uuid = action_done_async_process.delay(session, self._name, self.id, description=description)
@@ -817,15 +846,31 @@ class ExportXlsxTemplate(models.TransientModel):
         else:
             return self.act_getfile()
 
+
+        
     @api.multi
     def act_getfile(self):
         self.ensure_one()
-        out_file, out_name = self._export_template(self.template_id,
-                                                   self.res_model, self.res_id)
-        self.write({'state': 'get', 'data': out_file, 'name': out_name})
+        # Enqueue
+        if self.async_process:
+            Job = self.env['queue.job']
+            session = ConnectorSession(self._cr, self._uid, self._context)
+            description = 'Excel Report - %s' % (self._name, )
+            uuid = get_report_job.delay(
+                session, self._name, self.id, description=description,
+                lang=session.context.get('lang', False))
+            job = Job.search([('uuid', '=', uuid)], limit=1)
+            # Process Name
+            job.process_id = self.env.ref('pabi_utils.xlsx_report')
+            self.write({'state': 'get', 'uuid': uuid})
+            
+        else:
+            out_file, out_name = self.get_report()
+            self.write({'state': 'get', 'data': out_file, 'name': out_name})
+            
         return {
             'type': 'ir.actions.act_window',
-            'res_model': 'export.xlsx.template',
+            'res_model': self._name,
             'view_mode': 'form',
             'view_type': 'form',
             'res_id': self.id,
