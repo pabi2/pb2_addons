8c2645a1ff933dc5302d4685f2bfdf64cdbb920d
developer <developer@10.0.2.15>
2019-06-18 11:08:45 +0700

    #3509 - edit call function 003 and 006

--- payment_export_text/wizard/export_parser.py
+++ payment_export_text/wizard/export_parser.py
@@ -106,10 +106,43 @@ class DocumentExportParser(models.TransientModel):
                         value = line['default_value'] and\
                             line['default_value'] or ''
                         line.update({'value': value})
-                data_list003.append(line_detail_config_lines)
+                #data_list.append(line_detail_config_lines)
+                
+                # If use invoice_detail
+                voucher_lines = export_line.voucher_id.line_ids
+                for voucher_line in voucher_lines:
+                    for invoice_line in voucher_line:
+                        line_invoice_detail_config_lines = config_id.invoice_detail_config_line_ids.read(config_fields_to_read)
+                        for line in line_invoice_detail_config_lines:
+                            model_id = active_model.id
+                            if line.get('model_id', []):
+                                model_id = line['model_id'][0]
+                            if model_id == active_model.id:
+                                eval_context = self._get_eval_context(
+                                    active_model.id, active_id)
+                            else:
+                                eval_context = self._get_eval_context(
+                                    model_id, invoice_line.id)
+                            if line['field_code']:
+                                #eval(line['field_code'], eval_context,
+                                #     mode="exec", nocopy=True)
+                                exec(line['field_code'], eval_context)
+                                value = eval_context.get('value', False)
+                                line.update({'value': value})
+                            else:
+                                value = line['default_value'] and\
+                                    line['default_value'] or ''
+                                line.update({'value': value})
+            
+            
+                        data_list.append(line_detail_config_lines)
+                        data_list.append(line_invoice_detail_config_lines)
+
+
+                        
 
         # If use invoice_detail
-        if not config_id.invoice_detail_disabled:
+        """if not config_id.invoice_detail_disabled:
             voucher_lines = [x.voucher_id.line_ids for x in export_lines]
             if export_lines and voucher_lines:
                 for voucher_line in voucher_lines:
@@ -135,13 +168,8 @@ class DocumentExportParser(models.TransientModel):
                                 value = line['default_value'] and\
                                     line['default_value'] or ''
                                 line.update({'value': value})
-                        data_list006.append(line_invoice_detail_config_lines)
+                        data_list.append(line_invoice_detail_config_lines)"""
                         
-        for list in range(len(data_list003)):
-            try: data_list.append(data_list003[list-1])
-            except : pass
-            try: data_list.append(data_list006[list-1])
-            except : pass
 
         # If not use footer
         if config_id.footer_disabled:
