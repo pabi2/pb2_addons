d3e9b12d8969326efcaa5aaa80be3b9a30123d1d
developer <developer@10.0.2.15>
2019-06-28 17:55:35 +0700

    Report PrePrint

--- pabi_account_report/reports/xlsx_report_preprint_receipt.py
+++ pabi_account_report/reports/xlsx_report_preprint_receipt.py
@@ -34,10 +34,10 @@ class AccountMove(models.Model):
                 lines = Fund.search(domain)
                 move.preprint_number = lines.invoice_number                                        
 
-class Accountmovepreprintview(models.Model):
+class Accountmovepreprint(models.Model):
     _name = 'account.move.preprint'    
     #_auto = False    
-
+ 
     move_id = fields.Many2one(
         'account.move',
         string='Document No',
@@ -50,19 +50,50 @@ class Accountmovepreprintview(models.Model):
         'res.org',
         string='Org',
     )
-
+    
+class AccountMovePrePrintView(models.AbstractModel):
+    """ Contrast to normal view, this will be used as mock temp table only """
+    _name = 'account.move.preprint.view'
+    _inherit = 'account.move'
+    
+    number_preprint = fields.Char(
+        string='Preprint Number',
+    )
+    document_origin = fields.Char(
+        string='Preprint Number',
+    )
+    operating_unit = fields.Many2one(
+        'operating.unit',
+        string='Org',
+    )
+    amount = fields.Float(
+        string='Amount',
+    )
+    base = fields.Float(
+        string='Base Code Amount',
+    )
+    taxbranch_id = fields.Many2one(
+        'res.taxbranch',
+        string='Tax Branch',
+    )
+    
+    
 class XLSXReportGlProject(models.TransientModel):
     _name = 'xlsx.report.preprint.receipt'
     _inherit = 'report.account.common'
 
+    filter = fields.Selection(
+        readonly=True,
+        default='filter_period',
+    )
     postingdate_start = fields.Date(
         string='Start Posting Date',
     )
     postingdate_end = fields.Date(
         string='End Posting Date',
     )
-    org_ids = fields.Many2many(
-        'res.org',
+    operating_unit_ids = fields.Many2many(
+       'operating.unit',
         string='Org',
     )
     move_id = fields.Many2many(
@@ -70,8 +101,14 @@ class XLSXReportGlProject(models.TransientModel):
         string='Document No',
         domain=[('doctype','in',['adjustment','receipt','interface_account'])]
     )
+#     results = fields.Many2many(
+#         'account.move',
+#         string='Results',
+#         compute='_compute_results',
+#         help='Use compute fields, so there is nothing store in database',
+#     )
     results = fields.Many2many(
-        'account.move',
+        'account.move.preprint.view',
         string='Results',
         compute='_compute_results',
         help='Use compute fields, so there is nothing store in database',
@@ -165,12 +202,48 @@ class XLSXReportGlProject(models.TransientModel):
                 """
         )    
 
+#     @api.multi
+#     def _compute_results(self):
+#         self.ensure_one()
+#         Result = self.env['account.move']
+#         dom = []
+#         domain=[]
+#         if self.period_start_id:
+#             dom += [('date', '>=', self.period_start_id.date_start)]
+#         if self.period_end_id:
+#             dom += [('date', '<=', self.period_end_id.date_stop)]
+#         if self.date_start:
+#             dom += [('date', '>=', self.date_start)]
+#         if self.date_end:
+#             dom += [('date', '<=', self.date_end)]
+#         if self.postingdate_start:
+#             dom += [('date', '>=', self.postingdate_start)]
+#         if self.postingdate_end:
+#             dom += [('date', '<=', self.postingdate_end)]
+#         if self.move_id:
+#             domain += [('move_id', 'in', self.move_id.ids)]
+#         if self.org_ids:
+#             domain += [('org_id', 'in', self.org_ids.ids)]
+#         if self.preprint_number:
+#             domain += [('id', 'in', self.preprint_number.ids)]
+#         if domain:
+#             preprint=self.env['account.move.preprint'].search(domain)
+#             dom += [('id', 'in', [x.move_id.id for x in preprint])]
+#   
+#         self.results = Result.with_context(active=False).search(dom)
+
+    @api.model
+    def _domain_to_where_str(self, domain):
+        """ Helper Function for better performance """
+        where_dom = [" %s %s %s " % (x[0], x[1], isinstance(x[2], basestring)
+                     and "'%s'" % x[2] or x[2]) for x in domain]
+        where_str = 'and'.join(where_dom)
+        return where_str
+    
     @api.multi
     def _compute_results(self):
         self.ensure_one()
-        Result = self.env['account.move']
         dom = []
-        domain=[]
         if self.period_start_id:
             dom += [('date', '>=', self.period_start_id.date_start)]
         if self.period_end_id:
@@ -183,14 +256,86 @@ class XLSXReportGlProject(models.TransientModel):
             dom += [('date', '>=', self.postingdate_start)]
         if self.postingdate_end:
             dom += [('date', '<=', self.postingdate_end)]
+          
+        #prefix
+        whr_prefix= ""
+        m_id = []
         if self.move_id:
-            domain += [('move_id', 'in', self.move_id.ids)]
-        if self.org_ids:
-            domain += [('org_id', 'in', self.org_ids.ids)]
+            for id in self.move_id.ids:
+                m_id.append(id) #dom += [('m.id', 'in', self.move_id.ids)]
+        if self.operating_unit_ids:
+            if len(self.operating_unit_ids.ids) > 1: 
+                whr_prefix += """org.operating_unit_id IN %s  """ % (tuple(self.operating_unit_ids.ids),)
+            else:
+                whr_prefix += """org.operating_unit_id = %s  """ % self.operating_unit_ids.id
+            #dom += [('org.operating_unit_id', 'in',','.join(str(x) for x in self.operating_unit_ids.ids))]
         if self.preprint_number:
-            domain += [('id', 'in', self.preprint_number.ids)]
-        if domain:
-            preprint=self.env['account.move.preprint'].search(domain)
-            dom += [('id', 'in', [x.move_id.id for x in preprint])]
-
-        self.results = Result.with_context(active=False).search(dom)
+            for id in [x.move_id.id for x in self.preprint_number]:
+                m_id.append(id)
+        if m_id:
+            if len(m_id) > 1: 
+                whr_prefix += """m.id IN %s  """ % (tuple(m_id),)
+            else:
+                whr_prefix += """m.id = %s  """ % m_id[0]
+            #dom += [('m.id', 'in', tuple(self.preprint_number.ids))]
+                      
+        whr_depreciation = ""   
+        where_str = self._domain_to_where_str(dom) 
+        if where_str:
+            where_str = ' and '+ where_str
+        if whr_prefix and where_str:
+           where_str = where_str + ' and ' + whr_prefix   
+        if whr_prefix and(not where_str):
+            where_str = ' and ' + whr_prefix   
+           
+        self._cr.execute("""
+                       select m.*,aa.number_preprint,aa.amount,aa.base,aa.taxbranch_id ,
+                       org.operating_unit_id as operating_unit,org.document_origin
+                       from 
+                       (
+                       --RC
+                       select account_move.id as move_id,account_voucher.number_preprint as number_preprint,
+                                account_tax_detail.amount,account_tax_detail.base as base,account_tax_detail.taxbranch_id
+                            from account_move 
+                            LEFT JOIN account_voucher on account_voucher.move_id = account_move."id"
+                            INNER JOIN account_tax_detail on account_tax_detail.ref_move_id = account_move."id"
+                            WHERE account_tax_detail.doc_type = 'sale'
+                        UNION 
+                        -- IA
+                        select account_move.id as move_id,interface_account_entry.preprint_number as number_preprint,
+                                    account_tax_detail.amount,account_tax_detail.base as base,account_tax_detail.taxbranch_id
+                                from account_move 
+                                LEFT JOIN interface_account_entry on interface_account_entry.move_id = account_move."id"
+                                LEFT JOIN account_move_line on account_move_line.move_id = account_move.id
+                                INNER JOIN account_tax_detail on account_tax_detail.ref_move_id = account_move."id"
+                                WHERE account_tax_detail.doc_type = 'sale'
+                            UNION 
+                            --CV
+                            select account_move.id as move_id,account_tax_detail.number_preprint as number_preprint,
+                                        account_tax_detail.amount,account_tax_detail.base as base,account_tax_detail.taxbranch_id
+                                from account_move 
+                                LEFT JOIN account_tax_detail on account_tax_detail.ref_move_id = account_move."id"
+                                WHERE account_tax_detail.doc_type = 'sale')aa
+       
+                            LEFT JOIN 
+                            (
+                            --SELECT DISTINCT m.id,m.name,l.org_id as org_id  from account_move m
+                             --   LEFT JOIN account_move_line l ON l.move_id = m.id
+                             --   where l.org_id is not null
+                            --UNION
+                                select am.id,am.name,am.operating_unit_id as operating_unit_id,
+                                interface_account_entry.name as document_origin  
+                                from account_move am
+                                LEFT JOIN interface_account_entry on interface_account_entry.move_id = am."id"
+                            ) org on org."id" = aa.move_id
+                            LEFT JOIN account_move m on m.id = aa.move_id
+   
+                        where aa.number_preprint!=''
+                     
+        """  + where_str + ' order by aa.number_preprint ' )     
+           
+        results = self._cr.dictfetchall()
+        ReportLine = self.env['account.move.preprint.view']
+        for line in results:
+            self.results += ReportLine.new(line)
+        return True
