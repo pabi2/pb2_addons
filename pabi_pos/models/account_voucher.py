025c9577c13eb1dcfc85e3eb32ad0d811980b336
Sureerat Meepoo <sureerat.aom1504@gmail.com>
2019-09-10 14:09:13 +0700

    test

--- pabi_pos/models/account_voucher.py
+++ pabi_pos/models/account_voucher.py
@@ -15,6 +15,7 @@ class AccountVoucher(models.Model):
         if self.line_ids and \
             self.line_ids[0].move_line_id and \
             self.line_ids[0].move_line_id.move_id.document_id and \
+            self.line_ids[0].move_line_id.move_id.document_id.source_document_id and \
             'POS' in self.line_ids[0].move_line_id.move_id.document_id.source_document_id.name:
             picking = self.env['stock.picking'].search([('origin','=',self.line_ids[0].move_line_id.move_id.document_id.source_document_id.name)])
             for pick in picking:
