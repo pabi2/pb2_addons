498e8994ca0c6c32bb39ba7c644c1a1557abf773
Sureerat Meepoo <sureerat.aom1504@gmail.com>
2019-09-13 16:30:09 +0700

    #3529-PosStock_1309 Module: pabi_pos update validate voucher

--- pabi_pos/models/account_voucher.py
+++ pabi_pos/models/account_voucher.py
@@ -15,6 +15,7 @@ class AccountVoucher(models.Model):
         for voucher in self:
             if voucher.line_ids and \
                 voucher.line_ids[0].move_line_id and \
+                'DV' in voucher.line_ids[0].move_line_id.move_id.document and \
                 voucher.line_ids[0].move_line_id.move_id.document_id and \
                 voucher.line_ids[0].move_line_id.move_id.document_id.source_document_id and \
                 'POS' in voucher.line_ids[0].move_line_id.move_id.document_id.source_document_id.name:
