48814b27b590d3d348ad39199b61a1d1c4b05c3b
Sureerat Meepoo <sureerat.aom1504@gmail.com>
2019-10-01 16:22:38 +0700

    #3868-PND1 Module: pabi_account_report https://mobileapp.nstda.or.th/redmine/issues/3868#change-13684 แก้ไข filter fields Calendar Period

--- pabi_account_report/reports/xlsx_report_pnd1.py
+++ pabi_account_report/reports/xlsx_report_pnd1.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 from openerp import models, fields, api
-
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 class XLSXReportPND1(models.TransientModel):
     _name = 'xlsx.report.pnd1'
     _inherit = 'report.account.common'
-
+    
     filter = fields.Selection(
         readonly=True,
         default='filter_period',
@@ -14,7 +15,7 @@ class XLSXReportPND1(models.TransientModel):
         'account.period.calendar',
         string='Calendar Period',
         required=True,
-        default=lambda self: self.env['account.period.calendar'].find(),
+        default=lambda self: self.env['account.period.calendar'].find()
     )
     results = fields.Many2many(
         'personal.income.tax',
@@ -22,6 +23,17 @@ class XLSXReportPND1(models.TransientModel):
         compute='_compute_results',
         help='Use compute fields, so there is nothing store in database',
     )
+    
+    @api.onchange('fiscalyear_start_id', 'fiscalyear_end_id')
+    def _onchange_fiscalyear_id(self):
+        if self.fiscalyear_start_id and self.fiscalyear_end_id:
+            domain = [('company_id', '=', self.company_id.id),
+                      ('date_start', '>=', (datetime.strptime(str(self.fiscalyear_start_id.date_start), '%Y-%m-%d')-relativedelta(months=3)).strftime('%Y-%m-%d')), 
+                      ('date_stop', '<=', self.fiscalyear_end_id.date_stop)]
+        else:
+            domain =  [('company_id', '=', self.company_id.id)]
+            
+        return {'domain': {'calendar_period_id': domain}}
 
     @api.multi
     def _compute_results(self):
