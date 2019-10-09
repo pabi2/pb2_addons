2112d35f809f488b150c1aabf9f22347a5343804
Saksit Muangsamai <saksit.mun@ncr.nstda.or.th>
2019-10-09 08:57:16 +0700

    #3888_SLA_Expense_supplier
    Module : pabi_account_report
    FI : เพิ่มเงื่อนไขการเรียกรายงาน และเช็คการแสดงผล ฟิลด์ Accepted Date
    Redmine : https://mobileapp.nstda.or.th/redmine/issues/3888

--- pabi_account_report/reports/xlsx_report_sla_employee.py
+++ pabi_account_report/reports/xlsx_report_sla_employee.py
@@ -195,7 +195,7 @@ class XLSXReportSLAEmployee(models.TransientModel):
     supplier_category_name = fields.Selection(
         [('employee', 'พนักงาน สวทช'),
          ('supplier', 'Supplier ภายนอก'),
-         ('foreign', 'ต่างประเทศ')],
+         ('foreign', 'Supplier ต่างประเทศ')],
         string='Supplier Category',
     )
     async_process = fields.Boolean(
@@ -214,16 +214,13 @@ class XLSXReportSLAEmployee(models.TransientModel):
         self.ensure_one()
         Result = self.env['sla.employee.view']
         dom = []
-        # dom = [('invoice_id.source_document_type', 'in',
-        # ['advance', 'expense']),
-        # ('expense_id.pay_to', '!=', 'supplier')]
         if self.supplier_category_name:
             if self.supplier_category_name == 'employee':
                 dom += [('pay_to', '=', 'employee')]
-            elif self.supplier_category_name == 'foreign':
-                dom += [('pay_to', '!=', 'employee'),('invoice_id.partner_id.category_id.name', '=', 'ต่างประเทศ')]
             elif self.supplier_category_name == 'supplier':
                 dom += [('pay_to', '!=', 'employee'),('invoice_id.partner_id.category_id.name', '!=', 'ต่างประเทศ')]
+            elif self.supplier_category_name == 'foreign':
+                dom += [('pay_to', '!=', 'employee'),('invoice_id.partner_id.category_id.name', '=', 'ต่างประเทศ')]
         if self.user_ids:
             dom += [('voucher_id.validate_user_id', 'in', self.user_ids.ids)]
         if self.source_document_type:
