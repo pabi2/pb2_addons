bb8d14fb764e6687fbaf55104c1b346bdc04ba7e
Sirichai Chitvises <sirichaic@nstda.or.th>
2019-11-04 15:27:01 +0700

    add log

--- pabi_asset_management/wizard/stock_transfer_details.py
+++ pabi_asset_management/wizard/stock_transfer_details.py
@@ -57,10 +57,8 @@ class StockTransferDetails(models.TransientModel):
             self = self.with_context({'work_acceptance_id': wa.id,
                                       'installment': wa.installment,
                                       'num_installment': wa.num_installment})
-        _logger.info("self.id: %s", str(self.id))
-        _logger.info("self.asset_ids: %s", str(self.asset_ids))
-        _logger.info("self._context: %s", str(self._context))
+        _logger.info("self.picking_id: %s", str(self.picking_id))
+        _logger.info("self.picking_id.asset_ids: %s", str(self.picking_id.asset_ids))
         res = super(StockTransferDetails, self).do_detailed_transfer()
-        _logger.info("self.asset_ids: %s", str(self.asset_ids))
-        _logger.info("res: %s", str(res))
+        _logger.info("self.picking_id.asset_ids: %s", str(self.picking_id.asset_ids))
         return res
