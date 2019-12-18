ba92c2e55a6ff6f3b6304d32b2e1a3f736fb5375
Saran <saranp502@hotmail.com>
2019-12-18 16:58:59 +0700

    [FIX] validate stock pos

--- pabi_pos/models/stock.py
+++ pabi_pos/models/stock.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from openerp import models, api
+from openerp import models, api, _
+from openerp.exceptions import ValidationError
 
 
 class StockMove(models.Model):
@@ -17,25 +18,29 @@ class StockMove(models.Model):
                     'location_id': picking.workflow_process_id.location_id.id})
         return res
 
-class StockPicking(models.Model):
 
+class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
     @api.multi
     def do_enter_transfer_details(self):
         if 'POS' in self.origin:
-            #self.validate_picking()
+            for line in self.move_lines:
+                if line.product_uom_qty > \
+                        sum(line.reserved_quant_ids.mapped('qty')):
+                    raise ValidationError(
+                        _('%s don\'t have enough quantity')
+                        % line.product_id.name)
+            # self.validate_picking()
             only_available = self.filtered(
                 'workflow_process_id.ship_only_available')
             to_force = self - only_available
             if only_available:
-                #only_available.action_assign()
-                #only_available.do_prepare_partial()
+                # only_available.action_assign()
+                # only_available.do_prepare_partial()
                 only_available.do_transfer()
             if to_force:
-                #to_force.force_assign()
+                # to_force.force_assign()
                 to_force.do_transfer()
         else:
             return super(StockPicking, self).do_enter_transfer_details()
-        
-        
