cca4b6123f68b0c21ebb15693ab5ce77e54fa84d
Waritchapat Kaewsuriyaporn <waritchapat.kae@ncr.nstda.or.th>
2020-01-14 16:09:27 +0700

    #4131_edit_Revenue_in_Budget_Monitor
    Module: account_budget_activity
    BG: ปรับปรุงข้อมูลใน Tab Budget Monitor ส่วน Revenue
    Issues: https://mobileapp.nstda.or.th/redmine/issues/4131
    (แก้ไขเพิ่มเติม)

--- account_budget_activity/report/budget_monitor_dimension.py
+++ account_budget_activity/report/budget_monitor_dimension.py
@@ -67,13 +67,13 @@ class MonitorView(models.AbstractModel):
                 (ORDER BY budget_method, charge_type, fiscalyear_id, %s) AS id,
                 budget_method, charge_type, fiscalyear_id,
                 %s,
-                sum(planned_amount) planned_amount,
-                sum(released_amount) released_amount,
-                sum(amount_so_commit) amount_so_commit,
-                sum(amount_pr_commit) amount_pr_commit,
-                sum(amount_po_commit) amount_po_commit,
-                sum(amount_exp_commit) amount_exp_commit,
-                sum(amount_actual) amount_actual,
+                COALESCE(sum(planned_amount),0) planned_amount,
+                COALESCE(sum(released_amount),0) released_amount,
+                COALESCE(sum(amount_so_commit),0) amount_so_commit,
+                COALESCE(sum(amount_pr_commit),0) amount_pr_commit,
+                COALESCE(sum(amount_po_commit),0) amount_po_commit,
+                COALESCE(sum(amount_exp_commit),0) amount_exp_commit,
+                COALESCE(sum(amount_actual),0) amount_actual,
                 sum(amount_so_commit) + sum(amount_pr_commit) + 
                 sum(amount_po_commit) + sum(amount_exp_commit) + 
                 sum(amount_actual) as amount_consumed,
@@ -85,7 +85,7 @@ class MonitorView(models.AbstractModel):
     """
     #sum(amount_so_commit) + sum(amount_actual) as amount_consumed,
     #sum(amount_balance) amount_balance 
-           
+    #COALESCE(sum(amount_so_commit),0)       
     def _create_monitor_view(self, cr, field):
         conds = [x.strip() + ' is not null' for x in field.split(',')]
         where = ' and '.join(conds)
