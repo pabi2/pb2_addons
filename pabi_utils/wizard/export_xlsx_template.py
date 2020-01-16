c6ed1a3a111ff076bf3ee2287f27046c146bf283
Sureerat Meepoo <sureerat.mee@nstda.or.th>
2020-01-16 16:54:06 +0700

    #4126-PurchaseInvoicePlanReport Module: pabi_utils **แก้ไข Special Character https://mobileapp.nstda.or.th/redmine/issues/4126

--- pabi_utils/wizard/export_xlsx_template.py
+++ pabi_utils/wizard/export_xlsx_template.py
@@ -810,7 +810,7 @@ class ExportXlsxTemplate(models.TransientModel):
         ptemp = ConfParam.get_param('path_temp_file') or '/temp'
         stamp = dt.utcnow().strftime('%H%M%S%f')[:-3]
         ftemp = '%s/temp%s.xlsx' % (ptemp, stamp)
-        f = open(ftemp, 'wb+')
+        f = open(ftemp, 'w')
         f.write(decoded_data)
         f.seek(0)
         f.close()
