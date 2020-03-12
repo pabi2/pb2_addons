7416762b55cd1c578eed90d25e832a97af2f2550
Sureerat Meepoo <sureerat.mee@nstda.or.th>
2020-03-12 17:19:53 +0700

    test

--- pabi_account_move_adjustment/models/account.py
+++ pabi_account_move_adjustment/models/account.py
@@ -441,7 +441,7 @@ class AccountMoveLine(MergedChartField, models.Model):
             if rec.activity_group_id and \
                     not rec.activity_id and not rec.activity_rpt_id:
                 
-                print('\n activity_group_id: %s\n activity_id: %s\n activity_rpt_id: %s'+(
+                print('\n activity_group_id: %s\n activity_id: %s\n activity_rpt_id: %s' % (
                     str(rec.activity_group_id),str(rec.activity_id),str(rec.activity_rpt_id)))
                 raise ValidationError(
                     _('Activity is required for activity group!: '+str(rec.document_id)))
