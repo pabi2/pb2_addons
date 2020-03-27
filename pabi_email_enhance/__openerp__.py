d1bd1815a154d5b584a6366a81a49cf5d6b319ba
Chaithat Bunneramit <chaithat.bun@nstda.or.th>
2020-03-27 17:47:47 +0700

    #4240_module_pabi_email_enhance Module : pabi_email_enhance DEV : พัฒนา module : pabi_email_enhance สำหรับส่ง Email จากหน้าจอ Expense Issues : https://mobileapp.nstda.or.th/redmine/issues/4240

---
+++ pabi_email_enhance/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "NSTDA :: PABI2 - Email Enhance",
+    "summary": "",
+    "version": "1.0",
+    "category": "Human Resources",
+    "description": """
+
+    """,
+    "website": "-----",
+    "author": "Pabi 2",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "pabi_hr_expense",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/hr_expense_view.xml",
+    ],
+}
---
+++ pabi_email_enhance/models/hr_expense.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+from openerp import models, fields, api
+from google.auth._default import default
+from openpyxl.worksheet import related
+
+
+class HRExpense(models.Model):
+    _inherit = 'hr.expense.expense'
+ 
+    message_mail = fields.One2many(
+        'hr.email.enhance',
+        'expense_id',
+        string='Message',
+        readonly=True,
+    )
+    
+class PABIEmailEnhance(models.Model):
+    _name = 'hr.email.enhance'
+    
+    expense_id = fields.Many2one(
+        'hr.expense.expense',
+    )
+    mail_id = fields.Many2one(
+        'mail.mail',
+    )
+    subject = fields.Char(
+        string='Subject',
+        related='mail_id.mail_message_id.subject'
+    )
+    recipient_ids = fields.Many2many(
+        'res.partner', 
+        string='To (Partners)',
+        related='mail_id.recipient_ids'
+    )
+    state = fields.Selection([
+            ('outgoing', 'Outgoing'),
+            ('sent', 'Sent'),
+            ('received', 'Received'),
+            ('exception', 'Delivery Failed'),
+            ('cancel', 'Cancelled'),
+        ], 'Status', 
+        readonly=True,
+        copy=False,
+        related='mail_id.state',
+    )
+    
+
+class mail_mail(models.Model):
+    _inherit = 'mail.mail'
+    
+    def create(self, cr, uid, values, context=None):
+        values['model'] = context.get('active_model')
+        values['res_id'] = context.get('active_id')
+        res = super(mail_mail, self).create(cr, uid, values, context=context)
+        self.pool.get('hr.email.enhance').create(cr, uid,{'mail_id': res , 'expense_id': context.get('active_id')})
+        return res
+        
+    
