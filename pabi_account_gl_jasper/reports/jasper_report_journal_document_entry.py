b5c22ca2d6562c87f7cc0ef17123332a18d0c894
Peerapol Chergmarug <peerapol.che@ncr.nstda.or.th>
2020-04-03 16:52:04 +0700

    #4276_Journal_Document_Entry
    Module : pabi_account_gl_jasper
    FI: พัฒนารายงาน Journal Document Entry
    Issues : https://mobileapp.nstda.or.th/redmine/issues/4276

--- pabi_account_gl_jasper/reports/jasper_report_journal_document_entry.py
+++ pabi_account_gl_jasper/reports/jasper_report_journal_document_entry.py
@@ -43,51 +43,67 @@ class JasperReportJournalDocumentEntry(models.TransientModel):
         self.ensure_one()
 #         Result = self.env['account.move.line']
         dom = []
-#         
+         
 #         if self.user_type:
 #             dom = [('account_id.user_type', '=', self.user_type.id)]
-#         if self.account_ids:
+        if self.account_ids:
 #             dom += [('account_id', 'in', self.account_ids.ids)]
-#         if self.operating_unit_ids:
+            dom += [('account_id in ({})'.format(','.join(map(str, (self.account_ids.ids)))))]
+        if self.operating_unit_ids:
 #             dom += [('account_id.operating_unit_id', 'in', self.operating_unit_ids.ids)]
 #             dom += [('partner_id.user_id.default_operating_unit_id', 'in', self.operating_unit_ids.ids)]
-#         if self.chartfield_ids:
-#             # map beetween chartfield_id with chartfield type
-#             chartfields = [('section_id', 'sc:'),
-#                            ('project_id', 'pj:'),
-#                            ('invest_construction_phase_id', 'cp:'),
-#                            ('invest_asset_id', 'ia:'),
-#                            ('personnel_costcenter_id', 'pc:')]
-#             where_str = ''
-#             res_ids = []
-#             for chartfield_id, chartfield_type in chartfields:
-#                 chartfield_ids = self.chartfield_ids.filtered(
-#                     lambda l: l.type == chartfield_type).mapped('res_id')
-#                 if chartfield_ids:
-#                     if where_str:
-#                         where_str += ' or '
-#                     where_str += chartfield_id + ' in %s'
-#                     res_ids.append(tuple(chartfield_ids))
-#             if res_ids:
-#                 sql = "select id from account_move_line where " + where_str
-#                 self._cr.execute(sql, res_ids)
-#                 dom += [('id', 'in', map(lambda l: l[0], self._cr.fetchall()))]
-#         if self.partner_ids:
+            dom += [('operating_unit_id in ({})'.format(','.join(map(str, (self.operating_unit_ids.ids)))))]
+        if self.chartfield_ids:
+            # map beetween chartfield_id with chartfield type
+            chartfields = [('section_id', 'sc:'),
+                           ('project_id', 'pj:'),
+                           ('invest_construction_phase_id', 'cp:'),
+                           ('invest_asset_id', 'ia:'),
+                           ('personnel_costcenter_id', 'pc:')]
+            where_str = ''
+            res_ids = []
+            for chartfield_id, chartfield_type in chartfields:
+                chartfield_ids = self.chartfield_ids.filtered(
+                    lambda l: l.type == chartfield_type).mapped('res_id')
+                if chartfield_ids:
+                    if where_str:
+                        where_str += ' or '
+                    where_str += chartfield_id + ' in %s'
+                    res_ids.append(tuple(chartfield_ids))
+            if res_ids:
+                sql = "select id from account_move_line where " + where_str
+                self._cr.execute(sql, res_ids)
+                dom += [('id', 'in', map(lambda l: l[0], self._cr.fetchall()))]
+                dom += [('id in ({})'.format(','.join(map(str, (map(lambda l: l[0], self._cr.fetchall()))))))]
+        if self.partner_ids:
 #             dom += [('partner_id', 'in', self.partner_ids.ids)]
-#         if self.charge_type:
+            dom += [('partner_id in ({})'.format(','.join(map(str, (self.partner_ids.ids)))))]
+        if self.charge_type:
 #             dom += [('charge_type', '=', self.charge_type)]
-#         if self.fiscalyear_start_id:
+            dom += [('charge_type = {}'.format(self.charge_type))]
+        if self.fiscalyear_start_id:
 #             dom += [('date', '>=', self.fiscalyear_start_id.date_start)]
-#         if self.fiscalyear_end_id:
+            date_start = [(" date >= '{}' ".format(self.fiscalyear_start_id.date_start))]
+        if self.fiscalyear_end_id:
 #             dom += [('date', '<=', self.fiscalyear_end_id.date_stop)]
-#         if self.period_start_id:
+            date_end = [(" date <= '{}' ".format(self.fiscalyear_end_id.date_stop))]
+        if self.period_start_id:
 #             dom += [('date', '>=', self.period_start_id.date_start)]
-#         if self.period_end_id:
+            dom += [(" date >= '{}' ".format(self.period_start_id.date_start))]
+        if self.period_end_id:
 #             dom += [('date', '<=', self.period_end_id.date_stop)]
-#         if self.date_start:
+            dom += [(" date <= '{}' ".format(self.period_end_id.date_stop))]
+        if self.date_start:
 #             dom += [('date', '>=', self.date_start)]
-#         if self.date_end:
+            dom += [(" date >= '{}' ".format(self.date_start))]
+        if self.date_end:
 #             dom += [('date', '<=', self.date_end)]
+            dom += [(" date <= '{}' ".format(self.date_end))]
+        if self.doc_type:
+            dom += [(" journal_id = {} ".format(self.doc_type.id))]
+        if self.doc_number_ids:
+            dom += [(" move_id in ({}) ".format(','.join(map(str, (self.doc_number_ids.ids)))))]
+            
 #         self.results = Result.with_context(active=False).search(dom).sorted(
 #                        key=lambda l: (l.charge_type,
 #                                       l.account_id.code,
@@ -95,30 +111,13 @@ class JasperReportJournalDocumentEntry(models.TransientModel):
 #                                       l.activity_id.code,
 #                                       l.period_id.fiscalyear_id.name,
 #                                       l.period_id.name))
-
-        if self.fiscalyear_start_id:
-            dom += [(" date >= '{}' ".format(self.fiscalyear_start_id.date_start))]
-        if self.fiscalyear_end_id:
-            dom += [(" date <= '{}' ".format(self.fiscalyear_end_id.date_stop))]
-        if self.period_start_id:
-            dom += [(" date >= '{}' ".format(self.period_start_id.date_start))]
-        if self.period_end_id:
-            dom += [(" date <= '{}' ".format(self.period_end_id.date_stop))]
-        if self.date_start:
-            dom += [(" date >= '{}' ".format(self.date_start))]
-        if self.date_end:
-            dom += [(" date <= '{}' ".format(self.date_end))]
-        if self.doc_type:
-            dom += [(" journal_id = {} ".format(self.doc_type.id))]
-        if self.doc_number_ids:
-            dom += [(" move_id in ({}) ".format(','.join(map(str, (self.doc_number_ids.ids)))))]
-
-        return dom
+        return date_start,date_end,dom
+        
             
     @api.multi
     def start_report(self):
         self.ensure_one()
-        where_str = self._compute_results()
+        date_start,date_end,dom = self._compute_results()
         params = {}
 #         params['condition'] = 'and'.join(map(str, where_str)) #error
 #         params['ids'] = [3613143,3613144] #pass
@@ -126,7 +125,7 @@ class JasperReportJournalDocumentEntry(models.TransientModel):
 #         params['condition'] = "date >= \'2019-10-01\' and date <= \'2020-09-30\' and move_id in (628204,627347,626953,624181)" #error
         params['date_start'] = self.fiscalyear_start_id.date_start
         params['date_end'] = self.fiscalyear_end_id.date_stop
-        params['move_id'] = '({})'.format(','.join(map(str, (self.doc_number_ids.ids))))
+        params['move_id'] = 'and'.join(map(str, dom))
         return { 
             'type': 'ir.actions.report.xml',
             'report_name': 'report_journal_document_entry',
