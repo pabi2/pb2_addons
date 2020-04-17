84192efd79bc59073a692d8201b147f0aca356b2
Sirichai Chitvises <sir.chitvises@gmail.com>
2020-04-17 11:31:20 +0700

    เพิ่มตัวแปรเพื่อเก็บว่าจะเอาอัตราแลกเปลี่ยนอันไหน

--- currency_rate_update_th/services/update_service_THB.py
+++ currency_rate_update_th/services/update_service_THB.py
@@ -50,12 +50,28 @@ class THB_getter(Currency_getter_interface):
                                 % str(curr)))
 
             _logger.debug("BOT sent a valid RSS file for: " + curr)
+            
+            # 1. อัตราแลกเปลี่ยนถัวเฉลี่ยที่ธนาคารพาณิชย์ใช้ซื้อขายกับลูกค้า
+            # index = 0 คือ อัตราซื้อ ตั๋วเงิน
+            # index = 1 คือ อัตราซื้อ เงินโอน
+            # index = 2 คือ อัตราขาย
+            # 2. อัตราในตลาดต่างประเทศ (ทอมสันรอยเตอร์) คำนวณผ่านอัตราซื้อขายเงินดอลลาร์ สรอ. ในตลาดกรุงเทพฯ
+            # index = 0 คือ อัตราซื้อ
+            # index = 1 คือ อัตราขาย
+            # 
+            # ตอนนี้ในระบบใช้อัตราขาย
+            index = 2
+            if len(dom.entries) < 2:
+                # ถ้า len(dom.entries) < 2 แปลว่าเป็นสกุลเงินประเภทที่ 2.
+                index = 1
+            else:
+                index = 2
 
             # check for valid exchange data
-            _logger.info("dom.entries[2] : %s" % str(dom.entries[2]))
-            if (dom.entries[2].cb_basecurrency == main_currency) and \
-                    (dom.entries[2].cb_targetcurrency[:3] == curr):
-                value = dom.entries[2].summary_detail.value.split('\n', 1)[0]
+            _logger.info("dom.entries[%s] : %s" % (str(index), str(dom.entries[index])))
+            if (dom.entries[index].cb_basecurrency == main_currency) and \
+                    (dom.entries[index].cb_targetcurrency[:3] == curr):
+                value = dom.entries[index].summary_detail.value.split('\n', 1)[0]
                 rate = value.split('\n', 1)[0].split()[0]
                 factor = value.split('=')[1].split()[0]
                 if rate:
@@ -65,7 +81,7 @@ class THB_getter(Currency_getter_interface):
                 # rate = 1 / rate
 
                 rate_date_datetime =\
-                    datetime.strptime(dom.entries[2].updated, '%Y-%m-%d')
+                    datetime.strptime(dom.entries[index].updated, '%Y-%m-%d')
                 self.check_rate_date(rate_date_datetime, max_delta_days)
 
                 _logger.info("rate : %s" % str(rate))
