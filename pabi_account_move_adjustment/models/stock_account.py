0f23a4c5572f25c3e9c6c7b1518134c0466facd3
kong <rattapong.chok@gmail.com>
2020-05-05 14:05:11 +0700

    Fix-currency_id and add context for create direct

--- pabi_account_move_adjustment/models/stock_account.py
+++ pabi_account_move_adjustment/models/stock_account.py
@@ -11,6 +11,10 @@ class StockQuant(models.Model):
                                   journal_id, context=None):
         # send context stock to account_move
         context.update({'stock_move_id': move})
+        if context.get('default_inv_adjust'):
+            acc_output = move.product_id.categ_id.\
+                property_stock_account_output_categ.id
+            credit_account_id = acc_output
         res = super(StockQuant, self)._create_account_move_line(
                 cr, uid, quants, move, credit_account_id, debit_account_id,
                 journal_id, context)
