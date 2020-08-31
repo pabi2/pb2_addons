834d5bfe7ee59e147ba04ea719d2fcb3e8f2504b
thapanat <thapanat.sop@live.com>
2020-08-31 15:08:26 +0700

    Update __init__.py

--- pabi_etl/models/__init__.py
+++ pabi_etl/models/__init__.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-# from . import issi_master_view
+from . import issi_master_view
 from . import etl_myproject_view
 from . import etl_budget_view
 from . import issi_account_view
-from . import issi_purchase_view
