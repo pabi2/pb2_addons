eec94ff914e6d61383b1c1d3fc5862fbc2f2e503
thapanat <thapanat.sop@live.com>
2020-09-08 16:55:31 +0700

    Update __init__.py
    -- Comment purchase tracking

--- pabi_etl/models/__init__.py
+++ pabi_etl/models/__init__.py
@@ -4,4 +4,4 @@ from . import etl_myproject_view
 from . import res_project_revenue_actual
 from . import etl_budget_view
 from . import issi_account_view
-from . import issi_purchase_view
+# from . import issi_purchase_view
