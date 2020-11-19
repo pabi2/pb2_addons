5f76f20e22f973302642d8385b07f8441b5f2a63
thapanat <thapanat.sop@live.com>
2020-11-19 22:42:30 +0700

    Create __openerp__.py

---
+++ pabi_jobs_hook/__openerp__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "NSTDA :: PABI2 - Jobs Hook",
+    "summary": "",
+    "version": "1.0",
+    "category": "Tools",
+    "description": """
+    
+PABI Jobs Hook
+====================
+- Release all commited budget for PR
+- Release all commited budget for EX
+- Recompute Adjustment for document JV
+
+    """,
+    "author": "Thapanat.S",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "account_budget_activity","account"
+    ],
+    "data": [
+    ],
+}
